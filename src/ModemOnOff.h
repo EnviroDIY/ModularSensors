9b5d7360322d9191459d0c2b042d3c3e8112c0a9
SRGDamia1 <sdamiano@stroudcenter.org>
2017-06-08 16:24:09 -0400

    Split the modem on/off into a separate file from modem support
    The file was too long and I was getting lost.  This may also compile a touch faster.

---
+++ src/ModemOnOff.h
@@ -0,0 +1,104 @@
+/*
+ *ModemOnOff.h
+ *This file is part of the EnviroDIY modular sensors library for Arduino
+ *
+ *Initial library developement done by Sara Damiano (sdamiano@stroudcenter.org).
+ *
+ *This file is for turning modems on and off to save power.
+*/
+
+#ifndef ModemOnOff_h
+#define ModemOnOff_h
+
+// #define MODULES_DBG Serial
+#include "ModSensorDebugger.h"
+#include <Arduino.h>
+
+
+/* ===========================================================================
+* Classes for turning modems on and off
+* IDEA FOR THIS TAKEN FROM SODAQ'S MODEM LIBRARIES
+* ========================================================================= */
+
+/* ===========================================================================
+* Functions for the OnOff class
+* ========================================================================= */
+
+class ModemOnOff
+{
+public:
+    // Constructor
+    ModemOnOff();
+
+    // Initializes the instance
+    virtual void init(int vcc33Pin, int onoff_DTR_pin, int status_CTS_pin);
+
+    virtual bool isOn(void);
+    virtual bool on(void) = 0;
+    virtual bool off(void) = 0;
+
+protected:
+    int8_t _vcc33Pin;
+    int8_t _onoff_DTR_pin;
+    int8_t _status_CTS_pin;
+
+    void powerOn(void);
+    void powerOff(void);
+
+};
+
+
+
+/* ===========================================================================
+* Functions for pulsed method.
+* This turns the modem on and off by turning the onoff/DTR/Key pin on for two
+* seconds and then back off.
+* This is used by the Sodaq GPRSBee v0.4 and the Adafruit Fona.
+* ========================================================================= */
+
+// Turns the modem on and off by pulsing the onoff/DTR/Key pin on for 2 seconds
+class pulsedOnOff : public ModemOnOff
+{
+public:
+    bool on(void) override;
+    bool off(void) override;
+
+private:
+    void pulse(void);
+
+};
+
+
+/* ===========================================================================
+* Functions for held method.
+* This turns the modem on by setting the onoff/DTR/Key pin high and off by
+* setting it low.
+* This is used by the Sodaq GPRSBee v0.6.
+* ========================================================================= */
+
+// Turns the modem on by setting the onoff/DTR/Key high and off by setting it low
+class heldOnOff : public ModemOnOff
+{
+public:
+    bool on(void) override;
+    bool off(void) override;
+};
+
+
+/* ===========================================================================
+* Functions for reverse method.
+* This turns the modem on by setting the onoff/DTR/Key pin LOW and off by
+* setting it HIGH.
+* This is used by the XBee's
+* ========================================================================= */
+
+// Turns the modem on by setting the onoff/DTR/Key LOW and off by setting it HIGH
+class reverseOnOff : public ModemOnOff
+{
+public:
+    bool isOn(void) override;
+    bool on(void) override;
+    bool off(void) override;
+};
+
+#endif /* ModemOnOff_h */
