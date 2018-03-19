97a215ca5bacf05191d6c611428ecf5f22a5e072
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2018-03-18 21:23:39 -0500

    Updates to MS5803.h

--- src/MeaSpecMS5803.h
+++ src/MeaSpecMS5803.h
@@ -1,14 +1,16 @@
 /*
- *BoschBME280.h
+ *MeaSpecMS5803.h
  *This file is part of the EnviroDIY modular sensors library for Arduino
  *
- *Initial library developement done by Sara Damiano (sdamiano@stroudcenter.org).
+ *Initial library developement done by Anthony Aufdenkampe <aaufdenkampe@limno.com>.
+ * with help from Beth Fisher and Evan Host
  *
- *This file is for the Bosch BME280 Digital Pressure and Humidity Sensor
- *It is dependent on the Adafruit BME280 Library
+ *This file is for the Measurement Specialties MS5803-14BA pressure sensor,
+  as in SparkFun Pressure Sensor Breakout - MS5803-14BA, which uses the .
+ *It is dependent on the SparkFun_MS5803-14BA_Breakout_Arduino_Library
  *
  *Documentation for the sensor can be found at:
- *https://www.bosch-sensortec.com/bst/products/all_products/bme280
+ *https://www.sparkfun.com/products/12909
  *
  * For Barometric Pressure:
  *  Resolution is 0.18Pa
@@ -21,17 +23,13 @@
  *  Accuracy is ±0.5°C
  *  Range is -40°C to +85°C
  *
- * For Humidity:
- *  Resolution is 0.008 % RH (16 bit)
- *  Accuracy is ± 3 % RH
- *
  * Sensor takes about 100ms to respond
  * Slowest response time (humidity): 1sec
  * Assume sensor is immediately stable
 */
 
-#ifndef BoschBME280_h
-#define BoschBME280_h
+#ifndef MeaSpecMS5803_h
+#define MeaSpecMS5803_h
 
 #include <Arduino.h>
 
@@ -41,31 +39,26 @@
 
 #include "SensorBase.h"
 #include "VariableBase.h"
-#include <Adafruit_BME280.h>
+#include <SparkFun_MS5803_I2C.h>
 
-#define BME280_NUM_VARIABLES 4
-#define BME280_WARM_UP_TIME_MS 100
-#define BME280_STABILIZATION_TIME_MS 0
-#define BME280_MEASUREMENT_TIME_MS 1000
+#define MS5803_NUM_VARIABLES 2
+#define MS5803_WARM_UP_TIME_MS 100
+#define MS5803_STABILIZATION_TIME_MS 0
+#define MS5803_MEASUREMENT_TIME_MS 1000
 
-#define BME280_TEMP_RESOLUTION 2
-#define BME280_TEMP_VAR_NUM 0
+#define MS5803_TEMP_RESOLUTION 2
+#define MS5803_TEMP_VAR_NUM 0
 
-#define BME280_HUMIDITY_RESOLUTION 3
-#define BME280_HUMIDITY_VAR_NUM 1
 
-#define BME280_PRESSURE_RESOLUTION 2
-#define BME280_PRESSURE_VAR_NUM 2
+#define MS5803_PRESSURE_RESOLUTION 2
+#define MS5803_PRESSURE_VAR_NUM 2
 
-#define BME280_ALTITUDE_RESOLUTION 0
-#define BME280_ALTITUDE_VAR_NUM 3
-#define SEALEVELPRESSURE_HPA (1013.25)
 
-// The main class for the Bosch BME280
-class BoschBME280 : public Sensor
+// The main class for the Measurement Specialties MS5803
+class MeaSpecMS5803 : public Sensor
 {
 public:
-    BoschBME280(int8_t powerPin, uint8_t i2cAddressHex = 0x76, uint8_t measurementsToAverage = 1);
+    MeaSpecMS5803(int8_t powerPin, uint8_t i2cAddressHex = 0x76, uint8_t measurementsToAverage = 1);
 
     bool wake(void) override;
     SENSOR_STATUS setup(void) override;
@@ -75,63 +68,35 @@ public:
     // bool startSingleMeasurement(void) override;  // for forced mode
     bool addSingleMeasurementResult(void) override;
 protected:
-    Adafruit_BME280 bme_internal;
+    SparkFun_MS5803_I2C MS5803_internal;
     uint8_t _i2cAddressHex;
 };
 
 
 // Defines the Temperature Variable
-class BoschBME280_Temp : public Variable
+class MeaSpecMS5803_Temp : public Variable
 {
 public:
-    BoschBME280_Temp(Sensor *parentSense,
+    MeaSpecMS5803_Temp(Sensor *parentSense,
                      String UUID = "", String customVarCode = "")
-      : Variable(parentSense, BME280_TEMP_VAR_NUM,
+      : Variable(parentSense, MS5803_TEMP_VAR_NUM,
                F("temperature"), F("degreeCelsius"),
-               BME280_TEMP_RESOLUTION,
-               F("BoschBME280Temp"), UUID, customVarCode)
-    {}
-};
-
-
-// Defines the Humidity Variable
-class BoschBME280_Humidity : public Variable
-{
-public:
-    BoschBME280_Humidity(Sensor *parentSense,
-                         String UUID = "", String customVarCode = "")
-      : Variable(parentSense, BME280_HUMIDITY_VAR_NUM,
-               F("relativeHumidity"), F("percent"),
-               BME280_HUMIDITY_RESOLUTION,
-               F("BoschBME280Humidity"), UUID, customVarCode)
+               MS5803_TEMP_RESOLUTION,
+               F("MeaSpecMS5803Temp"), UUID, customVarCode)
     {}
 };
 
 
 // Defines the Pressure Variable
-class BoschBME280_Pressure : public Variable
+class MeaSpecMS5803_Pressure : public Variable
 {
 public:
-    BoschBME280_Pressure(Sensor *parentSense,
+    MeaSpecMS5803_Pressure(Sensor *parentSense,
                          String UUID = "", String customVarCode = "")
-      : Variable(parentSense, BME280_PRESSURE_VAR_NUM,
+      : Variable(parentSense, MS5803_PRESSURE_VAR_NUM,
                F("barometricPressure"), F("pascal"),
-               BME280_PRESSURE_RESOLUTION,
-               F("BoschBME280Pressure"), UUID, customVarCode)
-    {}
-};
-
-
-// Defines the Altitude Variable
-class BoschBME280_Altitude : public Variable
-{
-public:
-    BoschBME280_Altitude(Sensor *parentSense,
-                         String UUID = "", String customVarCode = "")
-      : Variable(parentSense, BME280_ALTITUDE_VAR_NUM,
-               F("heightAboveSeaFloor"), F("meter"),
-               BME280_ALTITUDE_RESOLUTION,
-               F("BoschBME280Altitude"), UUID, customVarCode)
+               MS5803_PRESSURE_RESOLUTION,
+               F("MeaSpecMS5803Pressure"), UUID, customVarCode)
     {}
 };
 
