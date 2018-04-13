53d87c29d886b9b7ee56883a302889c220c5efa9
SRGDamia1 <sdamiano@stroudcenter.org>
2018-04-13 13:41:12 -0400

    Changes for linking with SCAN

--- src/VariableBase.h
+++ src/VariableBase.h
@@ -34,9 +34,9 @@ public:
     virtual bool setup(void);
 
     // This gets the variable's name using http://vocabulary.odm2.org/variablename/
-    String getVarName(void);
+    virtual String getVarName(void);
     // This gets the variable's unit using http://vocabulary.odm2.org/units/
-    String getVarUnit(void);
+    virtual String getVarUnit(void);
     // This returns a customized code for the variable, if one is given, and a default if not
     String getVarCode(void);
     // This returns the variable UUID, if one has been assigned
