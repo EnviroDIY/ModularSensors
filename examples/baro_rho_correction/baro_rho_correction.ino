bc67a8792d96581baec464e6bde9f8aecf05f64b
SRGDamia1 <sdamiano@stroudcenter.org>
2018-05-22 14:39:58 -0400

    baro/density correction example now uses calculated variables

--- examples/baro_rho_correction/baro_rho_correction.ino
+++ examples/baro_rho_correction/baro_rho_correction.ino
@@ -19,8 +19,8 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 // #define TINY_GSM_MODEM_A6  // Select for a AI-Thinker A6 or A7 chip
 // #define TINY_GSM_MODEM_M590  // Select for a Neoway M590
 // #define TINY_GSM_MODEM_UBLOX  // Select for most u-blox cellular modems
-#define TINY_GSM_MODEM_ESP8266  // Select for an ESP8266 using the DEFAULT AT COMMAND FIRMWARE
-// #define TINY_GSM_MODEM_XBEE  // Select for Digi brand WiFi or Cellular XBee's
+// #define TINY_GSM_MODEM_ESP8266  // Select for an ESP8266 using the DEFAULT AT COMMAND FIRMWARE
+#define TINY_GSM_MODEM_XBEE  // Select for Digi brand WiFi or Cellular XBee's
 
 // ==========================================================================
 //    Include the base required libraries
@@ -162,7 +162,6 @@ MaximDS18 ds18_u(OneWirePower, OneWireBus);
 Variable *ds18Temp = new MaximDS18_Temp(&ds18_u, "12345678-abcd-1234-efgh-1234567890ab");
 
 
-
 // ==========================================================================
 //    MeaSpecMS5803 (Pressure, Temperature)
 // ==========================================================================
@@ -171,7 +170,7 @@ Variable *ds18Temp = new MaximDS18_Temp(&ds18_u, "12345678-abcd-1234-efgh-123456
 const uint8_t MS5803i2c_addr = 0x76;  // The MS5803 can be addressed either as 0x76 (default) or 0x77
 const int MS5803maxPressure = 14;  // The maximum pressure measurable by the specific MS5803 model
 const uint8_t MS5803ReadingsToAvg = 1;
-// Create and return the MeaSpec MS5803 sensor object
+// Create and return the MeaSpec MS5803 pressure and temperature sensor object
 MeaSpecMS5803 ms5803(I2CPower, MS5803i2c_addr, MS5803maxPressure, MS5803ReadingsToAvg);
 // Create the temperature and pressure variable objects for the MS5803 and return variable pointers to them
 Variable *msTemp = new MeaSpecMS5803_Temp(&ms5803, "12345678-abcd-1234-efgh-1234567890ab");
@@ -179,8 +178,92 @@ Variable *msPress = new MeaSpecMS5803_Pressure(&ms5803, "12345678-abcd-1234-efgh
 
 
 // ==========================================================================
+//    Calculated Variables
+// ==========================================================================
+
+// Create the function to calculate the water pressure
+// Water pressure = pressure from MS5803 (water+baro) - pressure from BME280 (baro)
+// The MS5803 reports pressure in millibar, the BME280 in pascal
+// 1 pascal = 0.01 mbar
+float calculateWaterPressure(void)
+{
+    float waterPressure = msPress->getValue() - (bPress->getValue())*0.01;
+    // Serial.print(F("Water pressure is "));  // for debugging
+    // Serial.println(waterPressure);  // for debugging
+    return waterPressure;
+}
+// Properties of the calculated water pressure variable
+String waterPresureVarName = "pressureGauge";  // This must be a value from http://vocabulary.odm2.org/variablename/
+String waterPresureVarUnit = "millibar";  // This must be a value from http://vocabulary.odm2.org/units/
+int waterPresureVarResolution = 3;
+String waterPressureUUID = "12345678-abcd-1234-efgh-1234567890ab";
+String waterPresureVarCode = "CorrectedPressure";
+// Create the calculated water pressure variable objects and return a variable pointer to it
+Variable *calcWaterPress = new Variable(calculateWaterPressure, waterPresureVarName,
+                                        waterPresureVarUnit, waterPresureVarResolution,
+                                        waterPressureUUID, waterPresureVarCode);
+
+// Create the function to calculate the "raw" water depth
+// For this, we're using the conversion between mbar and mm pure water at 4°C
+// This calculation gives a final result in mm of water
+float calculateWaterDepthRaw(void)
+{
+    float waterDepth = calculateWaterPressure()*10.1972;
+    // Serial.print(F("'Raw' water depth is "));  // for debugging
+    // Serial.println(waterDepth);  // for debugging
+    return waterDepth;
+}
+// Properties of the calculated water depth variable
+String waterDepthVarName = "waterDepth";  // This must be a value from http://vocabulary.odm2.org/variablename/
+String waterDepthVarUnit = "millimeter";  // This must be a value from http://vocabulary.odm2.org/units/
+int waterDepthVarResolution = 3;
+String waterDepthUUID = "12345678-abcd-1234-efgh-1234567890ab";
+String waterDepthVarCode = "CalcDepth";
+// Create the calculated raw water depth variable objects and return a variable pointer to it
+Variable *calcRawDepth = new Variable(calculateWaterDepthRaw, waterDepthVarName,
+                                      waterDepthVarUnit, waterDepthVarResolution,
+                                      waterDepthUUID, waterDepthVarCode);
+
+// Create the function to calculate the water depth after correcting water density for temperature
+// This calculation gives a final result in mm of water
+float calculateWaterDepthTempCorrected(void)
+{
+    const float gravitationalConstant = 9.80665; // m/s2, meters per second squared
+    // First get water pressure in Pa for the calculation: 1 mbar = 100 Pa
+    float waterPressurePa = 100 * calculateWaterPressure();
+    float waterTempertureC = msTemp->getValue();
+    // Converting water depth for the changes of pressure with depth
+    // Water density (kg/m3) from equation 6 from JonesHarris1992-NIST-DensityWater.pdf
+    float waterDensity =  + 999.84847
+                          + 6.337563e-2 * waterTempertureC
+                          - 8.523829e-3 * pow(waterTempertureC,2)
+                          + 6.943248e-5 * pow(waterTempertureC,3)
+                          - 3.821216e-7 * pow(waterTempertureC,4)
+                          ;
+    // This calculation gives a final result in mm of water
+    // from P = rho * g * h
+    float rhoDepth = 1000 * waterPressurePa/(waterDensity * gravitationalConstant);
+    // Serial.print(F("Temperature corrected water depth is "));  // for debugging
+    // Serial.println(rhoDepth);  // for debugging
+    return rhoDepth;
+}
+// Properties of the calculated temperature corrected water depth variable
+String rhoDepthVarName = "waterDepth";  // This must be a value from http://vocabulary.odm2.org/variablename/
+String rhoDepthVarUnit = "millimeter";  // This must be a value from http://vocabulary.odm2.org/units/
+int rhoDepthVarResolution = 3;
+String rhoDepthUUID = "12345678-abcd-1234-efgh-1234567890ab";
+String rhoDepthVarCode = "DensityDepth";
+// Create the temperature corrected water depth variable objects and return a variable pointer to it
+Variable *calcCorrDepth = new Variable(calculateWaterDepthTempCorrected, rhoDepthVarName,
+                                       rhoDepthVarUnit, rhoDepthVarResolution,
+                                       rhoDepthUUID, rhoDepthVarCode);
+
+
+// ==========================================================================
 //    The array that contains all variables to be logged
 // ==========================================================================
+// NOTE:  Since we've created all of the variable pointers above, we can just
+// reference them by name here.
 Variable *variableList[] = {
     mayflyBatt,
     mayflyRAM,
@@ -192,6 +275,9 @@ Variable *variableList[] = {
     msTemp,
     msPress,
     ds18Temp,
+    calcWaterPress,
+    calcRawDepth,
+    calcCorrDepth,
     modemRSSI,
     modemSinalPct
 };
@@ -204,34 +290,6 @@ int variableCount = sizeof(variableList) / sizeof(variableList[0]);
 // ==========================================================================
 const char *registrationToken = "12345678-abcd-1234-efgh-1234567890ab";   // Device registration token
 const char *samplingFeature = "12345678-abcd-1234-efgh-1234567890ab";     // Sampling feature UUID
-
-
-// ==========================================================================
-// Extra information and tokens for calculted variables
-// ==========================================================================
-const char *waterPressureUUID = "12345678-abcd-1234-efgh-1234567890ab";
-const char *waterPresureVarName = "pressureGauge";
-const char *waterPresureVarUnit = "Millibar";
-const char *waterPresureVarCode = "CorrectedPressure";
-const char *waterDepthUUID = "12345678-abcd-1234-efgh-1234567890ab";
-const char *waterDepthVarName = "waterDepth";
-const char *waterDepthVarUnit = "millimeter";
-const char *waterDepthVarCode = "CalcDepth";
-const char *rhoDepthUUID = "12345678-abcd-1234-efgh-1234567890ab";
-const char *rhoDepthVarName = "rhoDepth";
-const char *rhoDepthVarUnit = "millimeter";
-const char *rhoDepthVarCode = "DensityDepth";
-
-
-// ==========================================================================
-// Initialize variables for density correction
-// ==========================================================================
-float waterPressureBar = -9999.0;
-float waterPressurePa  = -9999.0;
-float waterTempertureC = -9999.0;
-float waterDensity = -9999.0;
-float rhoDepth = -9999.0;
-const float gravitationalConstant = 9.80665; // m/s2, meters per second squared
 
 
 // ==========================================================================
@@ -287,14 +345,6 @@ void setup()
                 loggingInterval, LoggerID);
     EnviroDIYLogger.setAlertPin(greenLED);
 
-    // Begin the real time clock
-    rtc.begin();
-    delay(100);
-
-    // Print out the current time
-    Serial.print(F("Current RTC time is: "));
-    Serial.println(Logger::formatDateTime_ISO8601(Logger::getNowEpoch()));
-
     // Setup the logger modem
     #if defined(TINY_GSM_MODEM_ESP8266)
         modem.setupModem(&ModemSerial, modemVCCPin, modemStatusPin, modemSleepRqPin, ModemSleepMode, wifiId, wifiPwd);
@@ -307,71 +357,18 @@ void setup()
 
     // Attach the modem to the logger
     EnviroDIYLogger.attachModem(&modem);
-    // Immediately turn on the modem
-    EnviroDIYLogger._logModem->modemPowerUp();
 
     // Enter the tokens for the connection with EnviroDIY
     EnviroDIYLogger.setToken(registrationToken);
     EnviroDIYLogger.setSamplingFeatureUUID(samplingFeature);
 
-    // Set up the sensors
-    EnviroDIYLogger.setupSensors();
+    // Set up the connection with DreamHost
+    #ifdef DreamHostPortalRX
+        EnviroDIYLogger.setDreamHostPortalRX(DreamHostPortalRX);
+    #endif
 
-    // Sync the clock with NIST
-    Serial.print(F("Attempting to synchronize RTC with NIST\n"));
-    // Connect to the network
-    if (EnviroDIYLogger._logModem->connectInternet())
-    {
-        EnviroDIYLogger.syncRTClock(EnviroDIYLogger._logModem->getNISTTime());
-        // Disconnect from the network
-        EnviroDIYLogger._logModem->disconnectInternet();
-    }
-    // Turn off the modem
-    EnviroDIYLogger._logModem->modemPowerDown();
-
-    // Generate a logger file name from the LoggerID and the date/time on the RTC
-    // This will start a new file every time the logger is reset
-    EnviroDIYLogger.setFileName();
-
-    // Set up the log file and add a header to it with all of the information
-    // for the sensors and variables in the variable array
-    EnviroDIYLogger.setupLogFile();
-
-    // Now we're going to add another set of rows to the file header with
-    // just the information for the calculated variable
-    // We could make a "prettier" header by adding commas and values onto the
-    // end of each line of the multi-line header generated by the
-    // generateFileHeader() function, but that would take a bunch of really
-    // annoying string subscripting and re-combining (or just rewriting that
-    // whole generateFileHeader function, which is already a long annoying
-    // string concatenation function) so instead we're just creating another set
-    // of header rows for the csv with no values in the first spaces where
-    // header values exist and then starting the new header information shifted
-    // over by that many columns/commas
-    String extraHeaderSpacer = "";
-    for (uint8_t i=0; i <= variableCount; i++) extraHeaderSpacer += ',';
-    // the first row is the variable UUID's, we have two:
-    // for calculated pressure and calculated depth
-    EnviroDIYLogger.logToSD(extraHeaderSpacer + String(waterPressureUUID) +
-                            F(", ") + String(waterDepthUUID) + F(", ") +
-                            String(rhoDepthUUID));
-    // the next row is the sensor name, which is blank, so we'll put in a blank row
-    EnviroDIYLogger.logToSD(extraHeaderSpacer + F(", "));
-    // the third row is the variable name
-    EnviroDIYLogger.logToSD(extraHeaderSpacer + String(waterPresureVarName) +
-                            F(", ") + String(waterDepthVarName) + F(", ") +
-                            String(rhoDepthVarName));
-    // the fourth row is the variable unit name
-    EnviroDIYLogger.logToSD(extraHeaderSpacer + String(waterPresureVarUnit) +
-                            F(", ") + String(waterDepthVarUnit) + F(", ") +
-                            String(rhoDepthVarUnit));
-    // the last row is the variable code
-    EnviroDIYLogger.logToSD(extraHeaderSpacer + String(waterPresureVarCode) +
-                            F(", ") + String(waterDepthVarCode) + F(", ") +
-                            String(rhoDepthVarCode));
-
-    // Setup the logger sleep mode
-    EnviroDIYLogger.setupSleep();
+    // Begin the logger
+    EnviroDIYLogger.begin();
 
     // Hold up for 10-seconds to allow immediate entry into sensor testing mode
     // EnviroDIYLogger.checkForTestingMode(buttonPin);
@@ -382,9 +379,6 @@ void setup()
     Serial.print(F("Push button on pin "));
     Serial.print(buttonPin);
     Serial.println(F(" at any time to enter sensor testing mode."));
-
-    Serial.print(F("Logger setup finished!\n"));
-    Serial.print(F("------------------------------------------\n\n"));
 
     // Blink the LEDs really fast to show start-up is done
     greenredflash(6, 25);
@@ -397,135 +391,8 @@ void setup()
 // ==========================================================================
 // Main loop function
 // ==========================================================================
-
-// Because of the way the sleep mode is set up, the processor will wake up
-// and start the loop every minute exactly on the minute.
 void loop()
 {
-    // Assuming we were woken up by the clock, check if the current time is an
-    // even interval of the logging interval
-    if (EnviroDIYLogger.checkInterval())
-    {
-        // Print a line to show new reading
-        Serial.print(F("------------------------------------------\n"));
-        // Turn on the LED to show we're taking a reading
-        digitalWrite(greenLED, HIGH);
-
-        // Turn on the modem to let it start searching for the network
-        EnviroDIYLogger._logModem->modemPowerUp();
-
-        // Send power to all of the sensors
-        Serial.print(F("Powering sensors...\n"));
-        EnviroDIYLogger.sensorsPowerUp();
-        // Wake up all of the sensors
-        Serial.print(F("Waking sensors...\n"));
-        EnviroDIYLogger.sensorsWake();
-        // Update the values from all attached sensors
-        Serial.print(F("Updating sensor values...\n"));
-        EnviroDIYLogger.updateAllSensors();
-        // Put sensors to sleep
-        Serial.print(F("Putting sensors back to sleep...\n"));
-        EnviroDIYLogger.sensorsSleep();
-        // Cut sensor power
-        Serial.print(F("Cutting sensor power...\n"));
-        EnviroDIYLogger.sensorsPowerDown();
-
-        // Generate a csv with the sensor-recorded variables
-        String csvRaw = EnviroDIYLogger.generateSensorDataCSV();
-
-        // Generate a json with the sensor-recorded variables
-        String jsonRaw = EnviroDIYLogger.generateSensorDataJSON();
-
-        // Calculate the water pressure after barometic correction
-        // As long as the seensor updates took less than two minutes, these
-        // getValue() functions will return the values from the last update
-        // that happened in the updateAllSensors() function
-        // The MS5803 reports pressure in millibar, the BME280 in pascal
-        // 1 pascal = 0.01 mbar
-        // This calculation gives a final result in mbar
-        Serial.print(F("Calculating water pressure...\n"));
-        float waterPressure = msPress->getValue() - (bPress->getValue())*0.01;
-        float waterTempertureC = msTemp->getValue();
-        // Now calculate the depth of the water
-        // For this, we're using the conversion between mbar and mm pure water at 4°C
-        // This calculation gives a final result in mm of water
-        float waterDepth = waterPressure*10.1972;
-        // Converting water depth for the changes of pressure with depth
-        // First get water pressure in Pa for the calculation: 1 mbar = 100 Pa
-        waterPressurePa = 100 * waterPressure;
-        // Water density (kg/m3) from equation 6 from JonesHarris1992-NIST-DensityWater.pdf
-        waterDensity =  + 999.84847
-                        + 6.337563e-2 * waterTempertureC
-                        - 8.523829e-3 * pow(waterTempertureC,2)
-                        + 6.943248e-5 * pow(waterTempertureC,3)
-                        - 3.821216e-7 * pow(waterTempertureC,4)
-                        ;
-        // This calculation gives a final result in mm of water
-        rhoDepth = 1000 * waterPressurePa/(waterDensity * gravitationalConstant);  // from P = rho * g * h
-
-
-        // Add the water pressure and depth to the csv string
-        String csvToGo = csvRaw + ",";
-        csvToGo += String(waterPressure);
-        csvToGo += ",";
-        csvToGo += String(waterDepth);
-        csvToGo += ",";
-        csvToGo += String(rhoDepth);
-
-        // Add the total water pressure to the raw json
-        // Figure out how long the original json is
-        int jsonLength = jsonRaw.length();
-        // Crop off the last ' }' from the json
-        String jsonToGo = jsonRaw.substring(0,jsonLength-1);
-        // add the UUID for the water pressure
-        jsonToGo += F(", \"");
-        jsonToGo += String(waterPressureUUID);
-        jsonToGo += F("\": ");
-        // add the water pressure value
-        jsonToGo += String(waterPressure);
-        // add the UUID for the water depth
-        jsonToGo += F(", \"");
-        jsonToGo += String(waterDepthUUID);
-        jsonToGo += F("\": ");
-        // add the water depth value
-        jsonToGo += String(waterDepth);
-        // add the UUID for the density corrected water depth
-        jsonToGo += F(", \"");
-        jsonToGo += String(rhoDepthUUID);
-        jsonToGo += F("\": ");
-        // add the density corrected water depth value
-        jsonToGo += String(rhoDepth);
-        // re-add the last '}'
-        jsonToGo += F("}");
-
-        // Connect to the network
-        Serial.print(F("Connecting to the internet...\n"));
-        if (EnviroDIYLogger._logModem->connectInternet())
-        {
-            // Post the data to the WebSDL
-            EnviroDIYLogger.postDataEnviroDIY(jsonToGo);
-
-            // Disconnect from the network
-            EnviroDIYLogger._logModem->disconnectInternet();
-        }
-        // Turn the modem off
-        EnviroDIYLogger._logModem->modemPowerDown();
-
-        // Create a csv data record and save it to the log file
-        EnviroDIYLogger.logToSD(csvToGo);
-
-        // Turn off the LED
-        digitalWrite(greenLED, LOW);
-        // Print a line to show reading ended
-        Serial.print(F("------------------------------------------\n\n"));
-    }
-
-    // Check if it was instead the testing interrupt that woke us up
-    // NOTE:  This testing mode will *NOT* show the values from any of the extra
-    // calculated variables added in the loop.  It will only show the variables
-    // listed in the variableList which is fed to the EnviroDIYLogger.
-    if (Logger::startTesting) EnviroDIYLogger.testingMode();
-
-    // Sleep
-    EnviroDIYLogger.systemSleep();
+    // Log the data
+    EnviroDIYLogger.log();
 }
