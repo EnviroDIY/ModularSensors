f9a0a341ef509f0ef8438aebc26b1210a346e4a9
SRGDamia1 <sdamiano@stroudcenter.org>
2018-05-22 12:50:49 -0400

    Fixed single_sensor for Travis

--- examples/single_sensor/single_sensor.ino
+++ examples/single_sensor/single_sensor.ino
@@ -48,15 +48,15 @@ const int8_t SonarPower = 22;   // excite (power) pin
 
 #if defined __AVR__
 
-// #include <SoftwareSerial_ExtInts.h>  // for the stream communication
-// SoftwareSerial_ExtInts sonarSerial(SonarData, -1);  // No Tx pin is required, only Rx
+#include <SoftwareSerial_ExtInts.h>  // for the stream communication
+SoftwareSerial_ExtInts sonarSerial(SonarData, -1);  // No Tx pin is required, only Rx
 
-#include <NeoSWSerial.h>  // for the stream communication
-NeoSWSerial sonarSerial(SonarData, -1);  // No Tx pin is required, only Rx
-void NeoSWSISR()
-{
-  NeoSWSerial::rxISR( *portInputRegister( digitalPinToPort( SonarData ) ) );
-}
+// #include <NeoSWSerial.h>  // for the stream communication
+// NeoSWSerial sonarSerial(SonarData, -1);  // No Tx pin is required, only Rx
+// void NeoSWSISR()
+// {
+//   NeoSWSerial::rxISR( *portInputRegister( digitalPinToPort( SonarData ) ) );
+// }
 
 #endif
 
