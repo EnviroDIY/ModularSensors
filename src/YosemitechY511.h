6af8a6b66cf479c7a702891b80ba8009d9d552a6
Sara Damiano <sdamiano@stroudcenter.org>
2018-09-26 14:05:02 -0400

    Fixed Y510/Y511

--- src/YosemitechY511.h
+++ src/YosemitechY511.h
@@ -57,7 +57,7 @@ public:
     {}
     YosemitechY511(byte modbusAddress, Stream& stream, int8_t powerPin, int8_t powerPin2 = -1,
                    int8_t enablePin = -1, uint8_t measurementsToAverage = 1)
-     : YosemitechParent(modbusAddress, stream,powerPin,  enablePin, measurementsToAverage,
+     : YosemitechParent(modbusAddress, stream, powerPin, powerPin2, enablePin, measurementsToAverage,
                         Y511, "YosemitechY511", Y511_NUM_VARIABLES,
                         Y511_WARM_UP_TIME_MS, Y511_STABILIZATION_TIME_MS, Y511_MEASUREMENT_TIME_MS)
     {}
