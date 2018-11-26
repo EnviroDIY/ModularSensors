ca3fd8a3e27215e0a9b6ba6992f0b418fd054fbf
Sara Damiano <sdamiano@stroudcenter.org>
2018-11-26 15:08:58 -0500

    dump for debugging

--- pio_set_global_flags.py
+++ pio_set_global_flags.py
@@ -1,11 +1,26 @@
 Import('env')
 from os.path import join, realpath
 
-# pass flags to a global build environment (for all libraries, etc)
+# pass the same flags to the global build environment (for all libraries, etc)
 global_env = DefaultEnvironment()
 global_env.Append(
     CPPDEFINES=[
-        "NEOSWSERIAL_EXTERNAL_PCINT",
-        "SDI12_EXTERNAL_PCINT"
+        ("NEOSWSERIAL_EXTERNAL_PCINT",),
+        ("SDI12_EXTERNAL_PCINT",)
     ]
 )
+
+print "<<<<<GLOBAL ENV>>>>>"
+print global_env.Dump()
+
+# pass flags to local build environment (for just this library)
+env.Append(
+    CPPDEFINES=[
+        ("NEOSWSERIAL_EXTERNAL_PCINT",),
+        ("SDI12_EXTERNAL_PCINT",)
+    ]
+)
+
+
+# print ">>>>>LOCAL ENV<<<<<"
+# print env.Dump()
