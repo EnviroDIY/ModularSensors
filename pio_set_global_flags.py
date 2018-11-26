4f060ba3305017e08ecceb31fa5653782151bec8
Sara Damiano <sdamiano@stroudcenter.org>
2018-11-26 15:51:08 -0500

    ci verbose

--- pio_set_global_flags.py
+++ pio_set_global_flags.py
@@ -1,6 +1,16 @@
 Import('env')
 from os.path import join, realpath
 
+pass flags to local build environment (for just this library)
+env.Append(
+    CPPDEFINES=[
+        ("NEOSWSERIAL_EXTERNAL_PCINT",),
+        ("SDI12_EXTERNAL_PCINT",)
+    ]
+)
+print ">>>>>LOCAL ENV<<<<<"
+print env.Dump()
+
 # pass the same flags to the global build environment (for all libraries, etc)
 global_env = DefaultEnvironment()
 global_env.Append(
@@ -9,18 +19,5 @@ global_env.Append(
         ("SDI12_EXTERNAL_PCINT",)
     ]
 )
-
 print "<<<<<GLOBAL ENV>>>>>"
 print global_env.Dump()
-
-# pass flags to local build environment (for just this library)
-env.Append(
-    CPPDEFINES=[
-        ("NEOSWSERIAL_EXTERNAL_PCINT",),
-        ("SDI12_EXTERNAL_PCINT",)
-    ]
-)
-
-
-# print ">>>>>LOCAL ENV<<<<<"
-# print env.Dump()
