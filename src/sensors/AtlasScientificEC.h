c559a52e50ac1a54cf4f08d51a32bfbd28de3787
Sara Damiano <sdamiano@stroudcenter.org>
2019-02-08 14:14:54 -0500

    Renamed from cond to EC to help not confuse sensor vs variable

--- src/sensors/AtlasScientificEC.h
+++ src/sensors/AtlasScientificEC.h
@@ -1,5 +1,5 @@
 /*
- * AtlasScientificCond.h
+ * AtlasScientificEC.h
  * This file is part of the EnviroDIY modular sensors library for Arduino
  *
  * Initial developement for Atlas Sensors was done by Adam Gold
@@ -18,8 +18,8 @@
  */
 
 // Header Guards
-#ifndef AtlasScientificCond_h
-#define AtlasScientificCond_h
+#ifndef AtlasScientificEC_h
+#define AtlasScientificEC_h
 
 // Debugging Statement
 // #define DEBUGGING_SERIAL_OUTPUT Serial
@@ -51,70 +51,70 @@
 #define ATLAS_SG_VAR_NUM 3
 
 // The main class for the Atlas Scientific Conductivity sensor
-class AtlasScientificCond : public AtlasParent
+class AtlasScientificEC : public AtlasParent
 {
 public:
-    AtlasScientificCond(int8_t powerPin, uint8_t i2cAddressHex = ATLAS_COND_I2C_ADDR, uint8_t measurementsToAverage = 1)
+    AtlasScientificEC(int8_t powerPin, uint8_t i2cAddressHex = ATLAS_COND_I2C_ADDR, uint8_t measurementsToAverage = 1)
      : SDI12Sensors(powerPin, i2cAddressHex, measurementsToAverage,
-                    "AtlasScientificCond", ATLAS_COND_NUM_VARIABLES,
+                    "AtlasScientificEC", ATLAS_COND_NUM_VARIABLES,
                     ATLAS_COND_WARM_UP_TIME_MS, ATLAS_COND_STABILIZATION_TIME_MS, ATLAS_COND_MEASUREMENT_TIME_MS)
-    ~AtlasScientificCond();
+    ~AtlasScientificEC();
 };
 
 // The class for the Conductivity Variable
-class AtlasScientificCond_Cond : public Variable
+class AtlasScientificEC_Cond : public Variable
 {
 public:
-    AtlasScientificCond_Cond(Sensor *parentSense,
+    AtlasScientificEC_Cond(Sensor *parentSense,
                         const char *UUID = "", const char *customVarCode = "")
       : Variable(parentSense, ATLAS_COND_VAR_NUM,
                  "electricalConductivity", "microsiemenPerCentimeter",
                  ATLAS_COND_RESOLUTION,
                  "AtlasCond", UUID, customVarCode)
     {}
-    ~AtlasScientificCond_Cond(){}
+    ~AtlasScientificEC_Cond(){}
 };
 
 // The class for the Total Dissolved Solids Variable
-class AtlasScientificCond_TDS : public Variable
+class AtlasScientificEC_TDS : public Variable
 {
 public:
-    AtlasScientificCond_TDS(Sensor *parentSense,
+    AtlasScientificEC_TDS(Sensor *parentSense,
                         const char *UUID = "", const char *customVarCode = "")
       : Variable(parentSense, ATLAS_TDS_VAR_NUM,
                  "electricalConductivity", "microsiemenPerCentimeter",
                  ATLAS_TDS_RESOLUTION,
                  "AtlasCond", UUID, customVarCode)
     {}
-    ~AtlasScientificCond_TDS(){}
+    ~AtlasScientificEC_TDS(){}
 };
 
 // The class for the Salinity Variable
-class AtlasScientificCond_Salinity : public Variable
+class AtlasScientificEC_Salinity : public Variable
 {
 public:
-    AtlasScientificCond_Salinity(Sensor *parentSense,
+    AtlasScientificEC_Salinity(Sensor *parentSense,
                         const char *UUID = "", const char *customVarCode = "")
       : Variable(parentSense, ATLAS_SALINITY_VAR_NUM,
                  "salinity", "practicalSalinityUnit",
                  ATLAS_SALINITY_RESOLUTION,
                  "AtlasSalinity", UUID, customVarCode)
     {}
-    ~AtlasScientificCond_Salinity(){}
+    ~AtlasScientificEC_Salinity(){}
 };
 
 // The class for the Specific Gravity Variable
-class AtlasScientificCond_SpecificGravity : public Variable
+class AtlasScientificEC_SpecificGravity : public Variable
 {
 public:
-    AtlasScientificCond_SpecificGravity(Sensor *parentSense,
+    AtlasScientificEC_SpecificGravity(Sensor *parentSense,
                         const char *UUID = "", const char *customVarCode = "")
       : Variable(parentSense, ATLAS_SG_VAR_NUM,
                  "specificGravity", "dimensionless",
                  ATLAS_SG_RESOLUTION,
                  "AtlasSpecGravity", UUID, customVarCode)
     {}
-    ~AtlasScientificCond_SpecificGravity(){}
+    ~AtlasScientificEC_SpecificGravity(){}
 };
 
 #endif  // Header Guard
