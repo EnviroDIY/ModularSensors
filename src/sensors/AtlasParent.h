eeea9647626c453f1b7954a9901af29f7fde3efa
Sara Damiano <sdamiano@stroudcenter.org>
2019-02-08 14:26:54 -0500

    Removed default i2C address from atlas parent

--- src/sensors/AtlasParent.h
+++ src/sensors/AtlasParent.h
@@ -29,7 +29,7 @@
 class AtlasParent : public Sensor
 {
 public:
-    AtlasParent(int8_t powerPin, uint8_t i2cAddressHex = ATLAS_COND_I2C_ADDR, uint8_t measurementsToAverage = 1,
+    AtlasParent(int8_t powerPin, uint8_t i2cAddressHex, uint8_t measurementsToAverage = 1,
                 const char *sensorName = "AtlasSensor", uint8_t numReturnedVars = 1,
                 uint32_t warmUpTime_ms = 0, uint32_t stabilizationTime_ms = 0, uint32_t measurementTime_ms = 0);
     virtual ~AtlasParent();
