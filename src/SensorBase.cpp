600cd5facaff4b6d28ee69777532cc56de81210d
Sara Damiano <sdamiano@stroudcenter.org>
2019-02-26 13:33:20 -0500

    Space change

--- src/SensorBase.cpp
+++ src/SensorBase.cpp
@@ -391,7 +391,7 @@ bool Sensor::update(void)
 
     // Check if the power is on, turn it on if not
     bool wasOn = checkPowerOn();
-    if (!wasOn){powerUp();}
+    if (!wasOn) {powerUp();}
 
     // Check if it's awake/active, activate it if not
     bool wasActive = bitRead(getStatus(), 3);
@@ -427,7 +427,7 @@ bool Sensor::update(void)
     if (wasActive){sleep();}
 
     // Turn the power back off it it had been turned on
-    if (!wasOn){powerDown();}
+    if (!wasOn) {powerDown();}
 
     // Update the registered variables with the new values
     notifyVariables();
