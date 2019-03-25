a5ba11c62a2c44f3d034eba453c1810167e196fa
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2019-03-24 22:56:10 -0500

    Update logging_to_mmw.ino
    Minimize code, as requested in #213.

--- examples/logging_to_mmw/logging_to_mmw.ino
+++ examples/logging_to_mmw/logging_to_mmw.ino
@@ -1,5 +1,5 @@
 /*****************************************************************************
-menu_a_la_carte.ino
+logging_to MMW.ino
 Written By:  Sara Damiano (sdamiano@stroudcenter.org)
 Development Environment: PlatformIO
 Hardware Platform: EnviroDIY Mayfly Arduino Datalogger
@@ -28,7 +28,7 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 // The library version this example was written for
 const char *libraryVersion = "0.21.2";
 // The name of this file
-const char *sketchName = "menu_a_la_carte.ino";
+const char *sketchName = "logging_to MMW.ino";
 // Logger ID, also becomes the prefix for the name of the data file on SD card
 const char *LoggerID = "XXXXX";
 // How frequently (in minutes) to log data
@@ -57,115 +57,6 @@ const int8_t sensorPowerPin = 22;  // MCU pin controlling main sensor power (-1 
 // Create the main processor chip "sensor" - for general metadata
 const char *mcuBoardVersion = "v0.5b";
 ProcessorStats mcuBoard(mcuBoardVersion);
-
-// Create sample number, battery voltage, and free RAM variable pointers for the processor
-// Variable *mcuBoardBatt = new ProcessorStats_Batt(&mcuBoard, "12345678-abcd-1234-efgh-1234567890ab");
-// Variable *mcuBoardAvailableRAM = new ProcessorStats_FreeRam(&mcuBoard, "12345678-abcd-1234-efgh-1234567890ab");
-// Variable *mcuBoardSampNo = new ProcessorStats_SampleNumber(&mcuBoard, "12345678-abcd-1234-efgh-1234567890ab");
-
-
-// ==========================================================================
-//    Settings for Additional Serial Ports
-// ==========================================================================
-
-// The modem and a number of sensors communicate over UART/TTL - often called "serial".
-// "Hardware" serial ports (automatically controlled by the MCU) are generally
-// the most accurate and should be configured and used for as many peripherals
-// as possible.  In some cases (ie, modbus communication) many sensors can share
-// the same serial port.
-
-#if not defined ARDUINO_ARCH_SAMD && not defined ATMEGA2560  // For AVR boards
-// Unfortunately, most AVR boards have only one or two hardware serial ports,
-// so we'll set up three types of extra software serial ports to use
-
-// AltSoftSerial by Paul Stoffregen (https://github.com/PaulStoffregen/AltSoftSerial)
-// is the most accurate software serial port for AVR boards.
-// AltSoftSerial can only be used on one set of pins on each board so only one
-// AltSoftSerial port can be used.
-// Not all AVR boards are supported by AltSoftSerial.
-#include <AltSoftSerial.h>
-AltSoftSerial altSoftSerial;
-
-// NeoSWSerial (https://github.com/SRGDamia1/NeoSWSerial) is the best software
-// serial that can be used on any pin supporting interrupts.
-// You can use as many instances of NeoSWSerial as you want.
-// Not all AVR boards are supported by NeoSWSerial.
-#include <NeoSWSerial.h>  // for the stream communication
-const int8_t neoSSerial1Rx = 11;     // data in pin
-const int8_t neoSSerial1Tx = -1;     // data out pin
-NeoSWSerial neoSSerial1(neoSSerial1Rx, neoSSerial1Tx);
-// To use NeoSWSerial in this library, we define a function to receive data
-// This is just a short-cut for later
-void neoSSerial1ISR()
-{
-    NeoSWSerial::rxISR(*portInputRegister(digitalPinToPort(neoSSerial1Rx)));
-}
-
-// The "standard" software serial library uses interrupts that conflict
-// with several other libraries used within this program, we must use a
-// version of software serial that has been stripped of interrupts.
-// NOTE:  Only use if necessary.  This is not a very accurate serial port!
-const int8_t softSerialRx = A3;     // data in pin
-const int8_t softSerialTx = A4;     // data out pin
-
-#include <SoftwareSerial_ExtInts.h>  // for the stream communication
-SoftwareSerial_ExtInts softSerial1(softSerialRx, softSerialTx);
-#endif  // End software serial for avr boards
-
-
-// The SAMD21 has 6 "SERCOM" ports, any of which can be used for UART communication.
-// The "core" code for most boards defines one or more UART (Serial) ports with
-// the SERCOMs and uses others for I2C and SPI.  We can create new UART ports on
-// any available SERCOM.  The table below shows definitions for select boards.
-
-// Board =>   Arduino Zero       Adafruit Feather    Sodaq Boards
-// -------    ---------------    ----------------    ----------------
-// SERCOM0    Serial1 (D0/D1)    Serial1 (D0/D1)     Serial (D0/D1)
-// SERCOM1    Available          Available           Serial3 (D12/D13)
-// SERCOM2    Available          Available           I2C (A4/A5)
-// SERCOM3    I2C (D20/D21)      I2C (D20/D21)       SPI (D11/12/13)
-// SERCOM4    SPI (D21/22/23)    SPI (D21/22/23)     SPI1/Serial2
-// SERCOM5    EDBG/Serial        Available           Serial1
-
-// If using a Sodaq board, do not define the new sercoms, instead:
-// #define ENABLE_SERIAL2
-// #define ENABLE_SERIAL3
-
-
-#if defined ARDUINO_ARCH_SAMD
-#include <wiring_private.h> // Needed for SAMD pinPeripheral() function
-
-#ifndef ENABLE_SERIAL2
-// Set up a 'new' UART using SERCOM1
-// The Rx will be on digital pin 11, which is SERCOM1's Pad #0
-// The Tx will be on digital pin 10, which is SERCOM1's Pad #2
-// NOTE:  SERCOM1 is undefinied on a "standard" Arduino Zero and many clones,
-//        but not all!  Please check the variant.cpp file for you individual board!
-//        Sodaq Autonomo's and Sodaq One's do NOT follow the 'standard' SERCOM definitions!
-Uart Serial2(&sercom1, 11, 10, SERCOM_RX_PAD_0, UART_TX_PAD_2);
-// Hand over the interrupts to the sercom port
-void SERCOM1_Handler()
-{
-    Serial2.IrqHandler();
-}
-#endif
-
-#ifndef ENABLE_SERIAL3
-// Set up a 'new' UART using SERCOM2
-// The Rx will be on digital pin 5, which is SERCOM2's Pad #3
-// The Tx will be on digital pin 2, which is SERCOM2's Pad #2
-// NOTE:  SERCOM2 is undefinied on a "standard" Arduino Zero and many clones,
-//        but not all!  Please check the variant.cpp file for you individual board!
-//        Sodaq Autonomo's and Sodaq One's do NOT follow the 'standard' SERCOM definitions!
-Uart Serial3(&sercom2, 5, 2, SERCOM_RX_PAD_3, UART_TX_PAD_2);
-// Hand over the interrupts to the sercom port
-void SERCOM2_Handler()
-{
-    Serial3.IrqHandler();
-}
-#endif
-
-#endif  // End hardware serial on SAMD21 boards
 
 
 // ==========================================================================
@@ -212,10 +103,7 @@ const int8_t modemResetPin = A4;    // MCU pin connected to modem reset pin (-1 
 #include <TinyGsmClient.h>
 
 // Create a reference to the serial port for the modem
-// Extra hardware and software serial ports are created in the "Settings for Additional Serial Ports" section
 HardwareSerial &modemSerial = Serial1;  // Use hardware serial if possible
-// AltSoftSerial &modemSerial = altSoftSerial;  // For software serial if needed
-// NeoSWSerial &modemSerial = neoSSerial1;  // For software serial if needed
 
 // Create a new TinyGSM modem to run on that serial port and return a pointer to it
 #if defined TINY_GSM_MODEM_XBEE
@@ -224,17 +112,6 @@ TinyGsm *tinyModem = new TinyGsm(modemSerial, modemResetPin);
 TinyGsm *tinyModem = new TinyGsm(modemSerial);
 #endif
 
-// Use this to create a modem if you want to spy on modem communication through
-// a secondary Arduino stream.  Make sure you install the StreamDebugger library!
-// https://github.com/vshymanskyy/StreamDebugger
-// Also make sure you comment out the modem creation above to use this.
-// #include <StreamDebugger.h>
-// StreamDebugger modemDebugger(modemSerial, Serial);
-// #if defined TINY_GSM_MODEM_XBEE
-// TinyGsm *tinyModem = new TinyGsm(modemDebugger, modemResetPin);
-// #else
-// TinyGsm *tinyModem = new TinyGsm(modemDebugger);
-// #endif
 
 // Create a new TCP client on that modem and return a pointer to it
 TinyGsmClient *tinyClient = new TinyGsmClient(*tinyModem);
@@ -710,12 +587,6 @@ uint8_t BMEi2c_addr = 0x76;
 // Create a Bosch BME280 sensor object
 BoschBME280 bme280(I2CPower, BMEi2c_addr);
 
-// Create four variable pointers for the BME280
-// Variable *bme280Humid = new BoschBME280_Humidity(&bme280, "12345678-abcd-1234-efgh-1234567890ab");
-// Variable *bme280Temp = new BoschBME280_Temp(&bme280, "12345678-abcd-1234-efgh-1234567890ab");
-// Variable *bme280Press = new BoschBME280_Pressure(&bme280, "12345678-abcd-1234-efgh-1234567890ab");
-// Variable *bme280Alt = new BoschBME280_Altitude(&bme280, "12345678-abcd-1234-efgh-1234567890ab");
-
 
 // ==========================================================================
 //    Maxim DS18 One Wire Temperature Sensor
@@ -734,18 +605,12 @@ const int8_t OneWireBus = 6;  // Pin attached to the OneWire Bus (-1 if unconnec
 // Create a Maxim DS18 sensor object (use this form for a single sensor on bus with an unknown address)
 MaximDS18 ds18(OneWirePower, OneWireBus);
 
-// Create a temperature variable pointer for the DS18
-// Variable *ds18Temp = new MaximDS18_Temp(&ds18, "12345678-abcd-1234-efgh-1234567890ab");
-
 
 // ==========================================================================
 //    Creating the Variable Array[s] and Filling with Variable Objects
 // ==========================================================================
 #include <VariableArray.h>
 
-// FORM1: Create pointers for all of the variables from the sensors,
-// at the same time putting them into an array
-// NOTE:  Forms one and two can be mixed
 Variable *variableList[] = {
     new ProcessorStats_SampleNumber(&mcuBoard, "12345678-abcd-1234-efgh-1234567890ab"),
     new BoschBME280_Temp(&bme280, "12345678-abcd-1234-efgh-1234567890ab"),
@@ -757,20 +622,6 @@ Variable *variableList[] = {
     new Modem_RSSI(&modem, "12345678-abcd-1234-efgh-1234567890ab"),
     new Modem_SignalPercent(&modem, "12345678-abcd-1234-efgh-1234567890ab"),
 };
-
-/*
-// FORM2: Fill array with already created and named variable pointers
-// NOTE:  Forms one and two can be mixed
-Variable *variableList[] = {
-    mcuBoardBatt,
-    mcuBoardAvailableRAM,
-    mcuBoardSampNo,
-    modemRSSI,
-    modemSignalPct,
-    // etc, etc, etc,
-    calculatedVar
-}
-*/
 
 
 // Count up the number of pointers in the array
@@ -870,24 +721,6 @@ void setup()
     // Start the serial connection with the modem
     modemSerial.begin(modemBaud);
 
-    // // Start the stream for the modbus sensors; all currently supported modbus sensors use 9600 baud
-    // modbusSerial.begin(9600);
-    //
-    // // Start the SoftwareSerial stream for the sonar; it will always be at 9600 baud
-    // sonarSerial.begin(9600);
-
-    // Assign pins SERCOM functionality for SAMD boards
-    // NOTE:  This must happen *after* the various serial.begin statements
-    #if defined ARDUINO_ARCH_SAMD
-    #ifndef ENABLE_SERIAL2
-    pinPeripheral(10, PIO_SERCOM);  // Serial2 Tx/Dout = SERCOM1 Pad #2
-    pinPeripheral(11, PIO_SERCOM);  // Serial2 Rx/Din = SERCOM1 Pad #0
-    #endif
-    #ifndef ENABLE_SERIAL3
-    pinPeripheral(2, PIO_SERCOM);  // Serial3 Tx/Dout = SERCOM2 Pad #2
-    pinPeripheral(5, PIO_SERCOM);  // Serial3 Rx/Din = SERCOM2 Pad #3
-    #endif
-    #endif
 
     // Set up pins for the LED's
     pinMode(greenLED, OUTPUT);
@@ -1024,90 +857,3 @@ void loop()
         dataLogger.logDataAndSend();
     }
 }
-// */
-
-
-// Use this long loop when you want to do something special
-// Because of the way alarms work on the RTC, it will wake the processor and
-// start the loop every minute exactly on the minute.
-// The processor may also be woken up by another interrupt or level change on a
-// pin - from a button or some other input.
-// The "if" statements in the loop determine what will happen - whether the
-// sensors update, testing mode starts, or it goes back to sleep.
-/*
-void loop()
-{
-    // Assuming we were woken up by the clock, check if the current time is an
-    // even interval of the logging interval
-    // We're only doing anything at all if the battery is above 3.4V
-    if (dataLogger.checkInterval() && getBatteryVoltage() > 3.4)
-    {
-        // Flag to notify that we're in already awake and logging a point
-        Logger::isLoggingNow = true;
-
-        // Print a line to show new reading
-        Serial.println(F("------------------------------------------"));
-        // Turn on the LED to show we're taking a reading
-        dataLogger.alertOn();
-        // Power up the SD Card, but skip any waits after power up
-        dataLogger.turnOnSDcard(false);
-
-        // Turn on the modem to let it start searching for the network
-        // Only turn the modem on if the battery at the last interval was high enough
-        // NOTE:  if the modemPowerUp function is not run before the completeUpdate
-        // function is run, the modem will not be powered and will not return
-        // a signal strength readign.
-        if (getBatteryVoltage() > 3.7)
-            modem.modemPowerUp();
-
-        // Do a complete update on the variable array.
-        // This this includes powering all of the sensors, getting updated
-        // values, and turing them back off.
-        // NOTE:  The wake function for each sensor should force sensor setup
-        // to run if the sensor was not previously set up.
-        varArray.completeUpdate();
-
-        // Create a csv data record and save it to the log file
-        dataLogger.logToSD();
-
-        // Connect to the network
-        // Again, we're only doing this if the battery is doing well
-        if (getBatteryVoltage() > 3.7)
-        {
-            if (modem.connectInternet())
-            {
-                // Publish data to remotes
-                dataLogger.sendDataToRemotes();
-
-                // Sync the clock at midnight
-                if (Logger::markedEpochTime != 0 && Logger::markedEpochTime % 86400 == 0)
-                {
-                    Serial.println(F("Running a daily clock sync..."));
-                    dataLogger.setRTClock(modem.getNISTTime());
-                }
-
-                // Disconnect from the network
-                modem.disconnectInternet();
-            }
-            // Turn the modem off
-            modem.modemSleepPowerDown();
-        }
-
-        // Cut power from the SD card - without additional housekeeping wait
-        dataLogger.turnOffSDcard(false);
-        // Turn off the LED
-        dataLogger.alertOff();
-        // Print a line to show reading ended
-        Serial.println(F("------------------------------------------\n"));
-
-        // Unset flag
-        Logger::isLoggingNow = false;
-    }
-
-    // Check if it was instead the testing interrupt that woke us up
-    if (Logger::startTesting) dataLogger.testingMode();
-
-    // Call the processor sleep
-    dataLogger.systemSleep();
-}
-*/
