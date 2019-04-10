31682bcbce68e38b6c8bcf6cf0c8cb77d84d04b3
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2019-04-10 11:41:36 -0500

    Y4000 stabilization time fix to 60000
    As described in https://github.com/EnviroDIY/ModularSensors/commit/fdef4f068560c1fd75a3c39d70e7564f8eb2312d#commitcomment-33125758, I think this might have caused an issue with stabilization time. Also, a regular integer (4bit) can handle the value of 60000.

--- src/sensors/YosemitechY4000.h
+++ src/sensors/YosemitechY4000.h
@@ -72,7 +72,7 @@
 #define Y4000_NUM_VARIABLES 8
 #define Y4000_WARM_UP_TIME_MS 1600
 // Time for communication to begin
-#define Y4000_STABILIZATION_TIME_MS 60000L
+#define Y4000_STABILIZATION_TIME_MS 60000
 // Y4000 Modbus manual says 60s; we find Cond takes about that long to respond.
 #define Y4000_MEASUREMENT_TIME_MS 3000
 
