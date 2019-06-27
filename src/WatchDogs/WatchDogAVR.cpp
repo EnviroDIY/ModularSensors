520172de0f1773f8ab7a70390e702b5869cea6b9
Sara Damiano <sdamiano@stroudcenter.org>
2019-06-27 17:28:01 -0400

    Using longest interrupt possible, not 1s

--- src/WatchDogs/WatchDogAVR.cpp
+++ src/WatchDogs/WatchDogAVR.cpp
@@ -30,7 +30,7 @@ extendedWatchDogAVR::~extendedWatchDogAVR()
 void extendedWatchDogAVR::setupWatchDog(uint32_t resetTime_s)
 {
     _resetTime_s = resetTime_s;
-    extendedWatchDog::_barksUntilReset = _resetTime_s;
+    extendedWatchDog::_barksUntilReset = _resetTime_s/8;
     MS_DBG(F("Watch-dog timeout is set for"),
            _resetTime_s,
            F("with the interrupt firing"),
@@ -42,7 +42,7 @@ void extendedWatchDogAVR::setupWatchDog(uint32_t resetTime_s)
 void extendedWatchDogAVR::enableWatchDog()
 {
     MS_DBG(F("Enabling watch dog..."),
-    
+
     cli();                              // disable interrupts
 
     MCUSR = 0;                          // reset status register flags
@@ -50,9 +50,9 @@ void extendedWatchDogAVR::enableWatchDog()
                                      // Put timer in interrupt-only mode:
     WDTCSR |= 0b00011000;               // Set WDCE (5th from left) and WDE (4th from left) to enter config mode,
                                      // using bitwise OR assignment (leaves other bits unchanged).
-    WDTCSR =  0b01000000 | 0b000110;    // set WDIE (interrupt enable...7th from left, on left side of bar)
+    WDTCSR =  0b01000000 | 0b100001;    // set WDIE (interrupt enable...7th from left, on left side of bar)
                                      // clr WDE (reset enable...4th from left)
-                                     // and set delay interval (right side of bar) to 1 second,
+                                     // and set delay interval (right side of bar) to 8 seconds,
                                      // using bitwise OR operator.
 
     sei();                              // re-enable interrupts
@@ -66,7 +66,7 @@ void extendedWatchDogAVR::enableWatchDog()
     //  4 seconds: 0b100000
     //  8 seconds: 0b100001
 
-    extendedWatchDog::_barksUntilReset = _resetTime_s;
+    extendedWatchDog::_barksUntilReset = _resetTime_s/8;
     MS_DBG(F("The watch dog enabled in interrupt-only mode.  The interrupt will fire"),
            extendedWatchDog::_barksUntilReset,
            F("times before the system resets."));
@@ -82,7 +82,7 @@ void extendedWatchDogAVR::disableWatchDog()
 
 void extendedWatchDogAVR::resetWatchDog()
 {
-    extendedWatchDog::_barksUntilReset = _resetTime_s;
+    extendedWatchDog::_barksUntilReset = _resetTime_s/8;
     // Reset the watchdog.
     wdt_reset();
 }
--- src/WatchDogs/WatchDogSAMD.cpp
+++ src/WatchDogs/WatchDogSAMD.cpp
@@ -22,10 +22,11 @@ extendedWatchDogSAMD::~extendedWatchDogSAMD()
 }
 
 // One-time initialization of watchdog timer.
-void extendedWatchDogSAMD::setupWatchDog(uint32_t resetTime_s);
+void extendedWatchDogSAMD::setupWatchDog(uint32_t resetTime_s)
 {
     _resetTime_s = resetTime_s;
-    extendedWatchDog::_barksUntilReset = _resetTime_s*2;  // warning is 1/2 second early
+    // Longest interrupt is 16s, so we loop that as many times as needed
+    extendedWatchDog::_barksUntilReset = _resetTime_s/16;
 
     MS_DBG(F("Setting up watch-dog timeout for"),
            _resetTime_s,
@@ -39,12 +40,6 @@ void extendedWatchDogSAMD::setupWatchDog(uint32_t resetTime_s);
     OSC32KCTRL->OSCULP32K.bit.EN1K  = 1; // Enable out 1K (for WDT)
     OSC32KCTRL->OSCULP32K.bit.EN32K = 0; // Disable out 32K
 
-    // Enable WDT early-warning interrupt
-    NVIC_DisableIRQ(WDT_IRQn);
-    NVIC_ClearPendingIRQ(WDT_IRQn);
-    NVIC_SetPriority(WDT_IRQn, 0); // Top priority
-    NVIC_EnableIRQ(WDT_IRQn);
-
     while(WDT->SYNCBUSY.reg);
 
     USB->DEVICE.CTRLA.bit.ENABLE = 0;         // Disable the USB peripheral
@@ -55,32 +50,32 @@ void extendedWatchDogSAMD::setupWatchDog(uint32_t resetTime_s);
 
 #else  // SAMD21
 
-    // Generic clock generator 2, divisor = 32 (2^(DIV+1))  = _x
-    GCLK->GENDIV.reg = GCLK_GENDIV_ID(2) | GCLK_GENDIV_DIV(_x);
+    // Generic clock generator 2, divisor = 32 (2^(DIV+1))  = 4
+    GCLK->GENDIV.reg = GCLK_GENDIV_ID(2) | GCLK_GENDIV_DIV(4);
     // Enable clock generator 2 using low-power 32.768kHz oscillator.
     // With /32 divisor above, this yields 1024Hz clock.
     GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(2) |
-                      GCLK_GENCTRL_GENEN |
-                      GCLK_GENCTRL_SRC_OSCULP32K |
-                      GCLK_GENCTRL_DIVSEL;
+                        GCLK_GENCTRL_GENEN |
+                        GCLK_GENCTRL_SRC_OSCULP32K |
+                        GCLK_GENCTRL_DIVSEL;
     while(GCLK->STATUS.bit.SYNCBUSY);
     // WDT clock = clock gen 2
     GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_WDT |
                         GCLK_CLKCTRL_CLKEN |
                         GCLK_CLKCTRL_GEN_GCLK2;
-
-    // Enable WDT early-warning interrupt
-    NVIC_DisableIRQ(WDT_IRQn);
-    NVIC_ClearPendingIRQ(WDT_IRQn);
-    NVIC_SetPriority(WDT_IRQn, 0); // Top priority
-    NVIC_EnableIRQ(WDT_IRQn);
 #endif
 }
 
 
 void extendedWatchDogSAMD::enableWatchDog()
 {
-    MS_DBG(F("Enabling watch dog..."),
+    MS_DBG(F("Enabling watch dog..."));
+
+    // Enable WDT early-warning interrupt
+    NVIC_DisableIRQ(WDT_IRQn);
+    NVIC_ClearPendingIRQ(WDT_IRQn);
+    NVIC_SetPriority(WDT_IRQn, 0); // Top priority
+    NVIC_EnableIRQ(WDT_IRQn);
 
 #if defined(__SAMD51__)
     WDT->CTRLA.reg = 0; // Disable watchdog for config
@@ -94,11 +89,10 @@ void extendedWatchDogSAMD::enableWatchDog()
     WDT->INTFLAG.bit.EW      = 1;     // Clear interrupt flag
 #endif
 
-    // We're always going to set up a 1-second watch dog with a half second early interrupt
-    WDT->INTENSET.bit.EW     = 1;     // Enable early warning interrupt
-    WDT->CONFIG.bit.PER      = 0x7;   // Period = 1024 clockcycles @ 1024hz = 1 seconds
-    WDT->EWCTRL.bit.EWOFFSET = 0x6;   // Early Warning Interrupt Time Offset 0x6 - 512 clockcycles = 0.5 seconds => trigger ISR
     WDT->CTRL.bit.WEN        = 0;     // Disable window mode
+    WDT->CONFIG.bit.PER      = 0xB;   // Period = 16384 clockcycles @ 1024hz = 16 seconds
+    WDT->EWCTRL.bit.EWOFFSET = 0x0;   // Early Warning Interrupt Time Offset 0x0 - 8 clockcycles = 7ms => trigger ISR
+    WDT->INTENSET.bit.EW     = 1;     // Enable early warning interrupt
 
 #if defined(__SAMD51__)
     while(WDT->SYNCBUSY.reg);
@@ -107,16 +101,16 @@ void extendedWatchDogSAMD::enableWatchDog()
 #endif
 
     resetWatchDog();
-    WDT->CTRL.bit.ENABLE     = 1;     // Start watchdog now!
-
 #if defined(__SAMD51__)
+    WDT->CTRLA.bit.ENABLE = 1;
     while(WDT->SYNCBUSY.reg);
 #else
+    WDT->CTRL.bit.ENABLE = 1;
     while(WDT->STATUS.bit.SYNCBUSY);
 #endif
 
-MS_DBG(F("Watch dog is enabled in normal mode at 1 second with a interrupt 0.5 seconds before reset."),
-       F("The interrupt will fire"),
+MS_DBG(F("Watch dog is enabled in normal mode at 16s with a interrupt 7ms before reset."),
+       F("The interrupt should fire"),
        extendedWatchDog::_barksUntilReset,
        F("times before the system resets."));
 }
@@ -138,7 +132,7 @@ void extendedWatchDogSAMD::disableWatchDog()
 
 void extendedWatchDogSAMD::resetWatchDog()
 {
-    extendedWatchDog::_barksUntilReset = _resetTime_s*2;  // warning is 1/2 second early
+    extendedWatchDog::_barksUntilReset = _resetTime_s/16;
     // Write the watchdog clear key value (0xA5) to the watchdog
     // clear register to clear the watchdog timer and reset it.
     WDT->CLEAR.reg = WDT_CLEAR_CLEAR_KEY;
