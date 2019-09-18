0e1d43b7f400a8c576340fbced0d030d9335f391
neil hancock <neilh20+github140819@biomoniotrs.com>
2019-09-18 12:56:19 -0700

    UF2 testing:

--- a/tu_mon/extra_script_uf2.py
+++ a/tu_mon/extra_script_uf2.py
@@ -1,4 +1,17 @@
-Import("env")
-env.AlwaysBuild(env.Alias("uf2conv",
-    "$BUILD_DIR/${PROGNAME}.elf",
-    ["uf2conv.py  $BUILD_DIR/${PROGNAME}.elf $BUILD_DIR/${PROGNAME}.elf"]))
+Import("env","projenv")
+# access to global construction environment
+#print(env)
+
+#print(projenv)
+# Dump construction environment (for debug purpose)
+#print(env.Dump())
+#print(projenv.Dump())
+
+# UF2 from .bin
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.bin",
+    env.VerboseAction(" ".join([
+        "uf2conv.py", "-C", "-O", "-R", 
+        "$BUILD_DIR/${PROGNAME}.uf2", "$BUILD_DIR/${PROGNAME}.bin"
+    ]), "Building $BUILD_DIR/${PROGNAME}.bin")
+)
