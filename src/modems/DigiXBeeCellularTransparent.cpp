2726701d2c6a5d0344dfa686ebcb69b6be7db64e
Sara Damiano <sdamiano@stroudcenter.org>
2019-09-19 14:31:09 -0400

    With latest firmware, faster to use LTE-M/NB-IoT rather than LTE-M only

--- src/modems/DigiXBeeCellularTransparent.cpp
+++ src/modems/DigiXBeeCellularTransparent.cpp
@@ -106,9 +106,8 @@ bool DigiXBeeCellularTransparent::extraModemSetup(void)
         // Hologram says they can use any network, but I've never succeeded with anything but AT&T
         gsmModem.sendAT(GF("CP"),2);
         gsmModem.waitResponse();  // Don't check for success - only works on LTE
-        // Cellular network technology - LTE-M Only
-        // LTE-M XBee connects much faster on AT&T/Hologram when set to LTE-M only (instead of LTE-M/NB IoT)
-        gsmModem.sendAT(GF("N#"),2);
+        // Cellular network technology - LTE-M/NB IoT
+        gsmModem.sendAT(GF("N#"),0);
         gsmModem.waitResponse();  // Don't check for success - only works on LTE
         // Put the network connection parameters into flash
         success &= gsmModem.gprsConnect(_apn);
--- src/modems/DigiXBeeLTEBypass.cpp
+++ src/modems/DigiXBeeLTEBypass.cpp
@@ -96,9 +96,8 @@ bool DigiXBeeLTEBypass::extraModemSetup(void)
         // Hologram says they can use any network, but I've never succeeded with anything but AT&T
         gsmModem.sendAT(GF("CP"),2);
         success &= gsmModem.waitResponse(GF("OK\r")) == 1;
-        // Cellular network technology - LTE-M Only
-        // LTE-M XBee connects much faster on AT&T/Hologram when set to LTE-M only (instead of LTE-M/NB IoT)
-        gsmModem.sendAT(GF("N#"),2);
+        // Cellular network technology - LTE-M/NB IoT
+        gsmModem.sendAT(GF("N#"),0);
         success &= gsmModem.waitResponse(GF("OK\r")) == 1;
         // Make sure airplane mode is off - bypass and airplane mode are incompatible
         MS_DBG(F("Making sure airplane mode is off..."));
