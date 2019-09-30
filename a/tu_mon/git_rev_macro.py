fc0c9dd841dc4f48c17682471c04802c37a7f3b0
neil hancock <neilh20+github140819@biomoniotrs.com>
2019-09-30 14:55:32 -0700

    renamed directory

--- a/tu_mon/git_rev_macro.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-git_rev = str(subprocess.check_output(["git", "rev-parse","--abbrev-ref", "HEAD"]).strip())
-
-print('-DPIO_SRC_REV={'+(''.join('0x%02x,' % ord(c) for c in git_rev) ) +'0x0}')
