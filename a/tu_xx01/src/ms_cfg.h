19a963b092321d60803c48d6f0acb32fdffad539
neilh20.gitk <neilh20+gitk1903@wLLw.net>
2020-07-06 17:25:40 -0700

    testing a/tu_xx01/src/ LTE

--- a/tu_xx01/src/ms_cfg.h
+++ a/tu_xx01/src/ms_cfg.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
-ms_cfg.h_EC  - ModularSensors Configuration - tgt _CTD10 _LT5 _EC
+ms_cfg.h_LT5  - ModularSensors Configuration - tgt _CTD10 _LT5 _EC
 Written By:  Neil Hancock www.envirodiy.org/members/neilh20/
 Development Environment: PlatformIO
 Hardware Platform(s): EnviroDIY Mayfly Arduino Datalogger+RS485 Wingboard
@@ -21,12 +21,12 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 //**************************************************************************
 //This configuration is for a standard Mayfly0.bb
 //Sensors Used - two std to begin then
-#define AnalogProcEC_ACT 1
-#define ENVIRODIY_MAYFLY_TEMPERATURE 1
+//#define AnalogProcEC_ACT 1
+//#define ENVIRODIY_MAYFLY_TEMPERATURE 1
 //#define Decagon_CTD_UUID 1
-//#define Insitu_TrollSdi12_UUID 1
+#define Insitu_TrollSdi12_UUID 1
 //Only one of NOT both KellerAcculevel and KellerNanolevel as share same ADDR
-//#define KellerAcculevel_ACT 1
+#define KellerAcculevel_ACT 1
 //#define KellerNanolevel_ACT 1
 //#define ASONG_AM23XX_UUID 1
 
@@ -75,9 +75,9 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 
 
 //Instructions: define only one  _Module
-#define DigiXBeeWifi_Module 1
+//#define DigiXBeeWifi_Module 1
 //#warning infoMayflyWithDigiXBeeWiFi
-//#define DigiXBeeCellularTransparent_Module 1
+#define DigiXBeeCellularTransparent_Module 1
 //#warning infoMayflyWithDigiXBeeCellTransparent
 // #define DigiXBeeLTE_Module 1 - unstable
 // #define TINY_GSM_MODEM_SIM800  // Select for a SIM800, SIM900, or variant thereof
@@ -89,7 +89,7 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 // In standalong logger, no internet, Modem can be required at factor to do a sync Time
 // Normally enable both of the following. In standalone, disable UseModem_PushData.
 #define UseModem_Module 1
-//#define UseModem_PushData 1
+#define UseModem_PushData 1
 //Required for TinyGsmClient.h 
 #define TINY_GSM_MODEM_XBEE
 
@@ -191,7 +191,7 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 #endif //ENVIRODIY_MAYFLY_TEMPERATURE
 
 #if defined UseModem_Module 
-//#define DIGI_RSSI_UUID "DIGI_RSSI_UUID"
+#define DIGI_RSSI_UUID "DIGI_RSSI_UUID"
 //#define Modem_SignalPercent_UUID    "SignalPercent_UUID"
 #endif //UseModem_Module 
 
