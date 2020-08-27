a429be04e885a5f59f21a7e74bfbeecd0d21effc
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2020-08-26 22:09:47 -0500

    Update Ubidots styling & docs for v0.26
    @gcutrell, this should be functionally identical to the last commit, but is updated to the styling and doxygen conventions of >0.25. Please test it, as this will be the version in the next release.

--- src/publishers/UbidotsPublisher.cpp
+++ src/publishers/UbidotsPublisher.cpp
@@ -1,12 +1,12 @@
-/*
- *EnviroDIYPublisher.cpp
- *This file is part of the EnviroDIY modular sensors library for Arduino
+/**
+* @file UbidotsPublisher.cpp
+* @copyright 2020 Stroud Water Research Center
+* Part of the EnviroDIY ModularSensors library for Arduino
+* @author Greg Cutrell <gcutrell@limno.com>
+* @author Sara Geleskie Damiano <sdamiano@stroudcenter.org>
  *
- *Initial library developement done by Sara Damiano (sdamiano@stroudcenter.org).
- *
- *This file is for the EnviroDIY logging functions - ie, sending JSON data to
- * http://data.enviroDIY.org
-*/
+ * @brief Implements the UbidotsPublisher class.
+ */
 
 #include "UbidotsPublisher.h"
 
@@ -16,53 +16,50 @@
 // ============================================================================
 
 // Constant values for post requests
-// I want to refer to these more than once while ensuring there is only one copy in memory
-const char *UbidotsPublisher::postEndpoint = "/api/v1.6/devices/";
-const char *UbidotsPublisher::ubidotsHost = "industrial.api.ubidots.com";
-const int UbidotsPublisher::ubidotsPort = 80;
-const char *UbidotsPublisher::tokenHeader = "\r\nX-Auth-Token: ";
-// const unsigned char *EnviroDIYPublisher::cacheHeader = "\r\nCache-Control: no-cache";
-// const unsigned char *EnviroDIYPublisher::connectionHeader = "\r\nConnection: close";
-const char *UbidotsPublisher::contentLengthHeader = "\r\nContent-Length: ";
-const char *UbidotsPublisher::contentTypeHeader = "\r\nContent-Type: application/json\r\n\r\n";
+// I want to refer to these more than once while ensuring there is only one copy
+// in memory
+const char* UbidotsPublisher::postEndpoint = "/api/v1.6/devices/";
+const char* UbidotsPublisher::ubidotsHost  = "industrial.api.ubidots.com";
+const int   UbidotsPublisher::ubidotsPort  = 80;
+const char* UbidotsPublisher::tokenHeader  = "\r\nX-Auth-Token: ";
+//
+//
+//
+const char* UbidotsPublisher::contentLengthHeader = "\r\nContent-Length: ";
+const char* UbidotsPublisher::contentTypeHeader   =
+                              "\r\nContent-Type: application/json\r\n\r\n";
 
-const char *UbidotsPublisher::payload = "{";
+const char* UbidotsPublisher::payload = "{";
 
 
 // Constructors
-UbidotsPublisher::UbidotsPublisher()
-  : dataPublisher()
-{
+UbidotsPublisher::UbidotsPublisher()  : dataPublisher() {
     // MS_DBG(F("dataPublisher object created"));
 }
-UbidotsPublisher::UbidotsPublisher(Logger& baseLogger,
-                                 uint8_t sendEveryX, uint8_t sendOffset)
-  : dataPublisher(baseLogger, sendEveryX, sendOffset)
-{
+UbidotsPublisher::UbidotsPublisher(Logger& baseLogger, uint8_t sendEveryX,
+                                   uint8_t sendOffset)
+  : dataPublisher(baseLogger, sendEveryX, sendOffset) {
     // MS_DBG(F("dataPublisher object created"));
 }
-UbidotsPublisher::UbidotsPublisher(Logger& baseLogger, Client *inClient,
-                                 uint8_t sendEveryX, uint8_t sendOffset)
-  : dataPublisher(baseLogger, inClient, sendEveryX, sendOffset)
-{
+UbidotsPublisher::UbidotsPublisher(Logger& baseLogger, Client* inClient,
+                                   uint8_t sendEveryX, uint8_t sendOffset)
+  : dataPublisher(baseLogger, inClient, sendEveryX, sendOffset) {
     // MS_DBG(F("dataPublisher object created"));
 }
-UbidotsPublisher::UbidotsPublisher(Logger& baseLogger,
-                                 const char *authentificationToken,
-                                 const char *deviceID,
-                                 uint8_t sendEveryX, uint8_t sendOffset)
-  : dataPublisher(baseLogger, sendEveryX, sendOffset)
-{
+UbidotsPublisher::UbidotsPublisher(Logger&     baseLogger,
+                                   const char* authentificationToken,
+                                   const char* deviceID,
+                                   uint8_t sendEveryX, uint8_t sendOffset)
+  : dataPublisher(baseLogger, sendEveryX, sendOffset) {
     setToken(authentificationToken);
     _baseLogger->setSamplingFeatureUUID(deviceID);
     MS_DBG(F("dataPublisher object created"));
 }
-UbidotsPublisher::UbidotsPublisher(Logger& baseLogger, Client *inClient,
-                                 const char *authentificationToken,
-                                 const char *deviceID,
-                                 uint8_t sendEveryX, uint8_t sendOffset)
-  : dataPublisher(baseLogger, inClient, sendEveryX, sendOffset)
-{
+UbidotsPublisher::UbidotsPublisher(Logger& baseLogger, Client* inClient,
+                                   const char* authentificationToken,
+                                   const char* deviceID,
+                                   uint8_t sendEveryX, uint8_t sendOffset)
+    : dataPublisher(baseLogger, inClient, sendEveryX, sendOffset) {
     setToken(authentificationToken);
     _baseLogger->setSamplingFeatureUUID(deviceID);
     MS_DBG(F("dataPublisher object created"));
@@ -71,67 +68,41 @@ UbidotsPublisher::UbidotsPublisher(Logger& baseLogger, Client *inClient,
 UbidotsPublisher::~UbidotsPublisher(){}
 
 
-void UbidotsPublisher::setToken(const char *authentificationToken)
-{
+void UbidotsPublisher::setToken(const char* authentificationToken) {
     _authentificationToken = authentificationToken;
     MS_DBG(F("Registration token set!"));
 }
 
 
 // Calculates how long the JSON will be
-uint16_t UbidotsPublisher::calculateJsonSize()
-{
+uint16_t UbidotsPublisher::calculateJsonSize() {
     uint16_t jsonLength = 1;  // {
-    //jsonLength += 36;  // sampling feature UUID
-    //jsonLength += 15;  // ","timestamp":"
-    //jsonLength += 25;  // markedISO8601Time
-    //jsonLength += 2;  //  ",
-    for (uint8_t i = 0; i < _baseLogger->getArrayVarCount(); i++)
-    {
+    // jsonLength += 36;          // sampling feature UUID
+    // jsonLength += 15;          // ","timestamp":"
+    // jsonLength += 25;          // markedISO8601Time
+    // jsonLength += 2;           //  ",
+    for (uint8_t i = 0; i < _baseLogger->getArrayVarCount(); i++) {
         jsonLength += 1;  //  "
         jsonLength += _baseLogger->getVarUUIDAtI(i).length();  // parameter ID length
         jsonLength += 11;  //  ":{"value":
         jsonLength += _baseLogger->getValueStringAtI(i).length();
         jsonLength += 13;   // ,"timestamp":
         jsonLength += 13;   //epoch time in milliseconds
-        if (i + 1 != _baseLogger->getArrayVarCount())
-        {
-            jsonLength += 2;  // },
+        if (i + 1 != _baseLogger->getArrayVarCount()) {
+            jsonLength += 2;  // ,
         }
     }
-    jsonLength += 2;  // }}
+    jsonLength += 2;  // }
 
     return jsonLength;
 }
 
 
-/*
-// Calculates how long the full post request will be, including headers
-uint16_t EnviroDIYPublisher::calculatePostSize()
-{
-    uint16_t postLength = 31;  // "POST /api/data-stream/ HTTP/1.1"
-    postLength += 28;  // "\r\nHost: data.envirodiy.org"
-    postLength += 11;  // "\r\nTOKEN: "
-    postLength += 36;  // registrationToken
-    // postLength += 27;  // "\r\nCache-Control: no-cache"
-    // postLength += 21;  // "\r\nConnection: close"
-    postLength += 20;  // "\r\nContent-Length: "
-    postLength += String(calculateJsonSize()).length();
-    postLength += 42;  // "\r\nContent-Type: application/json\r\n\r\n"
-    postLength += calculateJsonSize();
-    return postLength;
-}
-*/
-
-
 // This prints a properly formatted JSON for EnviroDIY to an Arduino stream
-void UbidotsPublisher::printSensorDataJSON(Stream *stream)
-{
-    //stream->print(_baseLogger->getSamplingFeatureUUID());
+void UbidotsPublisher::printSensorDataJSON(Stream* stream) {
     stream->print(payload);
 
-    for (uint8_t i = 0; i < _baseLogger->getArrayVarCount(); i++)
-    {
+    for (uint8_t i = 0; i < _baseLogger->getArrayVarCount(); i++) {
         stream->print('"');
         stream->print(_baseLogger->getVarUUIDAtI(i));
         stream->print(F("\":{'value':"));
@@ -139,10 +110,7 @@ void UbidotsPublisher::printSensorDataJSON(Stream *stream)
         stream->print(",'timestamp':");
         stream->print(Logger::markedEpochTimeUTC);
         stream->print(F("000}"));  //Convert microseconds to milliseconds for ubidots
-        if (i + 1 != _baseLogger->getArrayVarCount())
-        {
-            stream->print(',');
-        }
+        if (i + 1 != _baseLogger->getArrayVarCount()) { stream->print(','); }
     }
 
     stream->print(F("}}"));
@@ -151,8 +119,7 @@ void UbidotsPublisher::printSensorDataJSON(Stream *stream)
 
 // This prints a fully structured post request for Ubidots to the
 // specified stream.
-void UbidotsPublisher::printUbidotsRequest(Stream *stream)
-{
+void UbidotsPublisher::printUbidotsRequest(Stream* stream) {
     // Stream the HTTP headers for the post request
     stream->print(postHeader);
     stream->print(postEndpoint);
@@ -173,18 +140,16 @@ void UbidotsPublisher::printUbidotsRequest(Stream *stream)
 
 
 // A way to begin with everything already set
-void UbidotsPublisher::begin(Logger& baseLogger, Client *inClient,
-                               const char *authentificationToken,
-                               const char *deviceID)
-{
+void UbidotsPublisher::begin(Logger& baseLogger, Client* inClient,
+                             const char* authentificationToken,
+                             const char* deviceID) {
     setToken(authentificationToken);
     dataPublisher::begin(baseLogger, inClient);
     _baseLogger->setSamplingFeatureUUID(deviceID);
 }
-void UbidotsPublisher::begin(Logger& baseLogger,
-                               const char *authentificationToken,
-                               const char *deviceID)
-{
+void UbidotsPublisher::begin(Logger&     baseLogger,
+                             const char* authentificationToken,
+                             const char* deviceID) {
     setToken(authentificationToken);
     dataPublisher::begin(baseLogger);
     _baseLogger->setSamplingFeatureUUID(deviceID);
@@ -196,21 +161,18 @@ void UbidotsPublisher::begin(Logger& baseLogger,
 // over that connection.
 // The return is the http status code of the response.
 // int16_t EnviroDIYPublisher::postDataEnviroDIY(void)
-int16_t UbidotsPublisher::publishData(Client *_outClient)
-{
+int16_t UbidotsPublisher::publishData(Client* outClient) {
     // Create a buffer for the portions of the request and response
-    char tempBuffer[37] = "";
-    uint16_t did_respond = 0;
+    char     tempBuffer[37] = "";
+    uint16_t did_respond    = 0;
 
     MS_DBG(F("Outgoing JSON size:"), calculateJsonSize());
 
     // Open a TCP/IP connection to the Enviro DIY Data Portal (WebSDL)
     MS_DBG(F("Connecting client"));
     MS_START_DEBUG_TIMER;
-    if (_outClient->connect(ubidotsHost, ubidotsPort))
-    {
+    if (outClient->connect(ubidotsHost, ubidotsPort)) {
         MS_DBG(F("Client connected after"), MS_PRINT_DEBUG_TIMER, F("ms\n"));
-
 
         // copy the initial post header into the tx buffer
         strcpy(txBuffer, postHeader);
@@ -222,31 +184,30 @@ int16_t UbidotsPublisher::publishData(Client *_outClient)
         // add the rest of the HTTP POST headers to the outgoing buffer
         // before adding each line/chunk to the outgoing buffer, we make sure
         // there is space for that line, sending out buffer if not
-        if (bufferFree() < 28) printTxBuffer(_outClient);
+        if (bufferFree() < 28) printTxBuffer(outClient);
         strcat(txBuffer, hostHeader);
         strcat(txBuffer, ubidotsHost);
 
-        if (bufferFree() < 47) printTxBuffer(_outClient);
+        if (bufferFree() < 47) printTxBuffer(outClient);
         strcat(txBuffer, tokenHeader);
         strcat(txBuffer, _authentificationToken);
 
-        if (bufferFree() < 26) printTxBuffer(_outClient);
+        if (bufferFree() < 26) printTxBuffer(outClient);
         strcat(txBuffer, contentLengthHeader);
         itoa(calculateJsonSize(), tempBuffer, 10);  // BASE 10
         strcat(txBuffer, tempBuffer);
 
-        if (bufferFree() < 42) printTxBuffer(_outClient);
+        if (bufferFree() < 42) printTxBuffer(outClient);
         strcat(txBuffer, contentTypeHeader);
 
         // put the start of the JSON into the outgoing response_buffer
-        if (bufferFree() < 21) printTxBuffer(_outClient);
+        if (bufferFree() < 21) printTxBuffer(outClient);
 
         strcat(txBuffer, payload);
 
-        for (uint8_t i = 0; i < _baseLogger->getArrayVarCount(); i++)
-        {
+        for (uint8_t i = 0; i < _baseLogger->getArrayVarCount(); i++) {
             // Once the buffer fills, send it out
-            if (bufferFree() < 47) printTxBuffer(_outClient);
+            if (bufferFree() < 47) printTxBuffer(outClient);
 
             txBuffer[strlen(txBuffer)] = '"';
             _baseLogger->getVarUUIDAtI(i).toCharArray(tempBuffer, 37);
@@ -267,56 +228,48 @@ int16_t UbidotsPublisher::publishData(Client *_outClient)
             ltoa((Logger::markedEpochTimeUTC), tempBuffer, 10);  // BASE 10
             strcat(txBuffer, tempBuffer);
             strcat(txBuffer,"000");
-            if (i + 1 != _baseLogger->getArrayVarCount())
-            {
+            if (i + 1 != _baseLogger->getArrayVarCount()) {
                 txBuffer[strlen(txBuffer)] = '}';
                 txBuffer[strlen(txBuffer)] = ',';
-            }
-            else
-            {
+            } else {
                 txBuffer[strlen(txBuffer)] = '}';
                 txBuffer[strlen(txBuffer)] = '}';
             }
         }
 
         // Send out the finished request (or the last unsent section of it)
-        printTxBuffer(_outClient, true);
+        printTxBuffer(outClient, true);
 
         // Wait 10 seconds for a response from the server
         uint32_t start = millis();
-        while ((millis() - start) < 10000L && _outClient->available() < 12)
-        {delay(10);}
+        while ((millis() - start) < 10000L && outClient->available() < 12) {
+            delay(10);
+        }
 
         // Read only the first 12 characters of the response
         // We're only reading as far as the http code, anything beyond that
         // we don't care about.
-        did_respond = _outClient->readBytes(tempBuffer, 12);
+        did_respond = outClient->readBytes(tempBuffer, 12);
 
         // Close the TCP/IP connection
         MS_DBG(F("Stopping client"));
         MS_RESET_DEBUG_TIMER;
-        _outClient->stop();
+        outClient->stop();
         MS_DBG(F("Client stopped after"), MS_PRINT_DEBUG_TIMER, F("ms"));
-    }
-    else
-    {
-        PRINTOUT(F("\n -- Unable to Establish Connection to EnviroDIY Data Portal --"));
+    } else {
+        PRINTOUT(F("\n -- Unable to Establish Connection to Ubiots --"));
     }
 
     // Process the HTTP response
     int16_t responseCode = 0;
-    if (did_respond > 0)
-    {
+    if (did_respond > 0) {
         char responseCode_char[4];
-        for (uint8_t i = 0; i < 3; i++)
-        {
-            responseCode_char[i] = tempBuffer[i+9];
+        for (uint8_t i = 0; i < 3; i++) {
+            responseCode_char[i] = tempBuffer[i + 9];
         }
         responseCode = atoi(responseCode_char);
-    }
-    else
-    {
-        responseCode=504;
+    } else {
+        responseCode = 504;
     }
 
     PRINTOUT(F("-- Response Code --"));
--- src/publishers/UbidotsPublisher.h
+++ src/publishers/UbidotsPublisher.h
@@ -1,19 +1,20 @@
-/*
- *EnviroDIYPublisher.h
- *This file is part of the EnviroDIY modular sensors library for Arduino
+/**
+ * @file UbidotsPublisher.h
+ * @copyright 2020 Stroud Water Research Center
+ * Part of the EnviroDIY ModularSensors library for Arduino
+ * @author Greg Cutrell <gcutrell@limno.com>
+ * @author Sara Geleskie Damiano <sdamiano@stroudcenter.org>
  *
- *Initial library developement done by Sara Damiano (sdamiano@stroudcenter.org).
- *
- *This file is for the EnviroDIY logging functions - ie, sending JSON data to
- * http://data.enviroDIY.org
+ * @brief Contains the UbidotsPublisher subclass of dataPublisher for
+ * publishing data to the Ubidots IoT platform at https://ubidots.com
 */
 
 // Header Guards
-#ifndef UbidotsPublisher_h
-#define UbidotsPublisher_h
+#ifndef SRC_PUBLISHERS_UBIDOTSPUBLISHER_H_
+#define SRC_PUBLISHERS_UBIDOTSPUBLISHER_H_
 
 // Debugging Statement
-#define UBIDOTSPUBLISHER_DEBUG
+// #define UBIDOTSPUBLISHER_DEBUG
 
 #ifdef MS_UBIDOTSPUBLISHER_DEBUG
 #define MS_DEBUGGING_STD "UbidotsPublisher"
@@ -28,77 +29,209 @@
 // ============================================================================
 //  Functions for the EnviroDIY data portal receivers.
 // ============================================================================
-class UbidotsPublisher : public dataPublisher
-{
+/**
+ * @brief The UbidotsPublisher subclass of dataPublisher for publishing data
+ * to the Ubidots data portal at https://ubidots.com
+ */
+class UbidotsPublisher : public dataPublisher {
 public:
     // Constructors
+    /**
+     * @brief Construct a new Ubidots Publisher object with no members set.
+     */
     UbidotsPublisher();
-    UbidotsPublisher(Logger& baseLogger,
+    /**
+     * @brief Construct a new Ubidots Publisher object
+     *
+     * @note If a client is never specified, the publisher will attempt to
+     * create and use a client on a LoggerModem instance tied to the attached
+     * logger.
+     *
+     * @param baseLogger The logger supplying the data to be published
+     * @param sendEveryX Currently unimplemented, intended for future use to
+     * enable caching and bulk publishing
+     * @param sendOffset Currently unimplemented, intended for future use to
+     * enable publishing data at a time slightly delayed from when it is
+     * collected
+     *
+     * @note It is possible (though very unlikey) that using this constructor
+     * could cause errors if the compiler attempts to initialize the publisher
+     * instance before the logger instance.  If you suspect you are seeing that
+     * issue, use the null constructor and a populated begin(...) within your
+     * set-up function.
+     */
+    explicit UbidotsPublisher(Logger& baseLogger, uint8_t sendEveryX = 1,
+                     uint8_t sendOffset = 0);
+    /**
+    * @brief Construct a new Ubidots Publisher object
+    *
+    * @param baseLogger The logger supplying the data to be published
+    * @param inClient An Arduino client instance to use to print data to.
+    * Allows the use of any type of client and multiple clients tied to a
+    * single TinyGSM modem instance
+    * @param sendEveryX Currently unimplemented, intended for future use to
+    * enable caching and bulk publishing
+    * @param sendOffset Currently unimplemented, intended for future use to
+    * enable publishing data at a time slightly delayed from when it is
+    * collected
+    *
+    * @note It is possible (though very unlikey) that using this constructor
+    * could cause errors if the compiler attempts to initialize the publisher
+    * instance before the logger instance.  If you suspect you are seeing that
+    * issue, use the null constructor and a populated begin(...) within your
+    * set-up function.
+    */
+    UbidotsPublisher(Logger& baseLogger, Client* inClient,
                     uint8_t sendEveryX = 1, uint8_t sendOffset = 0);
-    UbidotsPublisher(Logger& baseLogger, Client *inClient,
-                    uint8_t sendEveryX = 1, uint8_t sendOffset = 0);
-    UbidotsPublisher(Logger& baseLogger,
-                    const char *authentificationToken,
-                    const char *deviceID,
-                    uint8_t sendEveryX = 1, uint8_t sendOffset = 0);
-    UbidotsPublisher(Logger& baseLogger, Client *inClient,
-                    const char *authentificationToken,
-                    const char *deviceID,
-                    uint8_t sendEveryX = 1, uint8_t sendOffset = 0);
-    // Destructor
+    /**
+     * @brief Construct a new Ubidots Publisher object
+     *
+     * @param baseLogger The logger supplying the data to be published
+     * @param authentificationToken The authentication token for the device
+     * that is automatically set by the Ubdots portal.
+     * @param deviceID The device code selected by the user to be recognizable.
+     * @param sendEveryX Currently unimplemented, intended for future use to
+     * enable caching and bulk publishing
+     * @param sendOffset Currently unimplemented, intended for future use to
+     * enable publishing data at a time slightly delayed from when it is
+     * collected
+     */
+    UbidotsPublisher(Logger& baseLogger, const char* authentificationToken,
+                    const char* deviceID,uint8_t sendEveryX = 1,
+                    uint8_t sendOffset = 0);
+    /**
+     * @brief Construct a new Ubidots Publisher object
+     *
+     * @param baseLogger The logger supplying the data to be published
+     * @param inClient An Arduino client instance to use to print data to.
+     * Allows the use of any type of client and multiple clients tied to a
+     * single TinyGSM modem instance
+     * @param authentificationToken The authentication token for the device
+     * that is automatically set by the Ubdots portal.
+     * @param deviceID The device code selected by the user to be recognizable.
+     * @param sendEveryX Currently unimplemented, intended for future use to
+     * enable caching and bulk publishing
+     * @param sendOffset Currently unimplemented, intended for future use to
+     * enable publishing data at a time slightly delayed from when it is
+     * collected
+     */
+    UbidotsPublisher(Logger& baseLogger, Client* inClient,
+                    const char* authentificationToken,
+                    const char* deviceID, uint8_t sendEveryX = 1,
+                    uint8_t sendOffset = 0);
+    /**
+     * @brief Destroy the EnviroDIY Publisher object
+     */
     virtual ~UbidotsPublisher();
 
     // Returns the data destination
-    virtual String getEndpoint(void){return String(ubidotsHost);}
+    String getEndpoint(void) override {
+        return String(ubidotsHost);
+    }
 
-    // Adds the site registration token
-    void setToken(const char *authentificationToken);
+    // Adds the device authentication token
+    /**
+     * @brief Set the device authentication token
+     *
+     * @param authentificationToken The authentication token for the device
+     * that is automatically set by the Ubdots portal.
+     */
+    void setToken(const char* authentificationToken);
 
-    // Calculates how long the JSON will be
+    /**
+     * @brief Calculates how long the outgoing JSON will be
+     *
+     * @return uint16_t The number of characters in the JSON object.
+     */
     uint16_t calculateJsonSize();
-    // Calculates how long the full post request will be, including headers
+    // /**
+    //  * @brief Calculates how long the full post request will be, including
+    //  * headers
+    //  *
+    //  * @return uint16_t The length of the full request including HTTP
+    //  headers.
+    //  */
     // uint16_t calculatePostSize();
 
-    // This generates a properly formatted JSON for EnviroDIY
-    void printSensorDataJSON(Stream *stream);
+    /**
+     * @brief This generates a properly formatted JSON for Ubidots and prints
+     * it to the input Arduino stream object.
+     *
+     * @param stream The Arduino stream to write out the JSON to.
+     */
+    void printSensorDataJSON(Stream* stream);
 
-    // This prints a fully structured post request for WikiWatershed/EnviroDIY
-    // to the specified stream.
-    void printUbidotsRequest(Stream *stream);
+    /**
+     * @brief This prints a fully structured post request for the Ubidots API
+     * to the specified stream.
+     *
+     * @param stream The Arduino stream to write out the request to.
+     */
+    void printUbidotsRequest(Stream* stream);
 
     // A way to begin with everything already set
-    void begin(Logger& baseLogger, Client *inClient,
-               const char *authentificationToken,
-               const char *deviceID);
-    void begin(Logger& baseLogger,
-              const char *authentificationToken,
-              const char *deviceID);
+    /**
+     * @copydoc dataPublisher::begin(Logger& baseLogger, Client* inClient)
+     * @param authentificationToken The authentication token for the device
+     * that is automatically set by the Ubdots portal.
+     * @param deviceID The device code selected by the user to be recognizable.
+     */
+    void begin(Logger& baseLogger, Client* inClient,
+               const char* authentificationToken, const char* deviceID);
+    /**
+    * @copydoc dataPublisher::begin(Logger& baseLogger)
+    * @param authentificationToken The authentication token for the device
+    * that is automatically set by the Ubdots portal.
+    * @param deviceID The device code selected by the user to be recognizable.
+    */
+    void begin(Logger& baseLogger, const char* authentificationToken,
+              const char* deviceID);
 
-    // This utilizes an attached modem to make a TCP connection to the
-    // EnviroDIY/ODM2DataSharingPortal and then streams out a post request
-    // over that connection.
-    // The return is the http status code of the response.
-    // int16_t postDataEnviroDIY(void);
-    virtual int16_t publishData(Client *_outClient);
+    // Post Data to Ubidots
+    /**
+     * @brief Utilize an attached modem to open a a TCP connection to the
+     * Ubidots API and then stream out a post request over
+     * that connection.
+     *
+     * This depends on an internet connection already having been made and a
+     * client being available.
+     *
+     * @param outClient An Arduino client instance to use to print data to.
+     * Allows the use of any type of client and multiple clients tied to a
+     * single TinyGSM modem instance
+     * @return **int16_t** The http status code of the response.
+     */
+     int16_t publishData(Client* outClient) override;
 
 protected:
+    /**
+     * @anchor ubidots_post_vars
+     * @name Portions of the POST request to Ubidots
+     *
+     * @{
+     */
+    static const char* postEndpoint;   ///< The endpoint
+    static const char* ubidotsHost;    ///< The host name
+    static const int   ubidotsPort;    ///< The host port
+    static const char* tokenHeader;    ///< The token header text
+    // static const char *cacheHeader;  ///< The cache header text
+    // static const char *connectionHeader;  ///< The keep alive header text
+    static const char* contentLengthHeader;  ///< The content length header text
+    static const char* contentTypeHeader;    ///< The content type header text
+    /**@}*/
 
-    // portions of the POST request
-    static const char *postEndpoint;
-    static const char *ubidotsHost;
-    static const int ubidotsPort;
-    static const char *tokenHeader;
-    // static const char *cacheHeader;
-    // static const char *connectionHeader;
-    static const char *contentLengthHeader;
-    static const char *contentTypeHeader;
-
-    // portions of the JSON
-    static const char *payload;
+    /**
+     * @anchor ubidots_json_vars
+     * @name The begining of the JSON object for Ubidots
+     *
+     * @{
+     */
+    static const char* payload;  ///< The JSON initial characters
+    /**@}*/
 
 private:
-    // Tokens and UUID's for EnviroDIY
-    const char *_authentificationToken;
+    // Tokens for Ubidots
+    const char* _authentificationToken;
 };
 
-#endif  // Header Guard
+#endif  // SRC_PUBLISHERS_UBIDOTSPUBLISHER_H_
