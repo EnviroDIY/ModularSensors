d1f35baa9703348dfdcf924ed330ec70af08fc95
Sara Damiano <sdamiano@stroudcenter.org>
2020-11-24 15:14:38 -0500

    Change case, add a bunch of notes, made k a param

--- src/sensors/analogElecConductivity.cpp
+++ src/sensors/analogElecConductivity.cpp
@@ -1,5 +1,5 @@
 /**
- * @file analogElecConductivity.cpp
+ * @file AnalogElecConductivity.cpp
  * @copyright 2020 Stroud Water Research Center
  * Part of the EnviroDIY ModularSensors library
  * @copyright 2020 Neil Hancock
@@ -8,105 +8,75 @@
  *input and onboard ADC and ADC ref.
  */
 
-#include "analogElecConductivity.h"
+#include "AnalogElecConductivity.h"
 
 // For Mayfly version; the battery resistor depends on it
-analogElecConductivity::analogElecConductivity(int8_t powerPin, int8_t dataPin,
+AnalogElecConductivity::AnalogElecConductivity(int8_t powerPin, int8_t dataPin,
                                                float   Rseries_ohms,
+                                               float   sensorEC_Konst,
                                                uint8_t measurementsToAverage)
-    : Sensor("analogElecConductivity", ANALOGELECCONDUCTIVITY_NUM_VARIABLES,
+    : Sensor("AnalogElecConductivity", ANALOGELECCONDUCTIVITY_NUM_VARIABLES,
              ANALOGELECCONDUCTIVITY_WARM_UP_TIME_MS,
              ANALOGELECCONDUCTIVITY_STABILIZATION_TIME_MS,
              ANALOGELECCONDUCTIVITY_MEASUREMENT_TIME_MS, powerPin, dataPin,
              measurementsToAverage) {
-    //_version = version;
     _EcPowerPin            = powerPin;
     _EcAdcPin              = dataPin;
     _ptrWaterTemperature_C = NULL;
     _Rseries_ohms          = Rseries_ohms;
-    /* clang-format off */
-    /* Not checked
-#if defined(ARDUINO_AVR_ENVIRODIY_MAYFLY) ||
-    defined(ARDUINO_AVR_SODAQ_MBILI) _EcAdcPin = A6;
-#elif defined(ARDUINO_AVR_FEATHER32U4) ||
-        defined(ARDUINO_SAMD_FEATHER_M0) ||
-        defined(ARDUINO_SAMD_FEATHER_M0_EXPRESS)
-        _EcAdcPin = 9;
-#elif defined(ADAFRUIT_FEATHER_M4_EXPRESS)
-        _EcAdcPin = A6;  // 20;  //Dedicated PB01 V_DIV
-#elif defined(ARDUINO_SODAQ_ONE) || defined(ARDUINO_SODAQ_ONE_BETA) ||
-        defined(ARDUINO_AVR_SODAQ_NDOGO)
-        _EcAdcPin = 10;
-#elif defined(ARDUINO_SODAQ_AUTONOMO)
-    if (strcmp(_version, "v0.1") == 0)
-        _EcAdcPin   = 48;
-    else _EcAdcPin  = 33;
-#else
-#info No board defined
-    _EcAdcPin = -1;
-#endif
-    */
-    /* clang-format on */
+    _sensorEC_Konst        = sensorEC_Konst;
 }
 // Destructor
-analogElecConductivity::~analogElecConductivity() {}
+AnalogElecConductivity::~AnalogElecConductivity() {}
 
-String analogElecConductivity::getSensorLocation(void) {
+String AnalogElecConductivity::getSensorLocation(void) {
     String sensorLocation = F("anlgEc Proc Data/Pwr");
     sensorLocation += String(_EcAdcPin) + "/" + String(_EcPowerPin);
     return sensorLocation;
 }
 
 
-float analogElecConductivity::readEC() {
+float AnalogElecConductivity::readEC() {
     return readEC(_EcAdcPin);
 }
 
 
-float analogElecConductivity::readEC(uint8_t analogPinNum) {
+float AnalogElecConductivity::readEC(uint8_t analogPinNum) {
     uint32_t sensorEC_adc;
     float    Rwater_ohms;         // literal value of water
     float    EC_uScm, EC25_uScm;  // units are uS per cm
 
+    // Set the resolution for the processor ADC, only applies to SAMD boards.
 #if !defined ARDUINO_ARCH_AVR
-    analogReadResolution(analogElecConductivityDef_Resolution);
-    analogReference(AR_EXTERNAL);  // ratio metric for the EC resistor
-// analogReference(PROC_ADC_DEF_REFERENCE); //VDDANA = 3V3
+    analogReadResolution(ANALOG_EC_DEFAULT_RESOLUTION);
 #endif  // ARDUINO_ARCH_AVR
+    // Set the analog reference mode for the voltage measurement.
+    // If possible, to get the best results, an external reference should be
+    // used.
+    analogReference(PROC_ADC_DEF_REFERENCE);
 
-    //************Estimates Resistance of Liquid ****************//
-    // digitalWrite(_EcPowerPin,HIGH); //assume done by class Sensor
-    delay(1);  // Total time is about 5mS
+    //************ Estimates Resistance of Liquid ****************//
 
+    // First measure the analog voltage.
+    // The return value from analogRead() is IN BITS NOT IN VOLTS!!
+    // Take a priming reading.
     // First reading will be low - discard
+    analogRead(analogPinNum);
+    // Take the reading we'll keep
     sensorEC_adc = analogRead(analogPinNum);
+    MS_DEEP_DBG("adc bits=", sensorEC_adc);
 
-    MS_DEEP_DBG("adc=", sensorEC_adc);
-
-    //***************** Converts to EC **************************//
-
-    // sensorEC_V= (SensorV* sensorEC_adc)/EC_SENSOR_ADC_RANGE;
-    // Rwater_ohms=(sensorEC_V*Rseries_ohms)/(SensorV-sensorEC_V);
-
-    /*Assuming sensorEC_adc is ratio metric - adc Reference is same as applied
-     * to EC sensor. The Vcc ~ 3.3V can vary, as battery level gets low, so
-     * would be nice to eliminate it in the calcs
-     *
-     *   raw_adc/EC_SENSOR_ADC_RANGE = Rwater_ohms/(Rwater_ohms+Rseries_ohms)
-     */
     if (0 == sensorEC_adc) {
-        // Prevent underflow, can never be EC_SENSOR_ADC_RANGE
+        // Prevent underflow, can never be ANALOG_EC_ADC_RANGE
         sensorEC_adc = 1;
     }
 
+    // see the header for an explanation of this calculation
     Rwater_ohms = _Rseries_ohms /
-        (((float)EC_SENSOR_ADC_RANGE / (float)sensorEC_adc) - 1);
+        (((float)ANALOG_EC_ADC_RANGE / (float)sensorEC_adc) - 1);
 
-    /* The Rwater is an absolute value, but is based on a defined size of the
-     * plates of the sensor. Translating a magic "sensorsEC_konst" is used,
-     * derived from others experiments
-     */
-    EC_uScm = 1000000 / (Rwater_ohms * sensorEC_Konst);
+    //***************** Converts to EC **************************//
+    EC_uScm = 1000000 / (Rwater_ohms * _sensorEC_Konst);
 
     //*************Compensating For Temperature********************//
     if (NULL != _ptrWaterTemperature_C) {
@@ -122,7 +92,7 @@ float analogElecConductivity::readEC(uint8_t analogPinNum) {
 }
 
 
-bool analogElecConductivity::addSingleMeasurementResult(void) {
+bool AnalogElecConductivity::addSingleMeasurementResult(void) {
     float sensorEC_uScm = -9999;
 
     if (bitRead(_sensorStatus, 6)) {
--- src/sensors/analogElecConductivity.h
+++ src/sensors/analogElecConductivity.h
@@ -1,5 +1,5 @@
 /**
- * @file analogElecConductivity.h
+ * @file AnalogElecConductivity.h
  * @copyright 2020 Stroud Water Research Center
  * Part of the EnviroDIY ModularSensors library
  * @copyright 2020 Neil Hancock
@@ -13,55 +13,103 @@
 /* clang-format off */
 /**
  * @defgroup sensor_analog_cond Conductivity via Analog Electrical Resistance
- * Classes for the Keller Nanolevel capacitive level sensor.
+ * Classes for measuring conductivity using a simple analog voltage divider.
  *
  * @ingroup the_sensors
  *
  * @tableofcontents
  * @m_footernavigation
  *
- * @section sensor_analog_cond_notes Quick Notes
- * EC from IEC probe, requires ADC_PIN, source resistance, applied
- * voltage, water temperature.
+ * @section sensor_analog_cond_notes Introduction
+ * This is for a very basic conductivity circuit built with a single resistor
+ * and an old power cord.  DC power is briefly supplied across the power cord
+ * causing the water to act as one of the resistors on a voltage divider.
+ * Knowing the voltage of the other resistor in the divider, we can calculate to
+ * resistance from the water (and then its electrical conductivity) based on the
+ * drop in volage across the divider.
  *
- * Requires switched power pin. The water temperature (if used) must be suplied
- * seperately for a calculation.
+ * For this to work, the power across the circuit **MUST** be turned off between
+ * readings.  If the power to the circuit is left on the water will become
+ * polarized and the values will not be valid. The water temperature (if used)
+ * must be suplied separately for a calculation.
  *
- * To use, must switch power to series resistor [default 499R], then measurement
- * from anlog pin A[0-7] and then sensor
+ * @section sensor_analog_cond_circuit The Circuit
+ * One pole of the power cord wire is connected to the ground of the main logger
+ * board.  The other pole is connected to the sensor power supply via a resistor
+ * of known resistance (R1) and then to an analog pin to measure the voltage.
  *
- *  SensorV-- adcPin/Ra --- R1 ---- Sensorconnector&Wire  -- Rwater --- Groond
- * R1 series resistance ~ 500ohms
- * Rwater - Resistenace of Water
- * Ra - Resistance of applied Source - possibly uP Digital Pin
+ * So the circuit is:
  *
- * Returns microSiemens the inverse of resistance
- * @section sensor_analog_cond_parts Parts & Wiring for Mayfly
- * - Mayfly 0.5b or later
- * - Sensor  AC Power Cord 12t with male IEC 320-C8 connector.
- * - 2x10 J3 for any pins A0-A7
- * - Rlmt eg 499R series 1/4W
+ *  Vin (sensor power) ---- R1 ---- power cord  ---- Vout
+ *                                       |
+ *                                       |
+ *                          water between prongs (Rwater)
+ *                                       |
+ *                                       |
+ *                                    ground
  *
- * @note Analog JP8 JP9 for ADC 16bit/ADS1115 uses different algorithim
+ * The above diagram and the calculations assume the reistance of the analog
+ * pins themselves on the Arduino is negligible.
  *
- * @section sensor_analog_cond_wiring Wiring
- * Will use J3-Pin2 A0 but could be any of A0-A3, A5
- *  and one additional pin switches power eg AA (PwrPin)
- *  A7 by default is tied to DS3231 Timer Int
- *  Rlmt/500ohms to A7 and Sensor; J3-Pin10 to Sensor J3-Pin2
- *  Sensors one side; J3-Pin2
- *  Sensor  one side to GND Mayfly J3-Pin20
+ * @section sensor_analog_cond_calcs Calculating the Conductivity
+ * First, we need to convert the bit reading of the ADC into volts based on the
+ * range of the ADC (1 bit more than the resolution):
+ *
+ * meas_voltage = (analog_ref_voltage * raw_adc_bits) / ANALOG_EC_ADC_RANGE
+ *
+ * Assuming the voltage of the ADC reference is the same as that used to power
+ * the EC resistor circuit we can replace the reference voltage with the sensor
+ * power voltage:
+ *
+ * meas_voltage = (sensor_power_voltage * raw_adc_bits) / ANALOG_EC_ADC_RANGE
+ *
+ * @note The Vcc going to the circuit (~3.3V) can and will vary, as battery
+ * level gets low.  If possible, you should use setup the processor to use an
+ * external reference and tie the Aref pin to the sensor power pin.
+ *
+ * @note The analog reference of the Mayfly is not broken out (and is tied to
+ * ground).  If using a Mayfly, you have no choice by to use the internal analog
+ * reference.
+ *
+ * Now we can calculate the resistance of the water, knowing the resistance of
+ * the resistor we put in the circuit and the voltage drop:
+ *
+ * Rwater_ohms = (meas_voltage * Rseries_ohms) / (sensor_power_voltage - meas_voltage);
+ *
+ * Combining the above equations and doing some rearranging, we get:
+ *
+ * Rwater_ohms = Rseries_ohms / ((ANALOG_EC_ADC_RANGE / raw_adc_bits) - 1)
+ *
+ * The conductivity is then the inverse of the resistance - multiplied by a
+ * measured cell constant and a 10^6 conversion to µS/cm.
+ *
+ * water_conductivity = 1000000 / (Rwater_ohms * sensorEC_Konst)
+ *
+ * The real cell constant will vary based on the size of the "cell" - that is,
+ * the size of the plug on the power cord.  You can calculate the cell constant
+ * for each power cord sensor you use following the calibration program here:
+ * https://hackaday.io/project/7008-fly-wars-a-hackers-solution-to-world-hunger/log/24646-three-dollar-ec-ppm-meter-arduino.
+ * For one AC Power Cord 12t with male IEC 320-C8 connector the cell constant
+ * was 2.88.
+ *
+ * @note These calulations are for the on-board processor ADC, not an external
+ * ACD like the TI ADS1115 built into the Mayfly!
  *
  * @section sensor_analog_cond_ref References
  * https://hackaday.io/project/7008-fly-wars-a-hackers-solution-to-world-hunger/log/24646-three-dollar-ec-ppm-meter-arduino
  * http://www.reagecon.com/pdf/technicalpapers/Effect_of_Temperature_TSP-07_Issue3.pdf
  *
+ * @section sensor_atlas_cond_flags Build flags
+ * - `-D MS_ATLAS_SOFTWAREWIRE`
+ *      - switches from using hardware I2C to software I2C
+ *
  * @section sensor_analog_cond_ctor Sensor Constructors
- * {{ @ref analogElecConductivity::analogElecConductivity }}
+ * {{ @ref AnalogElecConductivity::AnalogElecConductivity }}
  *
  * ___
  * @section sensor_analog_cond_examples Example Code
- * This sensor is not currently used in any of the examples
+ * The analog electrical conductivity sensor is used in the
+ * @menulink{analog_cond} example.
  *
  * @menusnip{analog_cond}
  *
@@ -73,10 +121,10 @@
 #define SRC_SENSORS_ANALOGELECCONDUCTIVITY_H_
 
 #ifdef MS_ANALOGELECCONDUCTIVITY_DEBUG
-#define MS_DEBUGGING_STD "analogElecConductivity"
+#define MS_DEBUGGING_STD "AnalogElecConductivity"
 #endif
 #ifdef MS_ANALOGELECCONDUCTIVITY_DEBUG_DEEP
-#define MS_DEBUGGING_DEEP "analogElecConductivity"
+#define MS_DEBUGGING_DEEP "AnalogElecConductivity"
 #endif
 // Included Dependencies
 #include "ModSensorDebugger.h"
@@ -99,11 +147,17 @@
  */
 /// @brief Sensor::_warmUpTime_ms; giving 2ms for warm-up.
 #define ANALOGELECCONDUCTIVITY_WARM_UP_TIME_MS 2
-/// @brief Sensor::_stabilizationTime_ms; we assume the analog voltage is stable
-/// immediately after warm-up
-#define ANALOGELECCONDUCTIVITY_STABILIZATION_TIME_MS 0
-/// @brief Sensor::_measurementTime_ms; we assume the analog voltage is measured
-/// instantly
+/// @brief Sensor::_stabilizationTime_ms; we give just a tiny delay for
+/// stabilization.
+#define ANALOGELECCONDUCTIVITY_STABILIZATION_TIME_MS 1
+/**
+ * @brief Sensor::_measurementTime_ms; we assume the analog voltage is measured
+ * instantly.  It's not really *quite* instantly, but it is very fast and the
+ * time to measure is included in the read function.
+ * On ATmega based boards (UNO, Nano, Mini, Mega), it takes about 100
+ * microseconds (0.0001 s) to read an analog input, so the maximum reading rate
+ * is about 10,000 times a second.
+ */
 #define ANALOGELECCONDUCTIVITY_MEASUREMENT_TIME_MS 0
 /**@}*/
 
@@ -116,7 +170,7 @@
  * - Accuracy: needs determining for each combination of ADC. ADC_REF, and
  * series R. its designed as a very simple relative EC measurement
  *
- * {{ @ref analogElecConductivity_EC::analogElecConductivity_EC }}
+ * {{ @ref AnalogElecConductivity_EC::AnalogElecConductivity_EC }}
  */
 /**@{*/
 /**
@@ -135,55 +189,131 @@
 #define ANALOGELECCONDUCTIVITY_EC_DEFAULT_CODE "anlgEc"
 /**@}*/
 
+#if !defined ANALOG_EC_DEFAULT_RESOLUTION
 /**
  * @brief Default resolution (in bits) of the voltage measurement
  *
  * This is the default for all boards, use a build flag to change this, if
  * necessary.
  */
-#define analogElecConductivityDef_Resolution 10
-#define analogElecConductivityAdc_Max \
-    ((1 << analogElecConductivityDef_Resolution) - 1)
-#define EC_SENSOR_ADC_RANGE (1 << analogElecConductivityDef_Resolution)
+#define ANALOG_EC_DEFAULT_RESOLUTION 10
+#endif  // ANALOG_EC_DEFAULT_RESOLUTION
+/// @brief The maximum possible value of the ADC - one less than the resolution
+/// shifted up one bit.
+#define ANALOG_EC_ADC_MAX ((1 << ANALOG_EC_DEFAULT_RESOLUTION) - 1)
+/// @brief The maximum possible range of the ADC - the resolution shifted up one
+/// bit.
+#define ANALOG_EC_ADC_RANGE (1 << ANALOG_EC_DEFAULT_RESOLUTION)
 
+/* clang-format off */
 #if !defined PROC_ADC_DEF_REFERENCE
-/// @brief One of eAnalogReference for all host platforms.
+#ifdef ARDUINO_ARCH_SAMD
+/**
+ * @brief The voltage reference mode for the processor's ADC.
+ *
+ * For a SAMD board, this must be one of:
+ * - AR_DEFAULT: the default analog reference of 3.3V
+ * - AR_INTERNAL: a built-in 2.23V reference
+ * - AR_INTERNAL1V0: a built-in 1.0V reference
+ * - AR_INTERNAL1V65: a built-in 1.65V reference
+ * - AR_INTERNAL2V23: a built-in 2.23V reference
+ * - AR_EXTERNAL: the voltage applied to the AREF pin is used as the reference
+ *
+ * @see https://www.arduino.cc/reference/en/language/functions/analog-io/analogreference/
+ */
 #define PROC_ADC_DEF_REFERENCE AR_DEFAULT
+#elif defined ARDUINO_ARCH_AVR
+/**
+ * @brief The voltage reference mode for the processor's ADC.
+ *
+ * For an AVR board, this must be one of:
+ * - DEFAULT: the default analog reference of 5 volts (on 5V Arduino boards)
+ * or 3.3 volts (on 3.3V Arduino boards)
+ * - INTERNAL: a built-in reference, equal to 1.1 volts on the ATmega168 or
+ * ATmega328P and 2.56 volts on the ATmega32U4 and ATmega8 (not available on the
+ * Arduino Mega)
+ * - INTERNAL1V1: a built-in 1.1V reference (Arduino Mega only)
+ * - INTERNAL2V56: a built-in 2.56V reference (Arduino Mega only)
+ * - EXTERNAL: the voltage applied to the AREF pin (0 to 5V only) is used as the
+ * reference.
+ *
+ * @see https://www.arduino.cc/reference/en/language/functions/analog-io/analogreference/
+ */
+#define PROC_ADC_DEF_REFERENCE DEFAULT
+#else
+#error The processor ADC reference type must be defined!
+#endif
 #endif  // PROC_ADC_DEF_REFERENCE
+/* clang-format on */
 
 #if !defined RSERIES_OHMS_DEF
-/// @brief The default resistance (in ohms) of the measuring resistor.
+/**
+ * @brief The default resistance (in ohms) of the measuring resistor.
+ * This should not be less than 300 ohms when measuring EC in water.
+ */
 #define RSERIES_OHMS_DEF 499
 #endif  // RSERIES_OHMS_DEF
+
+
+#if !defined TEMPERATURECOEF_DEF
+/**
+ *  @brief Linearized temperature correction coefficient per degrees Celsius.
+ *
+ * The value of 0.019 comes from measurements reported here:
+ * Hayashi M. Temperature-electrical conductivity relation of water for
+ * environmental monitoring and geophysical data inversion. Environ Monit
+ * Assess. 2004 Aug-Sep;96(1-3):119-28. doi: 10.1023/b:emas.0000031719.83065.68.
+ * PMID: 15327152.
+ */
+#define TEMPERATURECOEF_DEF 0.019
+#endif  // TEMPERATURECOEF_DEF
+
+#if !defined SENSOREC_KONST_DEF
+/**
+ * @brief Cell Constant For EC Measurements.
+ *
+ * This should be measured following the calibration example on
+ * https://hackaday.io/project/7008-fly-wars-a-hackers-solution-to-world-hunger/log/24646-three-dollar-ec-ppm-meter-arduino.
+ *
+ * Mine was around 2.9 with plugs being a standard size they should all be
+ * around the same. If you get bad readings you can use the calibration script
+ * and fluid to get a better estimate for K.
+ */
+#define SENSOREC_KONST_DEF 2.88
+#endif  // SENSOREC_KONST_DEF
 
 /**
  * @brief Class for the analog Electrical Conductivity monitor
  *
  * @ingroup sensor_analog_cond
  */
-class analogElecConductivity : public Sensor {
+class AnalogElecConductivity : public Sensor {
  public:
     /**
-     * @brief Construct a new analogElecConductivity object.
+     * @brief Construct a new AnalogElecConductivity object.
      *
      * @param powerPin The port pin providing power to the EC probe.
      * Needs to be switched, and assumed to be same V as the dataPin's ADC.
-     * @param dataPin The adc port pin to read the voltage from the EC probe.
-     * @param Rseries_ohms The series R in the line. Used to calculate the
-     * measured value. optional with default value of 499, that maybe overridden
-     * with a define/build flag.
+     * @param dataPin The processor ADC port pin to read the voltage from the EC
+     * probe.  Not all processor pins can be used as analog pins.  Those usable
+     * as analog pins generally are numbered with an "A" in front of the number
+     * - ie, A1.
+     * @param Rseries_ohms The resistance of the resistor series (R) in the
+     * line. Used to calculate the measured value. optional with default value
+     * of 499, that maybe overridden with a define/build flag.
      * @param measurementsToAverage The number of measurements to average;
      * optional with default value of 1.
      *
      */
-    analogElecConductivity(int8_t powerPin, int8_t dataPin,
+    AnalogElecConductivity(int8_t powerPin, int8_t dataPin,
                            float   Rseries_ohms          = RSERIES_OHMS_DEF,
+                           float   sensorEC_Konst        = SENSOREC_KONST_DEF,
                            uint8_t measurementsToAverage = 1);
 
     /**
-     * @brief Destroy the analogElecConductivity object - no action needed.
+     * @brief Destroy the AnalogElecConductivity object - no action needed.
      */
-    ~analogElecConductivity();
+    ~AnalogElecConductivity();
 
     /**
      * @brief Report the sensor info.
@@ -232,33 +362,20 @@ class analogElecConductivity : public Sensor {
     float readEC(uint8_t analogPinNum);
 
  private:
-    const char* _version;
-    int8_t      _EcPowerPin;
-    int8_t      _EcAdcPin;
+    int8_t _EcPowerPin;
+    int8_t _EcAdcPin;
 
-    float*      _ptrWaterTemperature_C;
-    const float SensorV = 3.3;
+    float* _ptrWaterTemperature_C;
 
-    // _Rseries_ohms ~ that is R1 + any series port resistance
+    /// @brief The resistance of the circiut resistor plus any series port
+    /// resistance
     float _Rseries_ohms = RSERIES_OHMS_DEF;
 
-#if !defined TEMPERATURECOEF_DEF
-/// @brief TEMPERATURECOEF_DEF defined for chemical/transport under measurement
-#define TEMPERATURECOEF_DEF 0.019
-#endif  // TEMPERATURECOEF_DEF
-    const float TemperatureCoef = TEMPERATURECOEF_DEF;
+    /// @brief the cell constant for the circuit
+    float _sensorEC_Konst = SENSOREC_KONST_DEF;
 
-#if !defined SENSOREC_KONST_DEF
-/**
- * @brief  Cell Constant For Ec Measurements - place holder.
- *
- * Mine was around 2.9 with plugs being a standard size they should all be
- * around the same. If you get bad readings you can use the calibration script
- * and fluid to get a better estimate for K.
- */
-#define SENSOREC_KONST_DEF 2.88
-#endif  // SENSOREC_KONST_DEF
-    const float sensorEC_Konst = SENSOREC_KONST_DEF;
+    /// @brief The temperature correction coefficient for the circuit
+    const float TemperatureCoef = TEMPERATURECOEF_DEF;
 };
 
 /**
@@ -268,20 +385,20 @@ class analogElecConductivity : public Sensor {
  * @ingroup sensor_analog_cond
  *
  */
-class analogElecConductivity_EC : public Variable {
+class AnalogElecConductivity_EC : public Variable {
  public:
     /**
-     * @brief Construct a new  analogElecConductivity_EC object.
+     * @brief Construct a new  AnalogElecConductivity_EC object.
      *
-     * @param parentSense The parent analogElecConductivity providing the result
+     * @param parentSense The parent AnalogElecConductivity providing the result
      * values.
      * @param uuid A universally unique identifier (UUID or GUID) for the
      * variable; optional with the default value of an empty string.
      * @param varCode A short code to help identify the variable in files;
      * optional with a default value of "anlgEc".
      */
-    analogElecConductivity_EC(
-        analogElecConductivity* parentSense, const char* uuid = "",
+    AnalogElecConductivity_EC(
+        AnalogElecConductivity* parentSense, const char* uuid = "",
         const char* varCode = ANALOGELECCONDUCTIVITY_EC_DEFAULT_CODE)
         : Variable(parentSense,
                    (const uint8_t)ANALOGELECCONDUCTIVITY_EC_VAR_NUM,
@@ -290,21 +407,21 @@ class analogElecConductivity_EC : public Variable {
                    ANALOGELECCONDUCTIVITY_EC_UNIT_NAME, varCode, uuid) {}
 
     /**
-     * @brief Construct a new analogElecConductivity_EC object.
+     * @brief Construct a new AnalogElecConductivity_EC object.
      *
-     * @note This must be tied with a parent analogElecConductivity before it
+     * @note This must be tied with a parent AnalogElecConductivity before it
      * can be used.
      */
-    analogElecConductivity_EC()
+    AnalogElecConductivity_EC()
         : Variable((const uint8_t)ANALOGELECCONDUCTIVITY_EC_VAR_NUM,
                    (uint8_t)ANALOGELECCONDUCTIVITY_EC_RESOLUTION,
                    ANALOGELECCONDUCTIVITY_EC_VAR_NAME,
                    ANALOGELECCONDUCTIVITY_EC_UNIT_NAME,
                    ANALOGELECCONDUCTIVITY_EC_DEFAULT_CODE) {}
     /**
-     * @brief Destroy the analogElecConductivity_EC object - no action needed.
+     * @brief Destroy the AnalogElecConductivity_EC object - no action needed.
      */
-    ~analogElecConductivity_EC() {}
+    ~AnalogElecConductivity_EC() {}
 };
 /**@}*/
 #endif  // SRC_SENSORS_ANALOGELECCONDUCTIVITY_H_
