ef04673a018236b9b1ccc8ce2b7ad69468018a25
Sara Damiano <sdamiano@stroudcenter.org>
2021-02-10 20:11:22 -0500

    Backwards conditional logic
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- continuous_integration/build-menu-configurations.sh
+++ continuous_integration/build-menu-configurations.sh
@@ -4,9 +4,9 @@
 set -e
 
 # keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+# trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 mkdir temp/menu_a_la_carte
 
