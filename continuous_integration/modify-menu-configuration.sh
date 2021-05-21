1d0214ad69e664bf1927c1a28a36dcfa0f4c2bd7
Sara Damiano <sdamiano@stroudcenter.org>
2021-05-21 12:32:17 -0400

    Clean menu build
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

---
+++ continuous_integration/modify-menu-configuration.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+# trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+# trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+mkdir temp
+mkdir temp/menu_a_la_carte
+
+echo "\e[36m--------------------------------------------------------------------------------------\e[0m"
+echo "\e[36m--------------------------------------------------------------------------------------\e[0m"
+echo "Modifying source for \e[32m$modemFlag\e[0m, \e[32m$sensorFlag\e[0m, and \e[32m$publisherFlag\e[0m"
+echo "\e[36m--------------------------------------------------------------------------------------\e[0m"
+echo "\e[36m--------------------------------------------------------------------------------------\e[0m"
+
+sed "1s/^/#define MS_BUILD_TEST_PRE_NAMED_VARS\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+
+if ["$modemFlag" != ""]
+then
+    sed "1s/^/#define $modemFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+fi
+if ["$sensorFlag" != ""]
+then
+    sed "1s/^/#define $sensorFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+fi
+if ["$publisherFlag" != ""]
+then
+    sed "1s/^/#define $publisherFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+fi
+
+echo "First few lines of source"
+head temp/menu_a_la_carte/menu_a_la_carte.ino
