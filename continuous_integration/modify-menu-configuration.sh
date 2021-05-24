5a3789aeb218210dda93afc34ab98c90bb070f71
Sara Damiano <sdamiano@stroudcenter.org>
2021-05-24 13:58:36 -0400

    use pio itself to verify library
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- continuous_integration/modify-menu-configuration.sh
+++ continuous_integration/modify-menu-configuration.sh
@@ -17,20 +17,17 @@ echo "Modifying source for \e[32m$modemFlag\e[0m, \e[32m$sensorFlag\e[0m, and \e
 echo "\e[36m--------------------------------------------------------------------------------------\e[0m"
 echo "\e[36m--------------------------------------------------------------------------------------\e[0m"
 
-sed -i "1s/^/#define MS_BUILD_TEST_PRE_NAMED_VARS\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+echo "Adding flag for pre-named variables"
+sed "1s/^/#define MS_BUILD_TEST_PRE_NAMED_VARS\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
 
-if [ "$modemFlag" != "" ]
-then
-    sed -i "1s/^/#define $modemFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
-fi
-if [ "$sensorFlag" != "" ]
-then
-    sed -i "1s/^/#define $sensorFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
-fi
-if [ "$publisherFlag" != "" ]
-then
-    sed -i "1s/^/#define $publisherFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
-fi
+echo "Adding flag for$modemFlag"
+sed "1s/^/#define $modemFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+
+echo "Adding flag for$sensorFlag"
+sed "1s/^/#define $sensorFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+
+echo "Adding flag for$publisherFlag"
+sed "1s/^/#define $publisherFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
 
 echo "First few lines of source"
 head temp/menu_a_la_carte/menu_a_la_carte.ino
