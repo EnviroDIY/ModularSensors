aa984a5a67dfae9d3bcfaceadbea3584eb762304
Sara Damiano <sdamiano@stroudcenter.org>
2021-05-24 13:28:35 -0400

    insert line with sed
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- continuous_integration/modify-menu-configuration.sh
+++ continuous_integration/modify-menu-configuration.sh
@@ -17,19 +17,19 @@ echo "Modifying source for \e[32m$modemFlag\e[0m, \e[32m$sensorFlag\e[0m, and \e
 echo "\e[36m--------------------------------------------------------------------------------------\e[0m"
 echo "\e[36m--------------------------------------------------------------------------------------\e[0m"
 
-sed "1s/^/#define MS_BUILD_TEST_PRE_NAMED_VARS\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+sed -i "1s/^/#define MS_BUILD_TEST_PRE_NAMED_VARS\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
 
 if [ "$modemFlag" != "" ]
 then
-    sed "1s/^/#define $modemFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+    sed -i "1s/^/#define $modemFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
 fi
 if [ "$sensorFlag" != "" ]
 then
-    sed "1s/^/#define $sensorFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+    sed -i "1s/^/#define $sensorFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
 fi
 if [ "$publisherFlag" != "" ]
 then
-    sed "1s/^/#define $publisherFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
+    sed -i "1s/^/#define $publisherFlag\n/" examples/menu_a_la_carte/menu_a_la_carte.ino > temp/menu_a_la_carte/menu_a_la_carte.ino
 fi
 
 echo "First few lines of source"
