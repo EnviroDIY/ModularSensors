314750c5ac204683a29475981f2fae827bd9106f
Sara Damiano <sdamiano@stroudcenter.org>
2022-04-01 12:10:07 -0400

    better action caching, auto-cancel
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- continuous_integration/build-install-doxygen.sh
+++ continuous_integration/build-install-doxygen.sh
@@ -3,6 +3,27 @@
 # Exit with nonzero exit code if anything fails
 set -e
 
+# install all the dependencies for make for Doxygen and m.css
+sudo apt-get update
+sudo apt-get -y install build-essential
+sudo apt-get -y install flex
+sudo apt-get -y install bison
+
+# install TeX Live for LaTeX formula rendering
+sudo apt-get -y install texlive-base
+sudo apt-get -y install texlive-latex-extra
+sudo apt-get -y install texlive-fonts-extra
+sudo apt-get -y install texlive-fonts-recommended
+
+echo "Current TeX version..."
+tex --version
+
+# install Graphviz for DOT class diagrams
+sudo apt-get -y install graphviz
+
+echo "Current graphviz version..."
+dot -v
+
 cd $TRAVIS_BUILD_DIR
 
 if [ ! -f $TRAVIS_BUILD_DIR/doxygen-src/build/bin/doxygen ]; then
