3698b5d210fa666a9db372a653bebda1a36e6a15
Sara Damiano <sdamiano@stroudcenter.org>
2022-04-13 11:25:03 -0400

    Compile commands for Sonar
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

---
+++ pioScripts/generate_compile_commands.py
@@ -0,0 +1,8 @@
+import os
+Import("env")
+
+# include toolchain paths
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
+
+# override compilation DB path
+env.Replace(COMPILATIONDB_PATH=os.path.join("$PROJECT_DIR/.vscode", "compile_commands.json"))
