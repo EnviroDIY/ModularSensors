50eb719f03a4d5c47e77b3cde15c52855cdf34f4
Sara Damiano <sdamiano@stroudcenter.org>
2022-06-30 12:24:33 -0400

    Beautify output
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

---
+++ continuous_integration/beautify_arduino_lint_log.py
@@ -0,0 +1,31 @@
+#%%
+import json
+import os
+
+in_json = open(os.environ["GITHUB_WORKSPACE"] + "arduino_lint.json")
+arduino_lint_results = json.load(in_json)
+
+out_md = open(os.environ["GITHUB_WORKSPACE"] + "arduino_lint.md", "w+")
+
+out_md.write("## Results of testing examples\n\n")
+out_md.write(" | Path | Project Type | Result | Problems | \n")
+out_md.write(" | --- | --- | --- | --- | \n")
+for project in arduino_lint_results["projects"]:
+    fail_list = "<ul>"
+    for failed_rule in project["rules"]:
+        fail_list += "<li> **{}** - {}</li>".format(
+            failed_rule["level"], failed_rule["message"]
+        )
+    fail_list += "</ul>"
+    out_md.write(
+        " | {} | {} | {} | {} | \n".format(
+            project["path"]
+            .replace(os.environ.get("GITHUB_WORKSPACE"), "")
+            .replace("examples\\", ""),
+            project["projectType"],
+            ":white_check_mark:" if project["summary"]["pass"] else ":x:",
+            fail_list,
+        )
+    )
+
+#%%
--- continuous_integration/build_examples.sh
+++ continuous_integration/build_examples.sh
@@ -15,27 +15,28 @@ do
     for fqbn in 'EnviroDIY:avr:envirodiy_mayfly' 'arduino:avr:mega' 'arduino:samd:mzero_bl' 'adafruit:samd:adafruit_feather_m0' 'SODAQ:samd:sodaq_autonomo'
     
     do
-        echo "::group::Setting build flags"
+        echo "::group::Setting build flags for $fqbn"
         echo "::debug::Setting build flags for $fqbn"
-        if [ $fqbn = 'EnviroDIY:avr:envirodiy_mayfly' ]; then
-            echo "EXTRA_BUILD_FLAGS=-DNEOSWSERIAL_EXTERNAL_PCINT" >> $GITHUB_ENV
+        if [ $fqbn == 'EnviroDIY:avr:envirodiy_mayfly' ]; then
+            EXTRA_BUILD_FLAGS="-DNEOSWSERIAL_EXTERNAL_PCINT"
         fi
-        if [ $fqbn = 'arduino:avr:mega' ]; then
-            echo "EXTRA_BUILD_FLAGS=-DNEOSWSERIAL_EXTERNAL_PCINT" >> $GITHUB_ENV
+        if [ $fqbn == 'arduino:avr:mega' ]; then
+            EXTRA_BUILD_FLAGS="-DNEOSWSERIAL_EXTERNAL_PCINT"
         fi
-        if [ $fqbn = 'arduino:samd:mzero_bl' ]; then
-            echo "EXTRA_BUILD_FLAGS=-DNEOSWSERIAL_EXTERNAL_PCINT -DARDUINO_SAMD_ZERO -D__SAMD21G18A__ -DUSB_VID=0x2341 -DUSB_PID=0x804d -DUSBCON" >> $GITHUB_ENV
+        if [ $fqbn == 'arduino:samd:mzero_bl' ]; then
+            EXTRA_BUILD_FLAGS="-DNEOSWSERIAL_EXTERNAL_PCINT -DARDUINO_SAMD_ZERO -D__SAMD21G18A__ -DUSB_VID=0x2341 -DUSB_PID=0x804d -DUSBCON"
         fi
-        if [ $fqbn = 'adafruit:samd:adafruit_feather_m0' ]; then
-            echo "EXTRA_BUILD_FLAGS=-DNEOSWSERIAL_EXTERNAL_PCINT -DARDUINO_SAMD_ZERO -DARM_MATH_CM0PLUS -DADAFRUIT_FEATHER_M0 -D__SAMD21G18A__ -DUSB_VID=0x239A -DUSB_PID=0x800B -DUSBCON -DUSB_CONFIG_POWER=100" >> $GITHUB_ENV
+        if [ $fqbn == 'adafruit:samd:adafruit_feather_m0' ]; then
+            EXTRA_BUILD_FLAGS="-DNEOSWSERIAL_EXTERNAL_PCINT -DARDUINO_SAMD_ZERO -DARM_MATH_CM0PLUS -DADAFRUIT_FEATHER_M0 -D__SAMD21G18A__ -DUSB_VID=0x239A -DUSB_PID=0x800B -DUSBCON -DUSB_CONFIG_POWER=100"
         fi
-        if [ $fqbn = 'SODAQ:samd:sodaq_autonomo' ]; then
-            echo "EXTRA_BUILD_FLAGS=-DNEOSWSERIAL_EXTERNAL_PCINT -DVERY_LOW_POWER -D__SAMD21J18A__ -DUSB_VID=0x2341 -DUSB_PID=0x804d -DUSBCON" >> $GITHUB_ENV
+        if [ $fqbn == 'SODAQ:samd:sodaq_autonomo' ]; then
+            EXTRA_BUILD_FLAGS="-DNEOSWSERIAL_EXTERNAL_PCINT -DVERY_LOW_POWER -D__SAMD21J18A__ -DUSB_VID=0x2341 -DUSB_PID=0x804d -DUSBCON"
         fi
         echo "::endgroup::"
         
         BUILD_EXAMPLE="examples/$example/"
         
+        echo "::group::Running Arduino CLI for examples/$example/"
         echo "::debug::Running Arduino CLI for examples/$example/"
         arduino-cli --config-file continuous_integration/arduino_cli.yaml compile --clean --build-property "build.extra_flags=$EXTRA_BUILD_FLAGS" --fqbn $fqbn $BUILD_EXAMPLE 2>&1
         result_code=${PIPESTATUS[0]}
@@ -45,6 +46,7 @@ do
             compile_failures=$(($compile_failures + 1))
         fi
         echo "Total failures so far: $compile_failures"
+        echo "::endgroup::"
         
     done
     
@@ -56,9 +58,10 @@ do
         PLATFORMIO_DEFAULT_ENVS=$pio_environment
         PLATFORMIO_LIB_EXTRA_DIRS=/home/runner/.platformio/lib
         
+        echo "::group::Running PlatformIO for $PLATFORMIO_CI_SRC"
         echo "::debug::Running PlatformIO for $PLATFORMIO_CI_SRC"
         echo "$LIBRARY_INSTALL_SOURCE"
-        pio run --environment autonomo --project-conf="continuous_integration/platformio.ini" 2>&1
+        pio run --environment $pio_environment --project-conf="continuous_integration/platformio.ini" 2>&1
         result_code=${PIPESTATUS[0]}
         
         echo " | PlatformIO | $example | $pio_environment | $result_code |" >> $GITHUB_STEP_SUMMARY
@@ -66,6 +69,7 @@ do
             compile_failures=$(($compile_failures + 1))
         fi
         echo "Total failures so far: $compile_failures"
+        echo "::endgroup::"
         
     done
 done
