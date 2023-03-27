cd4f3c387810a1a1d6f249beebb11db55fd33f0a
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2023-03-27 10:43:16 -0500

    GroPoint Profile draft wrapper
    For Modbus GPLP-8 sensor only, for now.

---
+++ src/sensors/GroPointParent.cpp
@@ -0,0 +1,263 @@
+/**
+ * @file GroPointParent.cpp
+ * @copyright 2017-2023 Stroud Water Research Center
+ * Part of the EnviroDIY ModularSensors library for Arduino
+ * @author Anthony Aufdenkampe <aaufdenkampe@limno.com>
+ *
+ * @brief Implements the GroPointParent class.
+ */
+
+#include "GroPointParent.h"
+
+// The constructor - need the sensor type, modbus address, power pin, stream for
+// data, and number of readings to average
+GroPointParent::GroPointParent(
+    byte modbusAddress, Stream* stream, int8_t powerPin, int8_t powerPin2,
+    int8_t enablePin, uint8_t measurementsToAverage, gropointModel model,
+    const char* sensName, uint8_t numVariables, uint32_t warmUpTime_ms,
+    uint32_t stabilizationTime_ms, uint32_t measurementTime_ms,
+    uint8_t incCalcValues)
+    : Sensor(sensName, numVariables, warmUpTime_ms, stabilizationTime_ms,
+             measurementTime_ms, powerPin, -1, measurementsToAverage,
+             incCalcValues),
+            _model(model),
+            _modbusAddress(modbusAddress),
+            _stream(stream),
+            _RS485EnablePin(enablePin),
+            _powerPin2(powerPin2) {}
+GroPointParent::GroPointParent(
+    byte modbusAddress, Stream& stream, int8_t powerPin, int8_t powerPin2,
+    int8_t enablePin, uint8_t measurementsToAverage, gropointModel model,
+    const char* sensName, uint8_t numVariables, uint32_t warmUpTime_ms,
+    uint32_t stabilizationTime_ms, uint32_t measurementTime_ms,
+    uint8_t incCalcValues)
+    : Sensor(sensName, numVariables, warmUpTime_ms, stabilizationTime_ms,
+             measurementTime_ms, powerPin, -1, measurementsToAverage,
+             incCalcValues),
+            _model(model),
+            _modbusAddress(modbusAddress),
+            _stream(&stream),
+            _RS485EnablePin(enablePin),
+            _powerPin2(powerPin2) {}
+// Destructor
+GroPointParent::~GroPointParent() {}
+
+
+// The sensor installation location on the Mayfly
+String GroPointParent::getSensorLocation(void) {
+    String sensorLocation = F("modbus_0x");
+    if (_modbusAddress < 16) sensorLocation += "0";
+    sensorLocation += String(_modbusAddress, HEX);
+    return sensorLocation;
+}
+
+
+bool GroPointParent::setup(void) {
+    bool retVal =
+        Sensor::setup();  // this will set pin modes and the setup status bit
+    if (_RS485EnablePin >= 0) pinMode(_RS485EnablePin, OUTPUT);
+    if (_powerPin2 >= 0) pinMode(_powerPin2, OUTPUT);
+
+#ifdef MS_GROPOINTPARENT_DEBUG_DEEP
+    _gsensor.setDebugStream(&DEEP_DEBUGGING_SERIAL_OUTPUT);
+#endif
+
+    // This sensor begin is just setting more pin modes, etc, no sensor power
+    // required This realy can't fail so adding the return value is just for
+    // show
+    retVal &= _gsensor.begin(_model, _modbusAddress, _stream, _RS485EnablePin);
+
+    return retVal;
+}
+
+
+// The function to wake up a sensor
+// Different from the standard in that it waits for warm up and starts
+// measurements
+bool GroPointParent::wake(void) {
+    // Sensor::wake() checks if the power pin is on and sets the wake timestamp
+    // and status bits.  If it returns false, there's no reason to go on.
+    if (!Sensor::wake()) return false;
+
+    // Send the command to begin taking readings, trying up to 5 times
+    bool    success = false;
+    uint8_t ntries  = 0;
+    MS_DBG(F("Start Measurement on"), getSensorNameAndLocation());
+    while (!success && ntries < 5) {
+        MS_DBG('(', ntries + 1, F("):"));
+        success = _gsensor.startMeasurement();
+        ntries++;
+    }
+
+    if (success) {
+        // Update the time that the sensor was activated
+        _millisSensorActivated = millis();
+        MS_DBG(getSensorNameAndLocation(), F("activated and measuring."));
+    } else {
+        MS_DBG(getSensorNameAndLocation(), F("was NOT activated!"));
+        // Make sure the activation time is zero and the wake success bit (bit
+        // 4) is unset
+        _millisSensorActivated = 0;
+        _sensorStatus &= 0b11101111;
+    }
+
+    return success;
+}
+
+
+// The function to put the sensor to sleep
+// Different from the standard in that it stops measurements
+bool GroPointParent::sleep(void) {
+    if (!checkPowerOn()) { return true; }
+    if (_millisSensorActivated == 0) {
+        MS_DBG(getSensorNameAndLocation(), F("was not measuring!"));
+        return true;
+    }
+
+    // Send the command to begin taking readings, trying up to 5 times
+    bool    success = false;
+    uint8_t ntries  = 0;
+    MS_DBG(F("Stop Measurement on"), getSensorNameAndLocation());
+    while (!success && ntries < 5) {
+        MS_DBG('(', ntries + 1, F("):"));
+        success = _gsensor.stopMeasurement();
+        ntries++;
+    }
+    if (success) {
+        // Unset the activation time
+        _millisSensorActivated = 0;
+        // Unset the measurement request time
+        _millisMeasurementRequested = 0;
+        // Unset the status bits for sensor activation (bits 3 & 4) and
+        // measurement request (bits 5 & 6)
+        _sensorStatus &= 0b10000111;
+        MS_DBG(F("Measurements stopped."));
+    } else {
+        MS_DBG(F("Measurements NOT stopped!"));
+    }
+
+    return success;
+}
+
+
+// This turns on sensor power
+void GroPointParent::powerUp(void) {
+    if (_powerPin >= 0) {
+        MS_DBG(F("Powering"), getSensorNameAndLocation(), F("with pin"),
+               _powerPin);
+        digitalWrite(_powerPin, HIGH);
+        // Mark the time that the sensor was powered
+        _millisPowerOn = millis();
+    }
+    if (_powerPin2 >= 0) {
+        MS_DBG(F("Applying secondary power to"), getSensorNameAndLocation(),
+               F("with pin"), _powerPin2);
+        digitalWrite(_powerPin2, HIGH);
+    }
+    if (_powerPin < 0 && _powerPin2 < 0) {
+        MS_DBG(F("Power to"), getSensorNameAndLocation(),
+               F("is not controlled by this library."));
+    }
+    // Set the status bit for sensor power attempt (bit 1) and success (bit 2)
+    _sensorStatus |= 0b00000110;
+}
+
+
+// This turns off sensor power
+void GroPointParent::powerDown(void) {
+    if (_powerPin >= 0) {
+        MS_DBG(F("Turning off power to"), getSensorNameAndLocation(),
+               F("with pin"), _powerPin);
+        digitalWrite(_powerPin, LOW);
+        // Unset the power-on time
+        _millisPowerOn = 0;
+        // Unset the activation time
+        _millisSensorActivated = 0;
+        // Unset the measurement request time
+        _millisMeasurementRequested = 0;
+        // Unset the status bits for sensor power (bits 1 & 2),
+        // activation (bits 3 & 4), and measurement request (bits 5 & 6)
+        _sensorStatus &= 0b10000001;
+    }
+    if (_powerPin2 >= 0) {
+        MS_DBG(F("Turning off secondary power to"), getSensorNameAndLocation(),
+               F("with pin"), _powerPin2);
+        digitalWrite(_powerPin2, LOW);
+    }
+    if (_powerPin < 0 && _powerPin2 < 0) {
+        MS_DBG(F("Power to"), getSensorNameAndLocation(),
+               F("is not controlled by this library."));
+        // Do NOT unset any status bits or timestamps if we didn't really power
+        // down!
+    }
+}
+
+
+bool GroPointParent::addSingleMeasurementResult(void) {
+    bool success = false;
+
+    // Check a measurement was *successfully* started (status bit 6 set)
+    // Only go on to get a result if it was
+    if (bitRead(_sensorStatus, 6)) {
+        switch (_model) {
+            case GPLP8: {
+                // Initialize float variables
+                float valueM1 = -9999;
+                float valueM2 = -9999;
+                float valueM3 = -9999;
+                float valueM4 = -9999;
+                float valueM5 = -9999;
+                float valueM6 = -9999;
+                float valueM7 = -9999;
+                float valueM8 = -9999;
+
+                // Get Values
+                MS_DBG(F("Get Values from"), getSensorNameAndLocation());
+                success = _gsensor.getValues(valueM1, valueM2, valueM3,
+                                             valueM4, valueM5, valueM6, 
+                                             valueM7, valueM8);
+
+                // Fix not-a-number values
+                if (!success || isnan(valueM1)) valueM1 = -9999;
+                if (!success || isnan(valueM2)) valueM2 = -9999;
+                if (!success || isnan(valueM3)) valueM3 = -9999;
+                if (!success || isnan(valueM4)) valueM4 = -9999;
+                if (!success || isnan(valueM5)) valueM5 = -9999;
+                if (!success || isnan(valueM6)) valueM6 = -9999;
+                if (!success || isnan(valueM7)) valueM7 = -9999;
+                if (!success || isnan(valueM8)) valueM8 = -9999;
+
+                MS_DBG(F("    "), _gsensor.getParameter());
+                MS_DBG(F("    "), DOmgL, ',', Turbidity, ',', Cond, ',', pH,
+                       ',', Temp, ',', ORP, ',', Chlorophyll, ',', BGA);
+
+                // Put values into the array
+                verifyAndAddMeasurementResult(0, valueM1);
+                verifyAndAddMeasurementResult(1, valueM2);
+                verifyAndAddMeasurementResult(2, valueM3);
+                verifyAndAddMeasurementResult(3, valueM4);
+                verifyAndAddMeasurementResult(4, valueM5);
+                verifyAndAddMeasurementResult(5, valueM6);
+                verifyAndAddMeasurementResult(6, valueM7);
+                verifyAndAddMeasurementResult(7, valueM8);
+
+                break;
+            }
+            default: {
+                // Get Values
+                MS_DBG(F("Other GroPoint models not yet implemented."));
+
+            }
+        }
+    } else {
+        MS_DBG(getSensorNameAndLocation(), F("is not currently measuring!"));
+    }
+
+    // Unset the time stamp for the beginning of this measurement
+    _millisMeasurementRequested = 0;
+    // Unset the status bits for a measurement request (bits 5 & 6)
+    _sensorStatus &= 0b10011111;
+
+    // Return true when finished
+    return success;
+}
---
+++ src/sensors/GroPointProfileGPLP8.h
@@ -0,0 +1,298 @@
+/**
+ * @file GroPointProfileGPLP8.h
+ * @copyright 2017-2023 Stroud Water Research Center
+ * Part of the EnviroDIY ModularSensors library for Arduino
+ * @author Anthony Aufdenkampe <aaufdenkampe@limno.com>
+ *
+ * @brief Contains the GroPointProfileGPLP8 sensor subclass and the variable
+ * subclasses GroPointProfileGPLP8_Moist and GroPointProfileGPLP8_Temp
+ *
+ * These are for the GroPoint Profile GPLP-8 Eight-Segment Soil Moisture 
+ * and Temperature Profiling Probe.
+ *
+ * This depends on the GroPointParent super class.
+ *
+ * Documentation for the Modbus Protocol commands and responses can be found
+ * within the documentation in the GroPointModbus library at:
+ * https://github.com/EnviroDIY/GroPointModbus
+ *
+ * More detailed infromation on each variable can be found in the documentation
+ * for the individual sensor probes
+ */
+/* clang-format off */
+/**
+ * @defgroup sensor_gplp8 GroPoint Profile GPLP-8 Soil Moisture & Temperature 
+ * Profiling Probe. Classes for the GroPoint Profile GPLP-8 Soil Moisture & 
+ * Temperature Probe.
+ *
+ * @ingroup GroPoint_group
+ *
+ * @tableofcontents
+ * @m_footernavigation
+ *
+ * @section sensor_gplp8_datasheet Sensor Datasheet
+ * - [GroPoint Profile User Manual](https://www.gropoint.com/s/2625-N-T-GroPoint-Profile-User-Manual-V113.pdf), including Modbus Instructions.
+ * - [GroPoint Profile Technical Info](https://www.gropoint.com/s/GroPoint-Profile-Technical-Info.pdf)
+ * *
+ * @section sensor_gplp8_ctor Sensor Constructor
+ * {{ @ref GroPointProfileGPLP8::GroPointProfileGPLP8 }}
+ *
+ * ___
+ * @section sensor_gplp8_examples Example Code
+ * The GPLP-8 Probe is used in the @menulink{GroPoint_gplp8} example.
+ *
+ * @menusnip{gropoint_gplp8}
+ */
+/* clang-format on */
+
+// Header Guards
+#ifndef SRC_SENSORS_GROPOINTPROFILEGPLP8_H_
+#define SRC_SENSORS_GROPOINTPROFILEGPLP8_H_
+
+// Included Dependencies
+#include "sensors/GroPointParent.h"
+
+/** @ingroup sensor_gplp8 */
+/**@{*/
+
+// Sensor Specific Defines
+/// @brief Sensor::_numReturnedValues; the GPLP8 can report 8 values.
+#define GPLP8_NUM_VARIABLES 2
+/// @brief Sensor::_incCalcValues; we don't calculate any additional values.
+#define GPLP8_INC_CALC_VARIABLES 0
+
+/**
+ * @anchor sensor_gplp8_timing
+ * @name Sensor Timing
+ * The sensor timing for a GroPoint Profile GPLP-8
+ */
+/**@{*/
+/**
+ * @brief Sensor::_warmUpTime_ms; time before sensor responds after power - 1.6
+ * seconds (1600ms).
+ *
+ * This is the time for communication to begin.
+ */
+#define GPLP8_WARM_UP_TIME_MS 350
+/**
+ * @brief Sensor::_stabilizationTime_ms; the GPLP-8 is stable after 100 ms.
+ *
+ */
+#define GPLP8_STABILIZATION_TIME_MS 100
+/// @brief Sensor::_measurementTime_ms; the GPLP-8 takes ~200 ms to complete a
+/// measurement.
+#define GPLP8_MEASUREMENT_TIME_MS 200
+/**@}*/
+
+/**
+ * @anchor sensor_gplp8_cond
+ * @name Conductivity
+ * The conductivity variable from a GroPoint Profile GPLP-8
+ * - Range is 1 µS/cm to 200 mS/cm
+ * - Accuracy is ± 1 % Full Scale
+ *
+ * {{ @ref GroPointProfileGPLP8_Moist::GroPointProfileGPLP8_Moist }}
+ */
+/**@{*/
+/// @brief Decimals places in string representation; conductivity should have 1
+/// - resolution is 0.1 µS/cm.
+#define GPLP8_MOIST_RESOLUTION 1
+/// @brief Sensor variable number; conductivity is stored in sensorValues[2].
+#define GPLP8_MOIST_VAR_NUM 2
+/// @brief Variable name in
+/// [ODM2 controlled vocabulary](http://vocabulary.odm2.org/variablename/);
+/// "specificConductance"
+#define GPLP8_MOIST_VAR_NAME "specificConductance"
+/// @brief Variable unit name in
+/// [ODM2 controlled vocabulary](http://vocabulary.odm2.org/units/);
+/// "microsiemenPerCentimeter" (µS/cm)
+#define GPLP8_MOIST_UNIT_NAME "microsiemenPerCentimeter"
+/// @brief Default variable short code; "GPLP8Moist"
+#define GPLP8_MOIST_DEFAULT_CODE "GPLP8Moist"
+/**@}*/
+
+/**
+ * @anchor sensor_gplp8_temp
+ * @name Temperature
+ * The temperature variable from a GroPoint Profile GPLP-8 
+ * - Range is 0°C to + 50°C
+ * - Accuracy is ± 0.2°C
+ *
+ * {{ @ref GroPointProfileGPLP8_Temp::GroPointProfileGPLP8_Temp }}
+ */
+/**@{*/
+/// @brief Decimals places in string representation; temperature should have 1 -
+/// resolution is 0.1°C.
+#define GPLP8_TEMP_RESOLUTION 1
+/// @brief Sensor variable number; temperature is stored in sensorValues[4].
+#define GPLP8_TEMP_VAR_NUM 4
+/// @brief Variable name in
+/// [ODM2 controlled vocabulary](http://vocabulary.odm2.org/variablename/);
+/// "temperature"
+#define GPLP8_TEMP_VAR_NAME "temperature"
+/// @brief Variable unit name in
+/// [ODM2 controlled vocabulary](http://vocabulary.odm2.org/units/);
+/// "degreeCelsius" (°C)
+#define GPLP8_TEMP_UNIT_NAME "degreeCelsius"
+/// @brief Default variable short code; "GPLP8Temp"
+#define GPLP8_TEMP_DEFAULT_CODE "GPLP8Temp"
+/**@}*/
+
+
+/* clang-format off */
+/**
+ * @brief The Sensor sub-class for the
+ * [GroPoint Profile GPLP8 probe](@ref sensor_gplp8).
+ *
+ * @ingroup sensor_gplp8
+ */
+/* clang-format on */
+class GroPointProfileGPLP8 : public GroPointParent {
+ public:
+    // Constructors with overloads
+    /**
+     * @brief Construct a new GroPoint GPLP8 object.
+     *
+     * @param modbusAddress The modbus address of the sensor.
+     * @param stream An Arduino data stream for modbus communication.  See
+     * [notes](@ref page_arduino_streams) for more information on what streams
+     * can be used.
+     * @param powerPin The pin on the mcu controlling power to the GPLP-8.
+     * Use -1 if it is continuously powered.
+     * @param powerPin2 The pin on the mcu controlling power to the RS485
+     * adapter, if it is different from that used to power the sensor.  Use -1
+     * or omit if not applicable.
+     * @param enablePin The pin on the mcu controlling the direction enable on
+     * the RS485 adapter, if necessary; use -1 or omit if not applicable.
+     * @note An RS485 adapter with integrated flow control is strongly
+     * recommended.
+     * @param measurementsToAverage The number of measurements to take and
+     * average before giving a "final" result from the sensor; optional with a
+     * default value of 1.
+     */
+    GroPointProfileGPLP8(byte modbusAddress, Stream* stream, int8_t powerPin,
+                    int8_t powerPin2 = -1, int8_t enablePin = -1,
+                    uint8_t measurementsToAverage = 1)
+        : GroPointParent(modbusAddress, stream, powerPin, powerPin2,
+                           enablePin, measurementsToAverage, GPLP8,
+                           "GroPointProfileGPLP8", GPLP8_NUM_VARIABLES,
+                           GPLP8_WARM_UP_TIME_MS, GPLP8_STABILIZATION_TIME_MS,
+                           GPLP8_MEASUREMENT_TIME_MS,
+                           GPLP8_INC_CALC_VARIABLES) {}
+    /**
+     * @copydoc GroPointProfileGPLP8::GroPointProfileGPLP8
+     */
+    GroPointProfileGPLP8(byte modbusAddress, Stream& stream, int8_t powerPin,
+                    int8_t powerPin2 = -1, int8_t enablePin = -1,
+                    uint8_t measurementsToAverage = 1)
+        : GroPointParent(modbusAddress, stream, powerPin, powerPin2,
+                           enablePin, measurementsToAverage, GPLP8,
+                           "GroPointProfileGPLP8", GPLP8_NUM_VARIABLES,
+                           GPLP8_WARM_UP_TIME_MS, GPLP8_STABILIZATION_TIME_MS,
+                           GPLP8_MEASUREMENT_TIME_MS,
+                           GPLP8_INC_CALC_VARIABLES) {}
+    /**
+     * @brief Destroy the GroPoint GPLP8 object
+     */
+    ~GroPointProfileGPLP8() {}
+};
+
+
+/* clang-format off */
+/**
+ * @brief The Variable sub-class used for the
+ * [dissolved oxygen concentration output](@ref sensor_gplp8_domgl) from a
+ * [GroPoint Profile GPLP8 probe](@ref sensor_gplp8).
+ *
+ * @ingroup sensor_gplp8
+ */
+/* clang-format on */
+
+/* clang-format off */
+/**
+ * @brief The Variable sub-class used for the
+ * [conductivity output](@ref sensor_gplp8_cond) from a
+ * [GroPoint Profile GPLP8 probe](@ref sensor_gplp8).
+ *
+ * @ingroup sensor_gplp8
+ */
+/* clang-format on */
+class GroPointProfileGPLP8_Moist : public Variable {
+ public:
+    /**
+     * @brief Construct a new GroPointProfileGPLP8_Moist object.
+     *
+     * @param parentSense The parent GroPointProfileGPLP8 providing the result
+     * values.
+     * @param uuid A universally unique identifier (UUID or GUID) for the
+     * variable; optional with the default value of an empty string.
+     * @param varCode A short code to help identify the variable in files;
+     * optional with a default value of "GPLP8Moist".
+     */
+    explicit GroPointProfileGPLP8_Moist(GroPointProfileGPLP8* parentSense,
+                                  const char*      uuid = "",
+                                  const char* varCode = GPLP8_MOIST_DEFAULT_CODE)
+        : Variable(parentSense, (const uint8_t)GPLP8_MOIST_VAR_NUM,
+                   (uint8_t)GPLP8_MOIST_RESOLUTION, GPLP8_MOIST_VAR_NAME,
+                   GPLP8_MOIST_UNIT_NAME, varCode, uuid) {}
+    /**
+     * @brief Construct a new GroPointProfileGPLP8_Moist object.
+     *
+     * @note This must be tied with a parent GroPointProfileGPLP8 before it can be
+     * used.
+     */
+    GroPointProfileGPLP8_Moist()
+        : Variable((const uint8_t)GPLP8_MOIST_VAR_NUM,
+                   (uint8_t)GPLP8_MOIST_RESOLUTION, GPLP8_MOIST_VAR_NAME,
+                   GPLP8_MOIST_UNIT_NAME, GPLP8_MOIST_DEFAULT_CODE) {}
+    /**
+     * @brief Destroy the GroPointProfileGPLP8_Moist object - no action needed.
+     */
+    ~GroPointProfileGPLP8_Moist() {}
+};
+
+/* clang-format off */
+/**
+ * @brief The Variable sub-class used for the
+ * [temperature output](@ref sensor_gplp8_temp) from a
+ * [GroPoint Profile GPLP8 probe](@ref sensor_gplp8).
+ *
+ * @ingroup sensor_gplp8
+ */
+/* clang-format on */
+class GroPointProfileGPLP8_Temp : public Variable {
+ public:
+    /**
+     * @brief Construct a new GroPointProfileGPLP8_Temp object.
+     *
+     * @param parentSense The parent GroPointProfileGPLP8 providing the result
+     * values.
+     * @param uuid A universally unique identifier (UUID or GUID) for the
+     * variable; optional with the default value of an empty string.
+     * @param varCode A short code to help identify the variable in files;
+     * optional with a default value of "GPLP8Temp".
+     */
+    explicit GroPointProfileGPLP8_Temp(GroPointProfileGPLP8* parentSense,
+                                  const char*      uuid = "",
+                                  const char* varCode = GPLP8_TEMP_DEFAULT_CODE)
+        : Variable(parentSense, (const uint8_t)GPLP8_TEMP_VAR_NUM,
+                   (uint8_t)GPLP8_TEMP_RESOLUTION, GPLP8_TEMP_VAR_NAME,
+                   GPLP8_TEMP_UNIT_NAME, varCode, uuid) {}
+    /**
+     * @brief Construct a new GroPointProfileGPLP8_Temp object.
+     *
+     * @note This must be tied with a parent GroPointProfileGPLP8 before it can be
+     * used.
+     */
+    GroPointProfileGPLP8_Temp()
+        : Variable((const uint8_t)GPLP8_TEMP_VAR_NUM,
+                   (uint8_t)GPLP8_TEMP_RESOLUTION, GPLP8_TEMP_VAR_NAME,
+                   GPLP8_TEMP_UNIT_NAME, GPLP8_TEMP_DEFAULT_CODE) {}
+    /**
+     * @brief Destroy the GroPointProfileGPLP8_Temp object - no action needed.
+     */
+    ~GroPointProfileGPLP8_Temp() {}
+};
+
+/**@}*/
+#endif  // SRC_SENSORS_GROPOINTPROFILEGPLP8_H_
