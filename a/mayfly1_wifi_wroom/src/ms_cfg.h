fafb4c8dd20dedd39dba54b7d02335c3a08b17be
neilh20 <neilh20+gitk1903@wLLw.net>
2023-07-11 15:17:10 -0700

    text changes

--- a/mayfly1_wifi_wroom/src/ms_cfg.h
+++ a/mayfly1_wifi_wroom/src/ms_cfg.h
@@ -17,8 +17,8 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 #ifndef mayfly1_wifi_wroom_ms_cfg_h
 #define mayfly1_wifi_wroom_ms_cfg_h
 
-#define WIFIID_SSID_DEF "ArthurGuestSsid" 
-#define WIFIPWD_DEF "Arthur8166"
+#define WIFIID_SSID_DEF "WiFiSsid" 
+#define WIFIPWD_DEF "pwd"
 // Defaults for data.envirodiy.org
 #define SENSIRION_SHT4X_UUID 1
 
@@ -28,13 +28,13 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 
 const char *UUIDs[] =                                                      // UUID array for device sensors
 {
-    "a61813cd-ac96-49a3-b374-fec5dffb42a4",   // Sequence number (EnviroDIY_Mayfly_SampleNum)
-    "597108b0-c147-49a2-a71d-704539d774ec",   // Battery voltage (EnviroDIY_Mayfly_Batt)
-    "09fbbfde-5b9a-48c9-88a3-39d68463e0af",   // Temperature (Sensirion_SHT40_Temperature)
-    "e1f4579e-2d6d-4382-a6e8-a8b054fb0263"    // Relative humidity (Sensirion_SHT40_Humidity)
+    "a61813cd-ac96-49a3-b374-fec5dffb42xx",   // Sequence number (EnviroDIY_Mayfly_SampleNum)
+    "597108b0-c147-49a2-a71d-704539d774xx",   // Battery voltage (EnviroDIY_Mayfly_Batt)
+    "09fbbfde-5b9a-48c9-88a3-39d68463e0xx",   // Temperature (Sensirion_SHT40_Temperature)
+    "e1f4579e-2d6d-4382-a6e8-a8b054fb02xx"    // Relative humidity (Sensirion_SHT40_Humidity)
 };
-const char *registrationToken = "945af9f2-709c-4d09-ab49-fc7070c33793";   // Device registration token
-const char *samplingFeature = "63bec1f9-b704-435c-bf67-4b8a04a0b6d2";     // Sampling feature UUID
+const char *registrationToken = "945af9f2-709c-4d09-ab49-fc7070c337xx";   // Device registration token
+const char *samplingFeature =   "63bec1f9-b704-435c-bf67-4b8a04a0b6xx";   // Sampling feature UUID
 
 
 #endif  // mayfly1_wifi_wroom_ms_cfg_h
--- a/mayfly2_wifi_s6b/src/ms_cfg.h
+++ a/mayfly2_wifi_s6b/src/ms_cfg.h
@@ -19,8 +19,8 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 #ifndef mayfly2_wifi_s6b_ms_cfg_h
 #define mayfly2_wifi_s6b_ms_cfg_h
 
-#define WIFIID_SSID_DEF "ArthurGuestSsid" 
-#define WIFIPWD_DEF "Arthur8166"
+#define WIFIID_SSID_DEF "WiFiSsid" 
+#define WIFIPWD_DEF "WiFi pwd"
 // Defaults for data.envirodiy.org
 #define SENSIRION_SHT4X_UUID 1
 
@@ -29,12 +29,12 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 
 const char *UUIDs[] =                                                      // UUID array for device sensors
 {
-    "c317f9ee-d0ca-410b-b051-11cb683c8902",   // Sequence number (EnviroDIY_Mayfly_SampleNum)
-    "135714b5-30fa-4fd9-83f4-3d70edce33b4",   // Battery voltage (EnviroDIY_Mayfly_Batt)
-    "f24f4af6-c469-4597-893b-9e9eee9e2b89",   // Temperature (Sensirion_SHT40_Temperature)
-    "2b8d3edb-e0ac-455e-99a4-09b2a859d604"    // Relative humidity (Sensirion_SHT40_Humidity)
+    "c317f9ee-d0ca-410b-b051-11cb683c89xx",   // Sequence number (EnviroDIY_Mayfly_SampleNum)
+    "135714b5-30fa-4fd9-83f4-3d70edce3xxx",   // Battery voltage (EnviroDIY_Mayfly_Batt)
+    "f24f4af6-c469-4597-893b-9e9eee9e2bxx",   // Temperature (Sensirion_SHT40_Temperature)
+    "2b8d3edb-e0ac-455e-99a4-09b2a859d6xx"    // Relative humidity (Sensirion_SHT40_Humidity)
 };
-const char *registrationToken = "8467bd56-3659-41a7-9fad-bd0c93b6bf33";   // Device registration token
-const char *samplingFeature = "8eed720a-345d-4587-9a68-24cd77771c17";     // Sampling feature UUID
+const char *registrationToken = "8467bd56-3659-41a7-9fad-bd0c93b6bfxx";   // Device registration token
+const char *samplingFeature   = "8eed720a-345d-4587-9a68-24cd77771cxx";   // Sampling feature UUID
 
 #endif  // mayfly2_wifi_s6b_ms_cfg_h
