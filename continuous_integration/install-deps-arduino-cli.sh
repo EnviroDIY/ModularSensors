63caa413abadb1fa92dad2ec9a4300b42c23b352
Sara Damiano <sdamiano@stroudcenter.org>
2024-06-14 13:56:35 -0400

    Remove cores from lib installer
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- continuous_integration/install-deps-arduino-cli.sh
+++ continuous_integration/install-deps-arduino-cli.sh
@@ -9,33 +9,6 @@ set -e
 echo "\n\e[32mCurrent Arduino CLI version:\e[0m"
 arduino-cli version
 
-echo "::group::Installing Platforms and Frameworks"
-echo "\n\e[32mUpdating the core index\e[0m"
-arduino-cli --config-file continuous_integration/arduino_cli.yaml core update-index
-
-echo "\n\e[32mInstalling the Arduino AVR Core\e[0m"
-arduino-cli --config-file continuous_integration/arduino_cli.yaml core install arduino:avr
-
-echo "\n\e[32mInstalling the EnviroDIY AVR Core\e[0m"
-arduino-cli --config-file continuous_integration/arduino_cli.yaml core install EnviroDIY:avr
-
-echo "\n\e[32mInstalling the Arduino SAMD Core\e[0m"
-arduino-cli --config-file continuous_integration/arduino_cli.yaml core install arduino:samd
-
-echo "\n\e[32mInstalling the Adafruit SAMD Core\e[0m"
-arduino-cli --config-file continuous_integration/arduino_cli.yaml core install adafruit:samd
-
-echo "\n\e[32mUpdating the core index\e[0m"
-arduino-cli --config-file continuous_integration/arduino_cli.yaml core update-index
-
-echo "\n\e[32mUpgrading all cores\e[0m"
-arduino-cli --config-file continuous_integration/arduino_cli.yaml core upgrade
-
-echo "\n\e[32mCurrently installed cores:\e[0m"
-arduino-cli --config-file continuous_integration/arduino_cli.yaml core list
-echo "::endgroup::"
-
-
 echo "::group::Installing Libraries"
 echo "\n\e[32mUpdating the library index\e[0m"
 arduino-cli --config-file continuous_integration/arduino_cli.yaml lib update-index
