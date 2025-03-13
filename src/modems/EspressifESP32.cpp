38a724d45da37fb085b6ac47c68a7b2a3ec3674f
Sara Damiano <sdamiano@stroudcenter.org>
2025-03-13 11:52:48 -0400

    Split espressif modules
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/modems/EspressifESP32.cpp
+++ src/modems/EspressifESP32.cpp
@@ -1,23 +1,23 @@
 /**
- * @file EspressifESP8266.cpp
+ * @file EspressifESP32.cpp
  * @copyright Stroud Water Research Center
  * Part of the EnviroDIY ModularSensors library for Arduino.
  * This library is published under the BSD-3 license.
  * @author Sara Geleskie Damiano <sdamiano@stroudcenter.org>
  *
- * @brief Implements the EspressifESP8266 class.
+ * @brief Implements the EspressifESP32 class.
  */
 
 // Included Dependencies
-#include "EspressifESP8266.h"
+#include "EspressifESP32.h"
 #include "LoggerModemMacros.h"
 
 // Constructors
-EspressifESP8266::EspressifESP8266(Stream* modemStream, int8_t powerPin,
-                                   int8_t modemResetPin, const char* ssid,
-                                   const char* pwd)
+EspressifESP32::EspressifESP32(Stream* modemStream, int8_t powerPin,
+                               int8_t modemResetPin, const char* ssid,
+                               const char* pwd)
     : Espressif(modemStream, powerPin, modemResetPin, ssid, pwd),
-#if defined(MS_ESPRESSIFESP8266_DEBUG_DEEP)
+#if defined(MS_ESPRESSIFESP32_DEBUG_DEEP)
       _modemATDebugger(*modemStream, MS_SERIAL_OUTPUT),
       gsmModem(_modemATDebugger)
 #else
@@ -27,43 +27,43 @@ EspressifESP8266::EspressifESP8266(Stream* modemStream, int8_t powerPin,
 }
 
 // Destructor
-EspressifESP8266::~EspressifESP8266() {}
+EspressifESP32::~EspressifESP32() {}
 
-MS_IS_MODEM_AWAKE(EspressifESP8266);
-MS_MODEM_WAKE(EspressifESP8266);
+MS_IS_MODEM_AWAKE(EspressifESP32);
+MS_MODEM_WAKE(EspressifESP32);
 
-MS_MODEM_CONNECT_INTERNET(EspressifESP8266, ESPRESSIF_RECONNECT_TIME_MS);
-MS_MODEM_DISCONNECT_INTERNET(EspressifESP8266);
-MS_MODEM_IS_INTERNET_AVAILABLE(EspressifESP8266);
+MS_MODEM_CONNECT_INTERNET(EspressifESP32, ESPRESSIF_RECONNECT_TIME_MS);
+MS_MODEM_DISCONNECT_INTERNET(EspressifESP32);
+MS_MODEM_IS_INTERNET_AVAILABLE(EspressifESP32);
 
-MS_MODEM_CREATE_CLIENT(EspressifESP8266);
-MS_MODEM_CREATE_SECURE_CLIENT(EspressifESP8266);
+MS_MODEM_CREATE_CLIENT(EspressifESP32);
+MS_MODEM_CREATE_SECURE_CLIENT(EspressifESP32);
 
-MS_MODEM_GET_NIST_TIME(EspressifESP8266);
+MS_MODEM_GET_NIST_TIME(EspressifESP32);
 
-MS_MODEM_GET_MODEM_SIGNAL_QUALITY(EspressifESP8266);
-MS_MODEM_GET_MODEM_BATTERY_DATA(EspressifESP8266);
-MS_MODEM_GET_MODEM_TEMPERATURE_DATA(EspressifESP8266);
+MS_MODEM_GET_MODEM_SIGNAL_QUALITY(EspressifESP32);
+MS_MODEM_GET_MODEM_BATTERY_DATA(EspressifESP32);
+MS_MODEM_GET_MODEM_TEMPERATURE_DATA(EspressifESP32);
 
-bool EspressifESP8266::modemSleepFxn(void) {
+bool EspressifESP32::modemSleepFxn(void) {
     // Use this if you have an MCU pin connected to the ESP's reset pin to wake
     // from deep sleep.  We'll also put it in deep sleep before yanking power.
     if (_modemResetPin >= 0 || _powerPin >= 0) {
-        MS_DBG(F("Requesting deep sleep for ESP8266"));
+        MS_DBG(F("Requesting deep sleep for ESP32"));
         bool retVal = gsmModem.poweroff();
         if (_modemSleepRqPin >= 0) {
             digitalWrite(_modemSleepRqPin, !_wakeLevel);
         }
         return retVal;
     } else {  // DON'T go to sleep if we can't wake up!
-        MS_DEEP_DBG(F("No pins for sleeping the ESP8266. Hopefully it's in the "
+        MS_DEEP_DBG(F("No pins for sleeping the ESP32. Hopefully it's in the "
                       "state you want."));
         return true;
     }
 }
 
 // Set up the light-sleep status pin, if applicable
-bool EspressifESP8266::extraModemSetup(void) {
+bool EspressifESP32::extraModemSetup(void) {
     if (_modemSleepRqPin >= 0) { digitalWrite(_modemSleepRqPin, !_wakeLevel); }
     gsmModem.init();
     _modemName = gsmModem.getModemName();
