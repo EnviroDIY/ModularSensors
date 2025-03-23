8cda60e9aad4350f750b265fd9146aa7ad3fde1b
Sara Damiano <sdamiano@stroudcenter.org>
2025-03-23 11:08:44 -0400

    Add country code for ESP32
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/modems/EspressifESP32.cpp
+++ src/modems/EspressifESP32.cpp
@@ -69,5 +69,19 @@ bool EspressifESP32::extraModemSetup(void) {
     if (_modemSleepRqPin >= 0) { digitalWrite(_modemSleepRqPin, !_wakeLevel); }
     gsmModem.init();
     _modemName = gsmModem.getModemName();
+    // AT+CWCOUNTRY=<country_policy>,<country_code>,<start_channel>,<total_channel_count>
+    // <country_policy>:
+    //     0: will change the county code to be the same as the AP that the
+    //     ESP32 is connected to.
+    //     1: the country code will not change, always be the one set by
+    //     command.
+    // <country_code>: country code. Maximum length: 3 characters. Refer to ISO
+    //     3166-1 alpha-2 for country codes.
+    // <start_channel>: the channel number to start. Range: [1,14].
+    // <total_channel_count>: total number of channels.
+    gsmModem.sendAT(
+        GF("+CWCOUNTRY=0,\"US\",1,13"));  // Set country code to default to US,
+                                          // but allow to change if the AP is
+    gsmModem.waitResponse();
     return true;
 }
