43f13bca14188c5c3421066017de952a0b34d963
Sara Damiano <sdamiano@stroudcenter.org>
2025-03-25 11:38:25 -0400

    Add ThingSpeak metadata update
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/publishers/ThingSpeakPublisher.cpp
+++ src/publishers/ThingSpeakPublisher.cpp
@@ -18,8 +18,11 @@
 // Constant values for MQTT publish
 // I want to refer to these more than once while ensuring there is only one copy
 // in memory
-const char* ThingSpeakPublisher::mqttServer = "mqtt3.thingspeak.com";
-const int   ThingSpeakPublisher::mqttPort   = 1883;
+const char* ThingSpeakPublisher::apiHost             = "api.thingspeak.com";
+const int   ThingSpeakPublisher::apiPort             = 443;
+const char* ThingSpeakPublisher::channelMetaResource = "/channels/";
+const char* ThingSpeakPublisher::mqttServer          = "mqtt3.thingspeak.com";
+const int   ThingSpeakPublisher::mqttPort            = 1883;
 
 
 // Constructors
@@ -76,6 +79,11 @@ void ThingSpeakPublisher::setChannelID(const char* thingSpeakChannelID) {
     _thingSpeakChannelID = thingSpeakChannelID;
 }
 
+
+void ThingSpeakPublisher::setRESTAPIKey(const char* thingSpeakAPIKey) {
+    _thingSpeakAPIKey = thingSpeakAPIKey;
+}
+
 // Sets all ThingSpeak parameters
 void ThingSpeakPublisher::setThingSpeakParams(
     const char* thingSpeakClientName, const char* thingSpeakMQTTUser,
@@ -123,8 +131,8 @@ int16_t ThingSpeakPublisher::publishData(Client* outClient, bool) {
                  "ThingSpeak channel!"));
         MS_DBG(F("Only the first 8 fields worth of data will be sent."));
     }
-    uint8_t numChannels = min(_baseLogger->getArrayVarCount(), 8);
-    MS_DBG(numChannels, F("fields will be sent to ThingSpeak"));
+    uint8_t numFields = min(_baseLogger->getArrayVarCount(), 8);
+    MS_DBG(numFields, F("fields will be sent to ThingSpeak"));
 
     // Create a new buffer for the **topic**
     char topicBuffer[42] = "channels/";
@@ -163,8 +171,8 @@ int16_t ThingSpeakPublisher::publishData(Client* outClient, bool) {
             Logger::formatDateTime_ISO8601(Logger::markedLocalUnixTime)
                 .c_str());
 
-        char tempBuffer[2] = "";  // for the channel number
-        for (uint8_t i = 0; i < numChannels; i++) {
+        char tempBuffer[2] = "";  // for the field number
+        for (uint8_t i = 0; i < numFields; i++) {
             txBufferAppend("&field");
             itoa(i + 1, tempBuffer, 10);  // BASE 10
             txBufferAppend(tempBuffer);
@@ -195,4 +203,100 @@ int16_t ThingSpeakPublisher::publishData(Client* outClient, bool) {
     _mqttClient.disconnect();
     MS_DBG(F("Disconnected after"), MS_PRINT_DEBUG_TIMER, F("ms"));
     return retVal;
+}
+
+// This upddates your channel field names on ThingSpeak
+int16_t ThingSpeakPublisher::publishMetadata(Client* outClient) {
+    // Create a buffer for the portions of the request and response
+    char     tempBuffer[37] = "";
+    uint16_t did_respond    = 0;
+    uint8_t  numFields      = min(_baseLogger->getArrayVarCount(), 8);
+
+    // Open a TCP/IP connection to ThingSpeak
+    MS_DBG(F("Connecting client"));
+    MS_START_DEBUG_TIMER;
+    if (outClient->connect(apiHost, apiPort)) {
+        MS_DBG(F("Client connected after"), MS_PRINT_DEBUG_TIMER, F("ms"));
+        txBufferInit(outClient);
+
+        // copy the initial put header into the tx buffer
+        txBufferAppend(putHeader);
+
+        // add in rest of the URL for the channel metadata
+        txBufferAppend(channelMetaResource);
+        txBufferAppend(_thingSpeakChannelID);
+        txBufferAppend(".json?api_key=");
+        txBufferAppend(_thingSpeakAPIKey);
+        txBufferAppend("&name=");
+        txBufferAppend(_baseLogger->getLoggerID());
+
+        char tempBuffer[2] = "";  // for the field number
+        for (uint8_t i = 0; i < numFields; i++) {
+            txBufferAppend("&field");
+            itoa(i + 1, tempBuffer, 10);  // BASE 10
+            txBufferAppend(tempBuffer);
+            txBufferAppend('=');
+            txBufferAppend(_baseLogger->getVarCodeAtI(i).c_str());
+        }
+
+        // add the rest of the HTTP GET headers to the outgoing buffer
+        txBufferAppend(HTTPtag);
+        txBufferAppend(hostHeader);
+        txBufferAppend(apiHost);
+        txBufferAppend('\r');
+        txBufferAppend('\n');
+        txBufferAppend('\r');
+        txBufferAppend('\n');
+
+        // Flush the complete request
+        txBufferFlush();
+
+        // Wait 10 seconds for a response from the server
+        uint32_t start = millis();
+        while ((millis() - start) < 10000L && outClient->connected() &&
+               outClient->available() < 12) {
+            delay(10);
+        }
+
+        // Read only the first 12 characters of the response
+        // We're only reading as far as the http code, anything beyond that
+        // we don't care about.
+        did_respond = outClient->readBytes(tempBuffer, 12);
+#if defined(MS_OUTPUT) || defined(MS_2ND_OUTPUT)
+        // throw the rest of the response into the tx buffer so we can debug it
+        txBufferInit(nullptr);
+        txBufferAppend(tempBuffer, 12, true);
+        while (outClient->available()) {
+            char c = outClient->read();
+            txBufferAppend(c);
+        }
+        txBufferFlush();
+#endif
+
+        // Close the TCP/IP connection
+        MS_DBG(F("Stopping client"));
+        MS_RESET_DEBUG_TIMER;
+        outClient->stop();
+        MS_DBG(F("Client stopped after"), MS_PRINT_DEBUG_TIMER, F("ms"));
+    } else {
+        PRINTOUT(F("\n -- Unable to Establish Connection to ThingSpeak --"));
+    }
+
+    // Process the HTTP response
+    int16_t responseCode = 0;
+    if (did_respond > 0) {
+        char responseCode_char[4];
+        responseCode_char[3] = 0;
+        for (uint8_t i = 0; i < 3; i++) {
+            responseCode_char[i] = tempBuffer[i + 9];
+        }
+        responseCode = atoi(responseCode_char);
+    } else {
+        responseCode = 504;
+    }
+
+    PRINTOUT(F("\n-- Response Code --"));
+    PRINTOUT(responseCode);
+
+    return responseCode;
 }
--- src/publishers/ThingSpeakPublisher.h
+++ src/publishers/ThingSpeakPublisher.h
@@ -162,6 +162,19 @@ class ThingSpeakPublisher : public dataPublisher {
     void setChannelID(const char* thingSpeakChannelID);
 
     /**
+     * @brief Set the ThingSpeak user REST API key
+     *
+     * This is only used to update the field names on the ThingSpeak channel
+     * during the updateMetadata call at boot up. The *user* REST API key is
+     * **NOT** the same as your *channel* API keys or your MQTT connection
+     * credentials.  Find this key in your ThingSpeak account under Account > My
+     * Profile.
+     *
+     * @param thingSpeakAPIKey The ThingSpeak user REST API key
+     */
+    void setRESTAPIKey(const char* thingSpeakAPIKey);
+
+    /**
      * @brief Sets all 4 ThingSpeak parameters
      *
      * @param thingSpeakClientName The client name for your MQTT device. This is
@@ -217,6 +230,7 @@ class ThingSpeakPublisher : public dataPublisher {
      * @return The PubSubClient status code of the response.
      */
     int16_t publishData(Client* outClient, bool forceFlush = false) override;
+    int16_t publishMetadata(Client* outClient) override;
 
  protected:
     /**
@@ -225,6 +239,10 @@ class ThingSpeakPublisher : public dataPublisher {
      *
      * @{
      */
+    static const char* apiHost;  ///< The REST API host
+    static const int   apiPort;  ///< The REST API port
+    static const char*
+        channelMetaResource;  ///< The REST API resource to put metadata to
     static const char* mqttServer;  ///< The MQTT server
     static const int   mqttPort;    ///< The MQTT port
                                     /**@}*/
@@ -249,6 +267,10 @@ class ThingSpeakPublisher : public dataPublisher {
      * @brief The channel ID for ThingSpeak
      */
     const char* _thingSpeakChannelID = nullptr;
+    /**
+     * @brief The ThingSpeak REST API key
+     */
+    const char* _thingSpeakAPIKey = nullptr;
     /**
      * @brief Internal reference to the PubSubClient instance for MQTT
      * communication.
