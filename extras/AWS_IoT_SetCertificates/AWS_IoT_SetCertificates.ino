86e667f474aebc73068d75775cf7a52d2fc6362e
Sara Damiano <sdamiano@stroudcenter.org>
2025-07-02 12:31:28 -0400

    Update for certs program
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- extras/AWS_IoT_SetCertificates/AWS_IoT_SetCertificates.ino
+++ extras/AWS_IoT_SetCertificates/AWS_IoT_SetCertificates.ino
@@ -29,8 +29,10 @@
 // #define TINY_GSM_MODEM_A7672X
 // #define TINY_GSM_MODEM_ESP32
 // #define TINY_GSM_MODEM_SEQUANS_MONARCH
+// #define TINY_GSM_MODEM_BG96
+// #define TINY_GSM_MODEM_XBEE
 
-#define TINY_GSM_KEEPALIVE 180
+#define TINY_GSM_TCP_KEEP_ALIVE 180
 
 // Set serial for debug console (to the Serial Monitor, default speed 115200)
 #define SerialMon Serial
@@ -47,10 +49,12 @@ SoftwareSerial SerialAT(2, 3);  // RX, TX
 #endif
 
 // See all AT commands, if wanted
+// WARNING: At high baud rates, incoming data may be lost when dumping AT
+// commands
 // #define DUMP_AT_COMMANDS
 
 // Define the serial console for debug prints, if needed
-// #define TINY_GSM_DEBUG SerialMon
+#define TINY_GSM_DEBUG SerialMon
 
 // Range to attempt to autobaud
 // NOTE:  DO NOT AUTOBAUD in production code.  Once you've established
@@ -93,6 +97,44 @@ uint16_t port = 8883;
 const char* clientId = THING_NAME;
 
 static const char topicInit[] TINY_GSM_PROGMEM = THING_NAME "/init";
+static const char msgInit[] TINY_GSM_PROGMEM   = "{\"" THING_NAME
+                                               "\":\"connected\"}";
+
+// whether to print certs after uploading
+// not all modules support printing the content of certificates after uploading
+// them
+bool print_certs  = false;
+bool delete_certs = false;
+
+// NOTE: some modems (SIM70xx modules) suggest that you delete the
+// certificate file from the file system after converting the certificate so
+// that they cannot be read back.  On other modules (SIM7600, A7672, ESP32,
+// BG96, XBee) the certificate must be in the file system to be used and cannot
+// be deleted.
+#if defined(TINY_GSM_MODEM_ESP32) && defined(TINY_GSM_MODEM_BG96)
+delete_certs = false;
+#endif
+
+
+// The certificates should generally be formatted as ".pem", ".der", or (for
+// some modules) ".p7b" files.
+
+// For Espressif modules, only two certificate sets are supported and the
+// certificates must be named "client_ca.{0|1}", "client_cert.{0|1}", or
+// "client_key.{0|1}"
+#ifdef TINY_GSM_MODEM_ESP32
+const char* root_ca_name     = "client_ca.1";
+const char* client_cert_name = "client_cert.1";
+const char* client_key_name  = "client_key.1";
+#else
+// For most modules the actual filename doesn't matter much but it CANNOT
+// HAVE SPACES and should be less than 64 characters.
+// NOTE: The certificate names as they are downloaded from AWS IoT Core
+// are often too long for the modem to handle. Pick something shorter.
+const char* root_ca_name     = "AmazonRootCA1.pem";
+const char* client_cert_name = THING_NAME "-certificate.pem.crt";
+const char* client_key_name  = THING_NAME "-private-key.pem.key";
+#endif
 
 // Just in case someone defined the wrong thing..
 #if TINY_GSM_USE_GPRS && not defined TINY_GSM_MODEM_HAS_GPRS
@@ -116,7 +158,7 @@ TinyGsm        modem(debugger);
 TinyGsm modem(SerialAT);
 #endif
 
-TinyGsmClientSecure secureClient(modem);
+TinyGsmClientSecure secureClient(modem, (uint8_t)0);
 PubSubClient        mqtt(secureClient);
 
 #define LED_PIN 13
@@ -134,14 +176,15 @@ boolean mqttConnect() {
     boolean status = mqtt.connect(clientId);
 
     if (status == false) {
-        SerialMon.println(" ...failed to connect to MQTT broker!");
+        SerialMon.println(" ...failed to connect to AWS IoT MQTT broker!");
         return false;
     }
     SerialMon.println(" ...success");
 
     SerialMon.print("Publishing a message to ");
     SerialMon.println(topicInit);
-    mqtt.publish(topicInit, "{\"" THING_NAME "\":\"connected\"}");
+    bool got_pub = mqtt.publish(topicInit, msgInit);
+    SerialMon.println(got_pub ? "published" : "failed to publish");
 
     return mqtt.connected();
 }
@@ -152,14 +195,16 @@ void setup() {
     SerialMon.begin(921600);
     delay(10);
 
+    while (!SerialMon && millis() < 10000L) {}
+
     pinMode(LED_PIN, OUTPUT);
 
     // !!!!!!!!!!!
-    // Set your reset, enable, power pins here
-    // pins
-    int8_t _modemPowerPin   = 18;  // Mayfly 1.1
-    int8_t _modemSleepRqPin = 23;  // Mayfly 1.1
-    int8_t _modemStatusPin  = 19;  // Mayfly 1.1
+    // Set your reset, enable, and power pins here
+    // pin numbers are for Mayfly 1.1 and Stonefly
+    int8_t _modemPowerPin   = 18;  // Mayfly 1.1 & Stonefly
+    int8_t _modemSleepRqPin = 23;  // Mayfly 1.1 & Stonefly
+    int8_t _modemStatusPin  = 19;  // Mayfly 1.1 & Stonefly
     // set pin modes
     pinMode(_modemPowerPin, OUTPUT);
     pinMode(_modemSleepRqPin, OUTPUT);
@@ -167,18 +212,27 @@ void setup() {
     // wake settings
     uint32_t _wakeDelay_ms = 1000L;  // SIM7080G
     uint32_t _wakePulse_ms = 1100L;  // SIM7080G
-    bool     _wakeLevel =
+#ifdef TINY_GSM_MODEM_SIM7080
+    bool _wakeLevel =
         HIGH;  // SIM7080G is low, but EnviroDIY LTE Bee inverts it
+#else
+    bool _wakeLevel = LOW;
+#endif
 
     // start with the modem powered off
     DBG(F("Starting with modem powered down. Wait..."));
+#ifdef TINY_GSM_MODEM_SIM7080
     digitalWrite(_modemSleepRqPin, !_wakeLevel);
+#endif
     digitalWrite(_modemPowerPin, LOW);
     delay(5000L);
 
     // power the modem
     DBG(F("Powering modem with pin"), _modemPowerPin, F("and waiting"),
         _wakeDelay_ms, F("ms for power up."));
+#ifdef TINY_GSM_MODEM_XBEE
+    digitalWrite(_modemSleepRqPin, _wakeLevel);
+#endif
     digitalWrite(_modemPowerPin, HIGH);
 
 #ifndef TINY_GSM_MODEM_ESP32
@@ -190,15 +244,18 @@ void setup() {
     digitalWrite(_modemSleepRqPin, _wakeLevel);
     delay(_wakePulse_ms);  // >1s
     digitalWrite(_modemSleepRqPin, !_wakeLevel);
-    // !!!!!!!!!!!
 #endif
+    // !!!!!!!!!!!
 
     DBG("Wait...");
     delay(500L);
 
-    // Set GSM module baud rate
+// Set GSM module baud rate
+#ifndef TINY_GSM_MODEM_XBEE
     TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
-    // SerialAT.begin(57600);
+#else
+    SerialAT.begin(9600);
+#endif
 
     // Restart takes quite some time
     // To skip it, call init() instead of restart()
@@ -244,8 +301,6 @@ void setup() {
     SerialMon.print(F("Setting SSID/password..."));
     if (!modem.networkConnect(wifiSSID, wifiPass)) {
         SerialMon.println(" ...failed to connect to WiFi!");
-        delay(10000);
-        return;
     }
     SerialMon.println(" ...success");
 #endif
@@ -257,79 +312,59 @@ void setup() {
     const char* client_cert = AWS_CLIENT_CERTIFICATE;
     const char* client_key  = AWS_CLIENT_PRIVATE_KEY;
 
-    // The certificates should generally be formatted as ".pem", ".der", or (for
-    // some modules) ".p7b" files.
-
-    // For most modules the actual filename doesn't matter much but it CANNOT
-    // HAVE SPACES and should be less than 64 characters.
-    // NOTE: The certificate names as they are downloaded from AWS IoT Core
-    // are often too long for the modem to handle. Pick something shorter, like
-    // your thing name.
-
-    // For Espressif modules, only two certificate sets are supported and the
-    // certificates must be named "client_ca.{0|1}", "client_cert.{0|1}", or
-    // "client_key.{0|1}"
-#ifdef TINY_GSM_MODEM_ESP32
-    const char* root_ca_name     = "client_ca.0";
-    const char* client_cert_name = "client_cert.0";
-    const char* client_key_name  = "client_key.0";
-#else
-    const char* root_ca_name     = "AmazonRootCA1.pem";
-    const char* client_cert_name = THING_NAME "-certificate.pem.crt";
-    const char* client_key_name  = THING_NAME "-private-key.pem.key";
-#endif
-
+#ifdef TINY_GSM_MODEM_CAN_LOAD_CERTS
     // ======================== CA CERTIFICATE LOADING ========================
     bool ca_cert_success = true;
     // add the server's certificate authority certificate to the modem
     SerialMon.print("Loading Certificate Authority Certificate");
     ca_cert_success &= modem.loadCertificate(root_ca_name, root_ca,
                                              strlen(root_ca));
-    if (!ca_cert_success) {  // modem.init();
+    if (!ca_cert_success) {
         SerialMon.println(" ...failed to load CA certificate!");
         delay(10000);
         return;
     }
     SerialMon.println(" ...success");
-    // print out the certificate to make sure it matches
-    SerialMon.println(
-        "Printing Certificate Authority Certificate to confirm it matches");
-    modem.printCertificate(root_ca_name, SerialMon);
+    if (print_certs) {
+        // print out the certificate to make sure it matches
+        SerialMon.println(
+            "Printing Certificate Authority Certificate to confirm it matches");
+        modem.printCertificate(root_ca_name, SerialMon);
+    }
     // convert the certificate to the modem's format
     SerialMon.print("Converting Certificate Authority Certificate");
     ca_cert_success &= modem.convertCACertificate(root_ca_name);
-    if (!ca_cert_success) {  // modem.init();
+    if (!ca_cert_success) {
         SerialMon.println(" ...failed to convert CA certificate!");
         delay(10000);
         return;
     }
     SerialMon.println(" ...success");
 
-// NOTE: some modems suggest that you delete the certificate file from the
-// file system after converting the certificate.  Do NOT do this with an
-// ESP32!  The certificate must be in the file system to be used. On Espressif
-// modules, the certificate must be in the file system to be used.
-#ifndef TINY_GSM_MODEM_ESP32
-    // ca_cert_success &= modem.deleteCertificate(root_ca_name);
-#endif
+    if (delete_certs) {
+        ca_cert_success &= modem.deleteCertificate(root_ca_name);
+    }
+    delay(1000);
 
-    // ======================= CLIENT CERTIFICATE LOADING
-    // ======================= add the client's certificate and private key to
-    // the modem
+    // ===================== CLIENT CERTIFICATE LOADING =====================
     bool client_cert_success = true;
+    // add the client's certificate and private key to the modem
     SerialMon.print("Loading Client Certificate");
     client_cert_success &= modem.loadCertificate(client_cert_name, client_cert,
                                                  strlen(client_cert));
-    ca_cert_success &= modem.loadCertificate(root_ca_name, root_ca,
-                                             strlen(root_ca));
-    // print out the certificate to make sure it matches
-    modem.printCertificate(client_cert_name, SerialMon);
+    if (print_certs) {
+        // print out the certificate to make sure it matches
+        modem.printCertificate(client_cert_name, SerialMon);
+    }
+    delay(1000);
     SerialMon.print(" and Client Private Key ");
     client_cert_success &= modem.loadCertificate(client_key_name, client_key,
                                                  strlen(client_key));
-    // print out the certificate to make sure it matches
-    modem.printCertificate(client_key_name, SerialMon);
-    if (!ca_cert_success) {  // modem.init();
+    if (print_certs) {
+        // print out the certificate to make sure it matches
+        modem.printCertificate(client_key_name, SerialMon);
+    }
+    if (!client_cert_success) {
         SerialMon.println(" ...failed to load client certificate or key!");
         delay(10000);
         return;
@@ -338,27 +373,26 @@ void setup() {
     // convert the client certificate pair to the modem's format
     client_cert_success &= modem.convertClientCertificates(client_cert_name,
                                                            client_key_name);
-    ca_cert_success &= modem.convertCACertificate(root_ca_name);
-    if (!ca_cert_success) {  // modem.init();
+    if (!client_cert_success) {
         SerialMon.println(" ...failed to convert client certificate and key!");
         delay(10000);
         return;
     }
     SerialMon.println(" ...success");
 
-    // NOTE: some modems suggest that you delete the certificate file from the
-    // file system after converting the certificate.  Do NOT do this with an
-    // ESP32!  On Espressif modules, the certificate must be in the file system
-    // to be used.
-#ifndef TINY_GSM_MODEM_ESP32
-    // cert_success &= modem.deleteCertificate(client_cert_name);
-    // cert_success &= modem.deleteCertificate(client_key_name);
+    if (delete_certs) {
+        client_cert_success &= modem.deleteCertificate(client_cert_name);
+        client_cert_success &= modem.deleteCertificate(client_key_name);
+    }
+    delay(1000);
 #endif
 
-    // =================== SET CERTIFICATES FOR THE CONNECTION
-    // =================== AWS IoT Core requires mutual authentication
+    // ================= SET CERTIFICATES FOR THE CONNECTION =================
+    // AWS IoT Core requires mutual authentication
     DBG("Requiring mutual authentication on socket");
     secureClient.setSSLAuthMode(SSLAuthMode::MUTUAL_AUTHENTICATION);
+    DBG("Requesting TLS 1.3 on socket");
+    secureClient.setSSLVersion(SSLVersion::TLS1_3);
     // attach the uploaded certificates to the secure client
     DBG("Assigning", root_ca_name, "as certificate authority on socket");
     secureClient.setCACertName(root_ca_name);
@@ -374,7 +408,7 @@ void setup() {
     modem.gprsConnect(apn, gprsUser, gprsPass);
 #endif
     SerialMon.print("Waiting for network...");
-    if (!modem.waitForNetwork()) {
+    if (!modem.waitForNetwork(300000L)) {
         SerialMon.println(" ...failed to connect to network!");
         delay(10000);
         return;
@@ -398,10 +432,15 @@ void setup() {
     if (modem.isGprsConnected()) { SerialMon.println("GPRS connected"); }
 #endif
 
+#ifdef TINY_GSM_MODEM_HAS_NTP
     // enable/force time sync with NTP server
     // This is **REQUIRED** for validated SSL connections
     DBG("Enabling time sync with NTP server");
-    modem.NTPServerSync("pool.ntp.org", -5);
+    modem.NTPServerSync("pool.ntp.org", -4);
+
+    String time = modem.getGSMDateTime(TinyGSMDateTimeFormat::DATE_FULL);
+    DBG("Current Network Time:", time);
+#endif
 
     // MQTT Broker setup
     mqtt.setServer(broker, port);
@@ -445,11 +484,11 @@ void loop() {
         SerialMon.println("=== MQTT NOT CONNECTED ===");
         // Reconnect every 10 seconds
         uint32_t t = millis();
-        if (t - lastReconnectAttempt > 10000L) {
+        if (t - lastReconnectAttempt > 30000L) {
             lastReconnectAttempt = t;
             if (mqttConnect()) { lastReconnectAttempt = 0; }
         }
-        delay(100);
+        delay(5000L);
         return;
     }
 
