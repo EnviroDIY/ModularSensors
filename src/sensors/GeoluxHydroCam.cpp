0b52e0f0c2dd8ea7ec61b32fecac15c8a85a9d10
Sara Damiano <sdamiano@stroudcenter.org>
2025-09-25 14:57:04 -0400

    Fix hydrocam return value on start measurement
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/sensors/GeoluxHydroCam.cpp
+++ src/sensors/GeoluxHydroCam.cpp
@@ -187,7 +187,7 @@ bool GeoluxHydroCam::startSingleMeasurement(void) {
         _measurementAttemptsCompleted++;
     }
 
-    return true;
+    return success;
 }
 
 
@@ -316,6 +316,9 @@ void GeoluxHydroCam::powerDown(void) {
         _millisSensorActivated = 0;
         // Unset the measurement request time
         _millisMeasurementRequested = 0;
+        // Unset the number of measurements attempted and succeeded
+        _measurementAttemptsCompleted = 0;
+        _measurementsSucceeded        = 0;
         // Unset the status bits for sensor power (bits 1 & 2),
         // activation (bits 3 & 4), and measurement request (bits 5 & 6)
         clearStatusBits(POWER_ATTEMPTED, POWER_SUCCESSFUL, WAKE_ATTEMPTED,
